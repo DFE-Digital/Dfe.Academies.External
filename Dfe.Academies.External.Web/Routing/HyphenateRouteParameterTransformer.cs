08918a061c27b6851339fe7b32f6cd94f5dad939
Wystan Palm <wystan.palm@madetech.com>
2022-06-15 13:35:35 +0100

    AB#98184 Lowercase Hyphenated URLs - add Parameter Transformer class

---
+++ Dfe.Academies.External.Web/Routing/HyphenateRouteParameterTransformer.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dfe.Academies.External.Web.Routing
+{
+    public class HyphenateRouteParameterTransformer : IOutboundParameterTransformer
+    {
+        public string? TransformOutbound(object? value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            string? stringValue = value.ToString();
+
+            if (stringValue == null)
+            {
+                return null;
+            }
+
+            return Regex.Replace(stringValue, "([a-z])([A-Z])", "$1-$2").ToLower();
+        }
+    }
+}
