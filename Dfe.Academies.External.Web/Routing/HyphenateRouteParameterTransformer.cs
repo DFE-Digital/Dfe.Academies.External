3218718339b54b6655a810f5ad627a5a783e242a
Wystan Palm <wystan.palm@madetech.com>
2022-06-16 09:38:13 +0100

    AB#98184 Lowercase Hyphenated URLs - spaces to tabs

--- Dfe.Academies.External.Web/Routing/HyphenateRouteParameterTransformer.cs
+++ Dfe.Academies.External.Web/Routing/HyphenateRouteParameterTransformer.cs
@@ -2,16 +2,16 @@
 
 namespace Dfe.Academies.External.Web.Routing
 {
-    public class HyphenateRouteParameterTransformer : IOutboundParameterTransformer
-    {
-        public string? TransformOutbound(object? value)
-        {
-            if (value is not string stringValue)
-            {
-                return null;
-            }
+	public class HyphenateRouteParameterTransformer : IOutboundParameterTransformer
+	{
+		public string? TransformOutbound(object? value)
+		{
+			if (value is not string stringValue)
+			{
+				return null;
+			}
 
-            return Regex.Replace(stringValue, "([a-z])([A-Z])", "$1-$2").ToLower();
-        }
-    }
+			return Regex.Replace(stringValue, "([a-z])([A-Z])", "$1-$2").ToLower();
+		}
+	}
 }
