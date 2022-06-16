ebc7d3cea3c6d35c0d05cba8cebe891c7d15a3ca
Wystan Palm <wystan.palm@madetech.com>
2022-06-16 17:12:39 +0100

    AB#92475 Page Header & Footer - add linked static pages with page titles

---
+++ Dfe.Academies.External.Web/Pages/Cookies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dfe.Academies.External.Web.Pages
+{
+    public class CookiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
