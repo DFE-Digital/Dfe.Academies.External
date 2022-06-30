fa400d3b201da40bd22df3e69ca0a5202d42d804
humboldtsystems <humboldtsystems@gmail.com>
2022-06-30 16:35:34 +0100

    ported enums & added new enum

---
+++ Dfe.Academies.External.Web/Enums/AcademisationWorkflowEnums.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Dfe.Academies.External.Web.Enums;
+
+public enum ApplicationTypes : int
+{
+    [Description("Join a multi-academy trust")]
+    JoinMat = 1,
+    [Description("Form a new multi-academy trust")]
+    FormNewMat = 2,
+    [Description("Form new single academy trust")]
+    FormNewSingleAcademyTrust = 3
+}
+
+public enum SchoolRoles : int
+{
+    [Description("The chair of the school's governors")]
+    Chair = 1,
+    [Description("Something Else")]
+    Other = 2
+}
