f81ce95fcc1718e93237665ac1d34ca749693983
humboldtsystems <humboldtsystems@gmail.com>
2022-07-01 13:24:03 +0100

    stub tests

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/WhatIsYourRoleModelTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Dfe.Academies.External.Web.Pages;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages;
+
+[Parallelizable(ParallelScope.All)]
+public class WhatIsYourRoleModelTests
+{
+    [Test]
+    public async Task OnGetAsync_Success()
+    {
+        // arrange
+        var mockAcademisationCreationService = new Mock<IAcademisationCreationService>();
+        var mockLogger = new Mock<ILogger<WhatIsYourRoleModel>>();
+
+        var pageModel = SetupWhatIsYourRoleModel(mockLogger.Object, mockAcademisationCreationService.Object);
+
+        // act
+        await pageModel.OnGetAsync();
+
+        // assert
+        Assert.That(pageModel.TempData["Error.Message"], Is.EqualTo(null));
+    }
+
+
+    private static WhatIsYourRoleModel SetupWhatIsYourRoleModel(
+        ILogger<WhatIsYourRoleModel> mockLogger, IAcademisationCreationService mockAcademisationCreationService, bool isAuthenticated = false)
+    {
+        (PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+        return new WhatIsYourRoleModel(mockLogger, mockAcademisationCreationService)
+        {
+            PageContext = pageContext,
+            TempData = tempData,
+            Url = new UrlHelper(actionContext),
+            MetadataProvider = pageContext.ViewData.ModelMetadata
+        };
+    }
+}
