0bf7d73db354e01e646e82cabb46296c3cebd85c
humboldtsystems <humboldtsystems@gmail.com>
2022-07-01 13:25:28 +0100

    Helper service to put stuff in and out of TempData[]

---
+++ Dfe.Academies.External.Web/Services/TempDataHelperService.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfe.Academies.External.Web.Services;
+
+/// <summary>
+/// Helper service to put stuff in and out of TempData[]
+/// to handle serialization / de-serialization / type conversion /
+/// whether key found in TempData[]
+/// </summary>
+public class TempDataHelperService
+{
+    
+}
