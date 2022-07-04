d5931509a1ada1570a8bac6b485ce397f6f9c45d
humboldtsystems <humboldtsystems@gmail.com>
2022-07-04 16:52:58 +0100

    just some stubbing of new files to begin with

---
+++ Dfe.Academies.External.Web/Pages/ApplicationOverview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dfe.Academies.External.Web.Pages
+{
+    public class ApplicationOverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
