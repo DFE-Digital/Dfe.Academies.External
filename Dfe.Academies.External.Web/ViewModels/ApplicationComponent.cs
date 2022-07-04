0b2285053ea459734d97b3445d2bfea71a7f296c
humboldtsystems <humboldtsystems@gmail.com>
2022-07-04 21:53:58 +0100

    created view  model to base application component list on

---
+++ Dfe.Academies.External.Web/ViewModels/ApplicationComponent.cs
@@ -0,0 +1,12 @@
+﻿using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.ViewModels;
+
+public class ApplicationComponent
+{
+    public string Name { get; set; }
+
+    public string URI { get; set; }
+
+    public ApplicationComponentsStatus ApplicationComponentStatus { get; set; }
+}
