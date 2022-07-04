bb02d5f1d1571a08a4dcd00171362848761635b7
humboldtsystems <humboldtsystems@gmail.com>
2022-07-04 10:43:07 +0100

    initilise ValidationErrorMessages() in ctor

--- Dfe.Academies.External.Web/ViewModels/ValidationErrorMessagesViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/ValidationErrorMessagesViewModel.cs
@@ -2,5 +2,10 @@
 
 public sealed class ValidationErrorMessagesViewModel
 {
+    public ValidationErrorMessagesViewModel()
+    {
+        this.ValidationErrorMessages = new Dictionary<string, string>();
+    }
+
     public Dictionary<string, string> ValidationErrorMessages { get; set; }
 }
