82b98cbcc19d5b85e162da4c7f02b674d657b5c9
mokoruwaDfE <moses.okoruwa@education.gov.uk>
2022-07-05 09:06:02 +0100

    Installed and setup .NET Core NPM for GOV.UK Design

--- Dfe.Academies.External.Web/Program.cs
+++ Dfe.Academies.External.Web/Program.cs
@@ -1,4 +1,5 @@
 using Dfe.Academies.External.Web.Routing;
+using GovUk.Frontend.AspNetCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
@@ -7,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
+
+//https://github.com/gunndabad/govuk-frontend-aspnetcore  
+builder.Services.AddGovUkFrontend();
+
 builder.Services
 	.AddRazorPages(options =>
 	{
