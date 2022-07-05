a8cd62336d320d898acf522971998d46784e157e
humboldtsystems <humboldtsystems@gmail.com>
2022-07-05 17:47:24 +0100

    basic property tests

---
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/ApplicationAuditViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AutoFixture;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ApplicationAuditViewModelTests
+{
+    private static readonly Fixture Fixture = new();
+
+    [Test]
+    public void ApplicationAuditViewModel___PropertyCheck___Success()
+    {
+        // arrange
+        string what = Fixture.Create<string>();
+        string who = Fixture.Create<string>();
+        DateTime when = Fixture.Create<DateTime>();
+
+        var applicationAuditViewModel = new ApplicationAuditViewModel
+        {
+            What = what,
+            Who = who,
+            When = when
+        };
+
+        // assert
+        Assert.That(applicationAuditViewModel, Is.Not.Null);
+        Assert.That(applicationAuditViewModel.What, Is.EqualTo(what));
+        Assert.That(applicationAuditViewModel.Who, Is.EqualTo(who));
+        Assert.That(applicationAuditViewModel.When, Is.EqualTo(when));
+    }
+}
---
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/ApplicationComponentViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ApplicationComponentViewModelTests
+{
+    private static readonly Fixture Fixture = new();
+
+    [Test]
+    public void ApplicationComponentViewModel___PropertyCheck___Success()
+    {
+        // arrange
+        ApplicationComponentsStatus applicationComponentStatus = ApplicationComponentsStatus.InProgress;
+        string name = Fixture.Create<string>();
+        string URI = Fixture.Create<string>();
+        
+        var applicationAuditViewModel = new ApplicationComponentViewModel
+        {
+            ApplicationComponentStatus = applicationComponentStatus,
+            Name = name,
+            URI = URI
+        };
+
+        // assert
+        Assert.That(applicationAuditViewModel, Is.Not.Null);
+        Assert.That(applicationAuditViewModel.ApplicationComponentStatus, Is.EqualTo(applicationComponentStatus));
+        Assert.That(applicationAuditViewModel.Name, Is.EqualTo(name));
+        Assert.That(applicationAuditViewModel.URI, Is.EqualTo(URI));
+    }
+}
