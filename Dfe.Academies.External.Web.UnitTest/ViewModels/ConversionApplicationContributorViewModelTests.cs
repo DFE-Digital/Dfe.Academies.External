0019ca5e3db8c0beaffb0a267a6d516723a08634
humboldtsystems <humboldtsystems@gmail.com>
2022-07-05 17:59:28 +0100

    basic property check test

---
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/ConversionApplicationContributorViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ConversionApplicationContributorViewModelTests
+{
+    private static readonly Fixture Fixture = new();
+
+    [Test]
+    public void ConversionApplicationContributorViewModel___PropertyCheck___Success()
+    {
+        // arrange
+        string name = Fixture.Create<string>();
+
+        var applicationAuditViewModel = new ConversionApplicationContributorViewModel
+        {
+            Name = name
+        };
+
+        // assert
+        Assert.That(applicationAuditViewModel, Is.Not.Null);
+        Assert.That(applicationAuditViewModel.Name, Is.EqualTo(name));
+    }
+}
