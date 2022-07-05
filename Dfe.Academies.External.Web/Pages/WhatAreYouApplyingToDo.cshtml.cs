ec619e3b041dcf32e4f0d3655497168238dc65b8
humboldtsystems <humboldtsystems@gmail.com>
2022-07-05 16:10:37 +0100

    fixed code alignment

--- Dfe.Academies.External.Web/Pages/WhatAreYouApplyingToDo.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/WhatAreYouApplyingToDo.cshtml.cs
@@ -12,24 +12,26 @@ public class WhatAreYouApplyingToDoModel : BasePageModel
     private readonly ILogger<WhatAreYouApplyingToDoModel> _logger;
     private readonly IConversionApplicationCreationService _academisationCreationService;
     private const string NextStepPage = "/WhatIsYourRole";
+    private ConversionApplication _draftConversionApplication;
 
     public WhatAreYouApplyingToDoModel(ILogger<WhatAreYouApplyingToDoModel> logger, 
                                         IConversionApplicationCreationService academisationCreationService)
     {
         _logger = logger;
         _academisationCreationService = academisationCreationService;
+        _draftConversionApplication = new ConversionApplication();
     }
 
     [BindProperty]
     [RequiredEnum(ErrorMessage = "Select an application type")]
     public ApplicationTypes ApplicationType { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            // like on load - if navigating backwards from NextStepPage - will need to set model value from somewhere!
-            // _draftConversionApplication = TempDataHelper.GetSerialisedValue<ConversionApplication>("draftConversionApplication", TempData) ?? new ConversionApplication();
-            // ApplicationType = _draftConversionApplication.ApplicationType;
-        }
+    public async Task OnGetAsync()
+    {
+        // like on load - if navigating backwards from NextStepPage - will need to set model value from somewhere!
+        _draftConversionApplication = TempDataHelper.GetSerialisedValue<ConversionApplication>("draftConversionApplication", TempData) ?? new ConversionApplication();
+        // ApplicationType = _draftConversionApplication.ApplicationType;
+    }
 
     public async Task<IActionResult> OnPostAsync()
     {
@@ -40,39 +42,36 @@ public class WhatAreYouApplyingToDoModel : BasePageModel
             return Page();
         }
 
-            var applicationTypeSelected = ApplicationType;
-            try
-            {
-                var _draftConversionApplication = new ConversionApplication
-                {
-                    ApplicationType = applicationTypeSelected,
-                    UserEmail = "Auth user"
-                };
+        var applicationTypeSelected = ApplicationType;
+        try
+        {
+            _draftConversionApplication.ApplicationType = applicationTypeSelected;
+            _draftConversionApplication.UserEmail = "Auth user";
 
-                _draftConversionApplication = await _academisationCreationService.CreateNewApplication(_draftConversionApplication);
+            _draftConversionApplication = await _academisationCreationService.CreateNewApplication(_draftConversionApplication);
 
-                if(_draftConversionApplication != null)
-                    // MR:- plop newApplication.Id somewhere so NextStepPage can pick this up !
-                    TempDataHelper.StoreSerialisedValue("draftConversionApplication", TempData, _draftConversionApplication);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("Application::WhatAreYouApplyingToDoModel::OnPostAsync::Exception - {Message}", ex.Message);
-                return Page();
-            }
-            
-            switch (applicationTypeSelected)
-            {
-                case ApplicationTypes.JoinMat:
-                    TempData["applicationTypeSelected"] = applicationTypeSelected;
-                    return RedirectToPage(NextStepPage);
-                case ApplicationTypes.FormNewMat:
-                    TempData["applicationTypeSelected"] = applicationTypeSelected;
-                    return RedirectToPage(NextStepPage);
-                case ApplicationTypes.FormNewSingleAcademyTrust:
-                    TempData["applicationTypeSelected"] = applicationTypeSelected;
-                    return RedirectToPage(NextStepPage);
-            }
+            if(_draftConversionApplication != null)
+                // MR:- plop newApplication.Id somewhere so NextStepPage can pick this up !
+                TempDataHelper.StoreSerialisedValue("draftConversionApplication", TempData, _draftConversionApplication);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Application::WhatAreYouApplyingToDoModel::OnPostAsync::Exception - {Message}", ex.Message);
+            return Page();
+        }
+        
+        switch (applicationTypeSelected)
+        {
+            case ApplicationTypes.JoinMat:
+                TempData["applicationTypeSelected"] = applicationTypeSelected;
+                return RedirectToPage(NextStepPage);
+            case ApplicationTypes.FormNewMat:
+                TempData["applicationTypeSelected"] = applicationTypeSelected;
+                return RedirectToPage(NextStepPage);
+            case ApplicationTypes.FormNewSingleAcademyTrust:
+                TempData["applicationTypeSelected"] = applicationTypeSelected;
+                return RedirectToPage(NextStepPage);
+        }
 
         return Page();
     }
