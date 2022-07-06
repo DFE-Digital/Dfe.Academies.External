26f8228559eb2feba63b79658ec09d7b7abfc443
humboldtsystems <humboldtsystems@gmail.com>
2022-07-06 10:42:03 +0100

    basic property check tests

--- Dfe.Academies.External.Web.UnitTest/Models/SchoolOrSchoolsApplyingToConvertTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Models/SchoolOrSchoolsApplyingToConvertTests.cs
@@ -1,9 +1,32 @@
-﻿using NUnit.Framework;
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Models;
+using NUnit.Framework;
 
 namespace Dfe.Academies.External.Web.UnitTest.Models;
 
 [Parallelizable(ParallelScope.All)]
 internal sealed class SchoolOrSchoolsApplyingToConvertTests
 {
-    // TODO MR:-
+    private static readonly Fixture Fixture = new();
+
+    [Test]
+    public void SchoolOrSchoolsApplyingToConvert___PropertyCheck___Success()
+    {
+        // arrange
+        string schoolName = Fixture.Create<string>();
+        
+        var conversionApplication = new SchoolOrSchoolsApplyingToConvert
+        {
+            Id = int.MaxValue,
+            SchoolName = schoolName
+        };
+
+        // act
+        // nothing!
+
+        // assert
+        Assert.That(conversionApplication, Is.Not.Null);
+        Assert.That(conversionApplication.Id, Is.EqualTo(int.MaxValue));
+        Assert.That(conversionApplication.SchoolName, Is.EqualTo(schoolName));
+    }
 }
