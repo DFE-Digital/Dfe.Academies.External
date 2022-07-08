3a5660bf1ff82ab9bba5f303e50d7bc1692e2541
humboldtsystems <humboldtsystems@gmail.com>
2022-07-08 11:26:39 +0100

    associated unit tests for new models & new service funcs

---
+++ Dfe.Academies.External.Web.UnitTest/Models/ConversionApplicationAuditEntryTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Models;
+using NUnit.Framework;
+using System;
+
+namespace Dfe.Academies.External.Web.UnitTest.Models;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ConversionApplicationAuditEntryTests
+{
+    private static readonly Fixture Fixture = new();
+
+    [Test]
+    public void ConversionApplicationAuditEntry___PropertyCheck___Success()
+    {
+        // arrange
+        string createdBy = Fixture.Create<string>();
+        string typeOfChange = Fixture.Create<string>();
+        string entityChanged = Fixture.Create<string>();
+        string propertyChanged = Fixture.Create<string>();
+        DateTime when = Fixture.Create<DateTime>();
+
+        var conversionApplicationAuditEntry = new ConversionApplicationAuditEntry(createdBy, typeOfChange, entityChanged, propertyChanged)
+        {
+            Id = int.MaxValue,
+            DateCreated = when
+        };
+
+        // act
+        // nothing!
+
+        // assert
+        Assert.That(conversionApplicationAuditEntry, Is.Not.Null);
+        Assert.That(conversionApplicationAuditEntry.Id, Is.EqualTo(int.MaxValue));
+        Assert.That(conversionApplicationAuditEntry.DateCreated, Is.EqualTo(when));
+        Assert.That(conversionApplicationAuditEntry.CreatedBy, Is.EqualTo(createdBy));
+        Assert.That(conversionApplicationAuditEntry.TypeOfChange, Is.EqualTo(typeOfChange));
+        Assert.That(conversionApplicationAuditEntry.EntityChanged, Is.EqualTo(entityChanged));
+        Assert.That(conversionApplicationAuditEntry.PropertyChanged, Is.EqualTo(propertyChanged));
+    }
+}
---
+++ Dfe.Academies.External.Web.UnitTest/Services/ConversionApplicationRetrievalServiceTests.cs
@@ -0,0 +1,180 @@
+﻿using Dfe.Academies.External.Web.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dfe.Academies.External.Web.UnitTest.Services;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ConversionApplicationRetrievalServiceTests
+{
+    [Test]
+    public void ConversionApplicationRetrievalService___GetPendingApplications___Success()
+    {
+        // arrange
+        var expected = @"{ ""foo"": ""bar"" }"; // TODO MR:- will be json from Academies API
+        int expectedCount = 3; // TODO: 
+        string userEmail = string.Empty; // TODO: filter by useremail
+        var mockFactory = new Mock<IHttpClientFactory>();
+
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expected)
+            });
+
+        var httpClient = new HttpClient(mockMessageHandler.Object);
+
+        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+
+        // act
+        var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+        var expectedExistingApplicationsTestData = applicationRetrievalService.GetPendingApplications(userEmail);
+
+        // assert
+        Assert.That(expectedExistingApplicationsTestData, Is.Not.Null);
+        Assert.AreEqual(expectedCount, expectedExistingApplicationsTestData.Count, "Count is not correct");
+    }
+
+    [Test]
+    public void ConversionApplicationRetrievalService___GetCompletedApplications___Success()
+    {
+        // arrange
+        var expected = @"{ ""foo"": ""bar"" }"; // TODO MR:- will be json from Academies API
+        int expectedCount = 1; // TODO: 
+        string userEmail = string.Empty; // TODO: filter by useremail
+        var mockFactory = new Mock<IHttpClientFactory>();
+
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expected)
+            });
+
+        var httpClient = new HttpClient(mockMessageHandler.Object);
+
+        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+
+        // act
+        var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+        var expectedExistingApplicationsTestData = applicationRetrievalService.GetCompletedApplications(userEmail);
+
+        // assert
+        Assert.That(expectedExistingApplicationsTestData, Is.Not.Null);
+        Assert.AreEqual(expectedCount, expectedExistingApplicationsTestData.Count, "Count is not correct");
+    }
+
+    [Test]
+    public async Task ConversionApplicationRetrievalService___GetConversionApplicationAuditEntries___Success()
+    {
+        // arrange
+        var expected = @"{ ""foo"": ""bar"" }"; // TODO MR:- will be json from Academies API
+        int expectedCount = 3; // TODO: 
+        long applicationId = 99; // TODO: 
+        var mockFactory = new Mock<IHttpClientFactory>();
+
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expected)
+            });
+
+        var httpClient = new HttpClient(mockMessageHandler.Object);
+
+        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+
+        // act
+        var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+        var auditEntries = await applicationRetrievalService.GetConversionApplicationAuditEntries(applicationId);
+
+        // assert
+        Assert.That(auditEntries, Is.Not.Null);
+        Assert.AreEqual(expectedCount, auditEntries.Count, "Count is not correct");
+    }
+
+    [Test]
+    public async Task ConversionApplicationRetrievalService___GetConversionApplicationComponentStatuses___Success()
+    {
+        // arrange
+        var expected = @"{ ""foo"": ""bar"" }"; // TODO MR:- will be json from Academies API
+        int expectedCount = 8; // TODO: 
+        long applicationId = 99; // TODO: 
+        var mockFactory = new Mock<IHttpClientFactory>();
+
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expected)
+            });
+
+        var httpClient = new HttpClient(mockMessageHandler.Object);
+
+        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+
+        // act
+        var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+        var applicationComponentStatuses = await applicationRetrievalService.GetConversionApplicationComponentStatuses(applicationId);
+
+        // assert
+        Assert.That(applicationComponentStatuses, Is.Not.Null);
+        Assert.AreEqual(expectedCount, applicationComponentStatuses.Count, "Count is not correct");
+    }
+
+    [Test]
+    public async Task ConversionApplicationRetrievalService___GetConversionApplicationContributors___Success()
+    {
+        // arrange
+        var expected = @"{ ""foo"": ""bar"" }"; // TODO MR:- will be json from Academies API
+        int expectedCount = 2; // TODO: 
+        long applicationId = 99; // TODO: 
+        var mockFactory = new Mock<IHttpClientFactory>();
+
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expected)
+            });
+
+        var httpClient = new HttpClient(mockMessageHandler.Object);
+
+        mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+
+        // act
+        var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+        var applicationContributors = await applicationRetrievalService.GetConversionApplicationContributors(applicationId);
+
+        // assert
+        Assert.That(applicationContributors, Is.Not.Null);
+        Assert.AreEqual(expectedCount, applicationContributors.Count, "Count is not correct");
+    }
+}
