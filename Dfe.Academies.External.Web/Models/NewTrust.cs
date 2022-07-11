baaf3ae3be940f3b65ddbad53066c04e2f15aa2b
humboldtsystems <humboldtsystems@gmail.com>
2022-07-11 16:06:46 +0100

    stubbing out trust obj

---
+++ Dfe.Academies.External.Web/Models/NewTrust.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfe.Academies.External.Web.Models;
+
+public class NewTrust
+{
+    public int Id { get; set; }
+    public string ProposedTrustName { get; set; }
+
+    public DateTime? ProposedTrustOpeningDate { get; set; }
+
+    public List<NewTrustKeyPerson> NewTrustKeyPersons { get; set; } = new();
+}
