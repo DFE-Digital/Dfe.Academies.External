bdaa37b3b60ae1a35706ac489bb9f185178709c1
humboldtsystems <humboldtsystems@gmail.com>
2022-07-12 14:23:21 +0100

    fleshing from A2C-SIP - ApplyingSchool object

--- Dfe.Academies.External.Web/Models/SchoolLease.cs
+++ Dfe.Academies.External.Web/Models/SchoolLease.cs
@@ -4,4 +4,25 @@ public class SchoolLease
 {
     public int Id { get; set; }
     public int SchoolId { get; set; }
+
+    //// MR:- below props from A2C-SIP - ApplyingSchool object
+
+    /// <summary>
+    /// months or years ?
+    /// </summary>
+    public short LeaseTerm { get; set; }
+
+    public decimal? RepaymentAmount { get; set; }
+
+    public decimal? InterestRate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public string Purpose { get; set; }
+
+    public string Provider { get; set; }
+
+    public decimal? ValueOfAssets { get; set; }
+
+    public string ResponsibleForAssets { get; set; }
 }
