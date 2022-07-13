ae488593ca5ebe7509bf073a1d837cf2f4ead015
humboldtsystems <humboldtsystems@gmail.com>
2022-07-13 17:29:01 +0100

    new edit wizard page base model - with get data method

---
+++ Dfe.Academies.External.Web/Pages/Base/BasePageEditModel.cs
@@ -0,0 +1,38 @@
+﻿using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.ViewModels;
+
+namespace Dfe.Academies.External.Web.Pages.Base;
+
+public abstract class BasePageEditModel : BasePageModel
+{
+	private readonly IConversionApplicationRetrievalService _conversionApplicationRetrievalService;
+
+	protected BasePageEditModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService)
+	{
+		_conversionApplicationRetrievalService = conversionApplicationRetrievalService;
+	}
+
+	public async Task LoadAndSetApplicationDetails(int applicationId)
+	{
+		var applicationDetails = await _conversionApplicationRetrievalService.GetApplication(applicationId);
+
+		if (applicationDetails != null)
+		{
+			ApplicationCacheValuesViewModel cachedValuesViewModel = new(applicationDetails.Id, applicationDetails.ApplicationType);
+
+			ViewDataHelper.StoreSerialisedValue(nameof(ApplicationCacheValuesViewModel), ViewData, cachedValuesViewModel);
+		}
+	}
+
+	public async Task LoadAndSetSchoolDetails(int schoolId)
+	{
+		var schoolDetails = await _conversionApplicationRetrievalService.GetSchool(schoolId);
+
+		if (schoolDetails != null)
+		{
+			SchoolCacheValuesViewModel cachedValuesViewModel = new(schoolDetails.Id, schoolDetails.SchoolName);
+
+			ViewDataHelper.StoreSerialisedValue(nameof(SchoolCacheValuesViewModel), ViewData, cachedValuesViewModel);
+		}
+	}
+}
