4fee5f00aa02985048a0b6d864fdf118e2240616
humboldtsystems <humboldtsystems@gmail.com>
2022-07-13 12:28:53 +0100

    more tests for test coverage

---
+++ Dfe.Academies.External.Web.UnitTest/Models/ExistingTrustTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Models;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Models;
+
+internal sealed class ExistingTrustTests
+{
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void ExistingTrust___PropertyCheck___Success()
+	{
+		// arrange
+		string trustName = Fixture.Create<string>();
+
+		var applicationComponent = new ExistingTrust(trustName)
+		{
+			Id = int.MaxValue
+		};
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(applicationComponent, Is.Not.Null);
+		Assert.That(applicationComponent.Id, Is.EqualTo(int.MaxValue));
+		Assert.That(applicationComponent.TrustName, Is.EqualTo(trustName));
+	}
+}
---
+++ Dfe.Academies.External.Web.UnitTest/Models/NewTrustTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Models;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Models;
+
+internal sealed class NewTrustTests
+{
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void NewTrust___PropertyCheck___Success()
+	{
+		// arrange
+		string proposedTrustName = Fixture.Create<string>();
+
+		var applicationComponent = new NewTrust(proposedTrustName)
+		{
+			Id = int.MaxValue
+		};
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(applicationComponent, Is.Not.Null);
+		Assert.That(applicationComponent.Id, Is.EqualTo(int.MaxValue));
+		Assert.That(applicationComponent.ProposedTrustName, Is.EqualTo(proposedTrustName));
+	}
+}
