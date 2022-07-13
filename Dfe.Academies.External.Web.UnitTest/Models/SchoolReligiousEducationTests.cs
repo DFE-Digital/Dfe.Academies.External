1eac9693f617f27da70844164db6aa48b0e98ef1
humboldtsystems <humboldtsystems@gmail.com>
2022-07-13 12:38:25 +0100

    more tests

---
+++ Dfe.Academies.External.Web.UnitTest/Models/SchoolReligiousEducationTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Models;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Models;
+
+internal sealed class SchoolReligiousEducationTests
+{
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void SchoolReligiousEducation___PropertyCheck___Success()
+	{
+		// arrange
+		string faithDioceseName = Fixture.Create<string>();
+
+		var applicationComponent = new SchoolReligiousEducation(faithDioceseName)
+		{
+			Id = int.MaxValue
+		};
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(applicationComponent, Is.Not.Null);
+		Assert.That(applicationComponent.Id, Is.EqualTo(int.MaxValue));
+		Assert.That(applicationComponent.FaithDioceseName, Is.EqualTo(faithDioceseName));
+	}
+}
