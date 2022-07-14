0719417b98e9eb971ded352ae40a58c4f1ec1a14
humboldtsystems <humboldtsystems@gmail.com>
2022-07-14 12:32:18 +0100

    basic property tests for VM

---
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/ApplicationCacheValuesViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+
+internal sealed class ApplicationCacheValuesViewModelTests
+{
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void ApplicationCacheValuesViewModel___PropertyCheck___Success()
+	{
+		// arrange
+		int applicationId = int.MaxValue;
+		ApplicationTypes applicationType = ApplicationTypes.FormNewMat;
+
+		var conversionApplicationAuditEntry = new ApplicationCacheValuesViewModel(applicationId, applicationType);
+
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(conversionApplicationAuditEntry, Is.Not.Null);
+		Assert.That(conversionApplicationAuditEntry.ApplicationId, Is.EqualTo(applicationId));
+		Assert.That(conversionApplicationAuditEntry.ApplicationType, Is.EqualTo(applicationType));
+	}
+}
