8cec58bf2cf1e00492c809e1780eab7c11f1477c
humboldtsystems <humboldtsystems@gmail.com>
2022-07-14 11:57:43 +0100

    new view models for new UI pages / partial views

---
+++ Dfe.Academies.External.Web/ViewModels/ApplicationCacheValuesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.ViewModels;
+
+public class ApplicationCacheValuesViewModel
+{
+	public ApplicationCacheValuesViewModel(int applicationId, ApplicationTypes applicationType)
+	{
+		ApplicationId = applicationId;
+		ApplicationType = applicationType;
+	}
+
+	public int ApplicationId { get; set; }
+
+	/// <summary>
+	/// e.g. 'A2B_xxx'
+	/// </summary>
+	public string ApplicationReference => $"A2B_{ApplicationId}";
+
+	public ApplicationTypes ApplicationType { get; set; }
+}
---
+++ Dfe.Academies.External.Web/ViewModels/ConversionApplicationContributorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.ViewModels;
+
+public class ConversionApplicationContributorViewModel
+{
+    public string Name { get; set; }
+
+    public SchoolRoles Role { get; set; }
+
+    public string? OtherRoleNotListed { get; set; }
+}
