fbc05ededa2e54a2033154d1fe0f1f45467ef7e1
humboldtsystems <humboldtsystems@gmail.com>
2022-07-14 12:37:09 +0100

    stub tests classes ready for tests

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/ApplicationOverviewTests.cs
@@ -0,0 +1,18 @@
+﻿using Dfe.Academies.External.Web.Logger;
+using Dfe.Academies.External.Web.Pages;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ApplicationOverviewTests
+{
+	// TODO MR:-	
+}
