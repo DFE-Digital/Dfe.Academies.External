372e2d406d3372e5539d9736e785b2fe04998f76
humboldtsystems <humboldtsystems@gmail.com>
2022-07-14 11:57:01 +0100

    new viewData helper, similar to TempData helper as existing app uses TempData

---
+++ Dfe.Academies.External.Web/Services/ViewDataHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Dfe.Academies.External.Web.Services;
+
+public static class ViewDataHelper
+{
+	public static T? GetSerialisedValue<T>(string key, ViewDataDictionary viewData)
+	{
+		if (viewData.ContainsKey(key))
+		{
+			return JsonSerializer.Deserialize<T>(viewData[key]?.ToString() ?? string.Empty) ?? default(T);
+		}
+		else
+		{
+			return default;
+		}
+	}
+
+	public static void StoreSerialisedValue(string key, ViewDataDictionary viewData, object data)
+	{
+		viewData[key] = JsonSerializer.Serialize(data);
+	}
+
+	public static string? GetNonSerialisedValue(string key, ViewDataDictionary viewData)
+	{
+		if (viewData.ContainsKey(key))
+		{
+			return viewData[key]?.ToString() ?? string.Empty;
+		}
+		else
+		{
+			return string.Empty;
+		}
+	}
+
+	public static void StoreNonSerialisedValue(string key, ViewDataDictionary viewData, object data)
+	{
+		viewData[key] = data;
+	}
+}
