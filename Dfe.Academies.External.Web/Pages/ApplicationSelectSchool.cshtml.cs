91bd71577219b3622b3b5d69af30abd67c99e5f3
humboldtsystems <humboldtsystems@gmail.com>
2022-07-19 14:56:33 +0100

    further wiring up

--- Dfe.Academies.External.Web/Pages/ApplicationSelectSchool.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/ApplicationSelectSchool.cshtml.cs
@@ -3,23 +3,29 @@ using Dfe.Academies.External.Web.Pages.Base;
 using Dfe.Academies.External.Web.Services;
 using Dfe.Academies.External.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Dfe.Academies.External.Web.Pages
 {
-    public class ApplicationSelectSchoolModel : BasePageModel
+	public class ApplicationSelectSchoolModel : BasePageModel
     {
 	    private readonly ILogger<ApplicationSelectSchoolModel> _logger;
 	    private readonly IConversionApplicationCreationService _academisationCreationService;
+	    private readonly IReferenceDataRetrievalService _referenceDataRetrievalService;
 		private const string NextSchoolStepPage = "/ApplicationOverview";
-		
+		private const int SearchQueryMinLength = 3;
+
 		public SchoolSelectorViewModel ViewModel { get; set; }
 
 		public ApplicationSelectSchoolModel(ILogger<ApplicationSelectSchoolModel> logger,
-		    IConversionApplicationCreationService academisationCreationService)
+		    IConversionApplicationCreationService academisationCreationService,
+		    IReferenceDataRetrievalService referenceDataRetrievalService)
 	    {
 		    _logger = logger;
 		    _academisationCreationService = academisationCreationService;
-	    }
+		    _referenceDataRetrievalService = referenceDataRetrievalService;
+
+		}
 
 		public async Task OnGetAsync()
 		{
@@ -36,9 +42,61 @@ namespace Dfe.Academies.External.Web.Pages
 			}
 			catch (Exception ex)
 			{
-				_logger.LogError("Application::WhatIsYourRoleModel::OnGetAsync::Exception - {Message}", ex.Message);
+				_logger.LogError("School::ApplicationSelectSchoolModel::OnGetAsync::Exception - {Message}", ex.Message);
 			}
 		}
+		
+		public async Task<ActionResult> OnGetSchoolsSearchResult(string searchQuery)
+		{
+			try
+			{
+				_logger.LogInformation("School::ApplicationSelectSchoolModel::OnGetSchoolsSearchResult");
+
+				// Double check search query.
+				if (string.IsNullOrEmpty(searchQuery) || searchQuery.Length < SearchQueryMinLength)
+				{
+					return new JsonResult(Array.Empty<SchoolSearchResultViewModel>());
+				}
+
+				var schoolSearch = new SchoolSearch(searchQuery, searchQuery);
+				var schoolSearchResponse = await _referenceDataRetrievalService.SearchSchools(schoolSearch);
+
+				return new JsonResult(schoolSearchResponse);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("School::ApplicationSelectSchoolModel::OnGetSchoolsSearchResult::Exception - {Message}", ex.Message);
+
+				return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+			}
+		}
+
+		// TODO MR:- do I need this????
+		//public async Task<ActionResult> OnGetSelectedSchool(string schoolUkPrn)
+		//{
+		//	try
+		//	{
+		//		_logger.LogInformation("School::ApplicationSelectSchoolModel::OnGetSelectedSchool");
+
+		//		// Double check selected trust.
+		//		if (string.IsNullOrEmpty(schoolUkPrn) || schoolUkPrn.Contains("-") || schoolUkPrn.Length < SearchQueryMinLength)
+		//			throw new Exception($"Trust::ApplicationSelectSchoolModel::OnGetSelectedSchool::Selected trust is incorrect - {schoolUkPrn}");
+
+		//		// Store CaseState into cache.
+		//		var userState = await _cachedService.GetData<UserState>(User.Identity.Name) ?? new UserState();
+		//		userState.TrustUkPrn = schoolUkPrn;
+		//		userState.CreateCaseModel = new CreateCaseModel();
+		//		await _cachedService.StoreData(User.Identity.Name, userState);
+
+		//		return new JsonResult(new { redirectUrl = Url.Page("Overview", new { id = schoolUkPrn }) });
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//		_logger.LogError("School::ApplicationSelectSchoolModel::OnGetSelectedSchool::Exception - {Message}", ex.Message);
+
+		//		return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+		//	}
+		//}
 
 		public async Task<IActionResult> OnPostAsync()
 	    {
