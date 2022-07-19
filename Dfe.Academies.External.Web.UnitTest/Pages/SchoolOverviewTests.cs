eef5fd0d7f243885b0fe496313e66bf52a43f134
humboldtsystems <humboldtsystems@gmail.com>
2022-07-19 11:27:49 +0100

    added missing unit test

--- Dfe.Academies.External.Web.UnitTest/Pages/SchoolOverviewTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Pages/SchoolOverviewTests.cs
@@ -1,4 +1,4 @@
-﻿using Dfe.Academies.External.Web.Logger;
+﻿using System.Threading.Tasks;
 using Dfe.Academies.External.Web.Pages;
 using Dfe.Academies.External.Web.Services;
 using Dfe.Academies.External.Web.UnitTest.Factories;
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 
@@ -14,5 +15,49 @@ namespace Dfe.Academies.External.Web.UnitTest.Pages;
 [Parallelizable(ParallelScope.All)]
 internal sealed class SchoolOverviewTests
 {
-	// TODO MR:-
+	/// <summary>
+	/// "draftConversionApplication" in temp storage
+	/// from previous step in the new application wizard
+	/// </summary>
+	/// <returns></returns>
+	[Test]
+	public async Task SchoolOverviewModel___OnGetAsync___Valid()
+	{
+		// arrange
+		var draftConversionApplicationStorageKey = TempDataHelper.DraftConversionApplicationKey;
+		var mockConversionApplicationRetrievalService = new Mock<IConversionApplicationRetrievalService>();
+		var mockLogger = new Mock<ILogger<SchoolOverviewModel>>();
+
+		var conversionApplication = ConversionApplicationTestDataFactory.BuildNewConversionApplicationWithChairRole();
+
+		// act
+		var pageModel = SetupSchoolOverviewModel(mockLogger.Object, mockConversionApplicationRetrievalService.Object);
+		TempDataHelper.StoreSerialisedValue(draftConversionApplicationStorageKey, pageModel.TempData, conversionApplication);
+
+		// act
+		await pageModel.OnGetAsync();
+
+		// assert
+		Assert.That(pageModel.TempData["Errors"], Is.EqualTo(null));
+	}
+
+	// TODO MR:- SchoolOverviewModel___OnPostAsync___ModelIsValid___InValid
+
+	// TODO MR:- SchoolOverviewModel___OnPostAsync___ModelIsValid___Valid
+
+	private static SchoolOverviewModel SetupSchoolOverviewModel(
+		ILogger<SchoolOverviewModel> mockLogger,
+		IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+		bool isAuthenticated = false)
+	{
+		(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+		return new SchoolOverviewModel(mockLogger, mockConversionApplicationRetrievalService)
+		{
+			PageContext = pageContext,
+			TempData = tempData,
+			Url = new UrlHelper(actionContext),
+			MetadataProvider = pageContext.ViewData.ModelMetadata
+		};
+	}
 }
