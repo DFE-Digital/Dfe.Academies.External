7abce8f776e6f2bfa7f707e4d3d31f64ca7cf7f0
humboldtsystems <humboldtsystems@gmail.com>
2022-07-20 21:02:32 +0100

    new tests

--- Dfe.Academies.External.Web.UnitTest/ViewModels/SchoolDetailsViewModelTests.cs
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/SchoolDetailsViewModelTests.cs
@@ -1,6 +1,39 @@
-﻿namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
 
 internal sealed class SchoolDetailsViewModelTests
 {
-	// TODO MR:- tests
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void SchoolDetailsViewModel___PropertyCheck___Success()
+	{
+		// arrange
+		string schoolName = Fixture.Create<string>();
+		int urn = Fixture.Create<int>();
+		string street = Fixture.Create<string>();
+		string town = Fixture.Create<string>();
+		string fullUkPostcode = Fixture.Create<string>();
+
+		var schoolDetailsViewModel = new SchoolDetailsViewModel(schoolName, urn, street, town, fullUkPostcode)
+			{
+				Address3 = "address3",
+				CountyDescription = "county",
+				EstablishmentNumber = "7083"
+			};
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(schoolDetailsViewModel, Is.Not.Null);
+		Assert.That(schoolDetailsViewModel.SchoolName, Is.EqualTo(schoolName));
+		Assert.That(schoolDetailsViewModel.URN, Is.EqualTo(urn));
+		Assert.That(schoolDetailsViewModel.Street, Is.EqualTo(street));
+		Assert.That(schoolDetailsViewModel.Town, Is.EqualTo(town));
+		Assert.That(schoolDetailsViewModel.FullUkPostcode, Is.EqualTo(fullUkPostcode));
+	}
 }
