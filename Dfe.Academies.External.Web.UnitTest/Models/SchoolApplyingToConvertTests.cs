55830dd6799c8efc02978964de8bc8539f35fafe
humboldtsystems <humboldtsystems@gmail.com>
2022-07-20 14:37:24 +0100

    test fixes as per object changes

--- Dfe.Academies.External.Web.UnitTest/Models/SchoolApplyingToConvertTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Models/SchoolApplyingToConvertTests.cs
@@ -13,9 +13,15 @@ internal sealed class SchoolApplyingToConvertTests
     public void SchoolOrSchoolsApplyingToConvert___PropertyCheck___Success()
     {
         // arrange
+        int urn = Fixture.Create<int>();
+        string? ukprn = null;
         string schoolName = Fixture.Create<string>();
-        
-        var conversionApplication = new SchoolApplyingToConvert(schoolName)
+        string street = Fixture.Create<string>();
+        string town = Fixture.Create<string>();
+        string fullUkPostcode = Fixture.Create<string>();
+
+        // 
+        var conversionApplication = new SchoolApplyingToConvert(schoolName, urn, ukprn, street, town, fullUkPostcode)
         {
 	        SchoolId = int.MaxValue
         };
@@ -25,7 +31,12 @@ internal sealed class SchoolApplyingToConvertTests
 
         // assert
         Assert.That(conversionApplication, Is.Not.Null);
-        Assert.That(conversionApplication.SchoolId, Is.EqualTo(int.MaxValue));
+        Assert.That(conversionApplication.URN, Is.EqualTo(urn));
+        Assert.That(conversionApplication.UKPRN, Is.EqualTo(ukprn));
         Assert.That(conversionApplication.SchoolName, Is.EqualTo(schoolName));
+        Assert.That(conversionApplication.Street, Is.EqualTo(street));
+        Assert.That(conversionApplication.Town, Is.EqualTo(town));
+        Assert.That(conversionApplication.FullUkPostcode, Is.EqualTo(fullUkPostcode));
+        Assert.That(conversionApplication.SchoolId, Is.EqualTo(int.MaxValue));
     }
 }
