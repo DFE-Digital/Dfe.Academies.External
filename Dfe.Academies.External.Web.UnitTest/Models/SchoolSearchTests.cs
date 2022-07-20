55f1cc0666caeeaf4dd3e63086f75c52b5897104
humboldtsystems <humboldtsystems@gmail.com>
2022-07-20 21:13:27 +0100

    more tests

--- Dfe.Academies.External.Web.UnitTest/Models/SchoolSearchTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Models/SchoolSearchTests.cs
@@ -1,6 +1,28 @@
-﻿namespace Dfe.Academies.External.Web.UnitTest.Models;
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Models;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Models;
 
 internal sealed class SchoolSearchTests
 {
-	// TODO MR:- property tests
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void SchoolSearch___PropertyCheck___Success()
+	{
+		// arrange
+		string schoolName = Fixture.Create<string>();
+		int urn = Fixture.Create<int>();
+
+		var schoolSearch = new SchoolSearch(schoolName, urn.ToString());
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(schoolSearch, Is.Not.Null);
+		Assert.That(schoolSearch.SchoolName, Is.EqualTo(schoolName));
+		Assert.That(schoolSearch.Urn, Is.EqualTo(urn.ToString()));
+	}
 }
