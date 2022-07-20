d83c02672e88e84a124eb6dc8855d20f734a19f9
humboldtsystems <humboldtsystems@gmail.com>
2022-07-20 17:01:19 +0100

    removed un-necessary code

--- Dfe.Academies.External.Web/wwwroot/js/a2c.schoolsearch.js
+++ Dfe.Academies.External.Web/wwwroot/js/a2c.schoolsearch.js
@@ -8,7 +8,7 @@
   * Dependencies:-
  * 1) https://www.npmjs.com/package/accessible-autocomplete
  *      accessible-autocomplete is a JavaScript autocomplete built from the ground up to be accessible.
- * 2) JQuery
+ * 2) JQuery - for $.ajax && selectElement: $('#schoolSelect')[0] && $('#schoolSelectedDetails').html
   */
 
 var A2C = window.A2C || {};
@@ -24,30 +24,17 @@ $(function () {
     //$("#autocomplete-container").empty();
 });
 
-//A2C.hideSelectedSchoolSectionAndConfirmCheckbox = function () {
-//    // hide school results div
-//    const schoolSelectedDetails = document.getElementById("schoolSelectedDetails");
-//    schoolSelectedDetails.classList.add("hideElement");
-    
-//    // hide confirm checkbox until school selected
-//	//$('#confirm-school-checkbox').css('display', 'none')
-// //       .css('margin-bottom', '1.3em');
-    
-//	const checkboxContainer = document.getElementById("confirm-school-checkbox");
-//    checkboxContainer.classList.add("hideElement");
-//};
-
 A2C.unhideSelectedSchoolSectionAndConfirmCheckbox = function () {
     A2C.unhideElement("schoolSelectedDetails");
-    A2C.unhideElement("confirm-school-cb");
     A2C.unhideElement("confirm-school-label");
+
+    const elementToManipulate = document.getElementById("confirm-school-cb");
+    elementToManipulate.classList.remove("hideElement");
 };
 
 A2C.unhideElement = function (elementName) {
     const elementToManipulate = document.getElementById(elementName);
-
-    console.log(elementToManipulate);
-
+    
     elementToManipulate.classList.remove("hideElement");
     elementToManipulate.classList.add("unHideElement");
 };
@@ -63,7 +50,8 @@ A2C.searchSchools = function () {
         onConfirm: (function (selectedValue) {
             A2C.renderSchoolSearchOption(selectedValue);
 
-			setTimeout(() => {
+            setTimeout(() => {
+                    // MR:- btnAdd always visible now !!!
 					//$('#btnAdd').addClass('govuk-button--disabled')
 					//	.attr('aria-disabled', 'true')
 					//	.prop('disabled', 'true');
@@ -103,39 +91,22 @@ A2C.renderSelectedSchool = function (responseHtml) {
 function debounceSuggest(query, syncResults) {
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
-	    getSchools(query, syncResults);
+	    A2C.GetSchoolSearchResults(query, syncResults);
     }, 500);
 }
 
-// call search schools endpoint
-function getSchools(query, syncResults) {
-    $.ajax({
-        url: 'school/Search', // this calls a controller endpoint
-        type: 'GET',
-        data: { 'searchQuery': query },
-        success: function (response) {
-	        syncResults(query
-		        ? response.filter(function(result) {
-			        return true;
-		        })
-		        : []
-	        );
-        }
-    });
-}
-
-function toggleConfirmationCheckbox() {
-    if ($('#confirm-school-cb').prop('checked') === true)
-    {
-        // MR:- current app disables add button until validation = success / confirm checkbox = true
-        $('#btnAdd').removeClass('govuk-button--disabled')
-            .attr('aria-disabled', 'false')
-            .removeAttr('disabled');
-    }
-    else
-    {
-        $('#btnAdd').addClass('govuk-button--disabled')
-            .attr('aria-disabled', 'true')
-            .prop('disabled', 'true');
-    }
-}
+A2C.GetSchoolSearchResults = function(query, syncResults) {
+	$.ajax({
+		url: 'school/Search', // this calls a controller endpoint
+		type: 'GET',
+		data: { 'searchQuery': query },
+		success: function(response) {
+			syncResults(query
+				? response.filter(function(result) {
+					return true;
+				})
+				: []
+			);
+		}
+	});
+};
