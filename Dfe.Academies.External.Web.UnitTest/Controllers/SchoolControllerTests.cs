8edb82ce12c1b922b5f6839042c86d13e9c0c08d
humboldtsystems <humboldtsystems@gmail.com>
2022-07-20 21:33:37 +0100

    more stub tests

---
+++ Dfe.Academies.External.Web.UnitTest/Controllers/SchoolControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Dfe.Academies.External.Web.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Dfe.Academies.External.Web.UnitTest.Controllers;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class SchoolControllerTests
+{
+	// TODO MR:- test Overview(int appId, int applyingSchoolId)
+	// returns IActionResult = html / razor page
+
+	// TODO MR:- test Search(string searchQuery)
+	// searchQuery = schoolname = 'wise' OR URN = 587
+	// returns IEnumerable<string>
+
+	// TODO MR:- ReturnSchoolDetailsPartialViewPopulated(string selectedSchool)
+	// selectedSchool = format from autocomplete = '$"{schoolName} ({urn})"'
+	// returns IActionResult = html / razor page
+}
