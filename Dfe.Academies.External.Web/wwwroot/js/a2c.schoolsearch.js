653aeea7b255ce549b6b5f97b0623091d777edd9
humboldtsystems <humboldtsystems@gmail.com>
2022-07-20 12:40:11 +0100

    code clean up && amend URLs

--- Dfe.Academies.External.Web/wwwroot/js/a2c.schoolsearch.js
+++ Dfe.Academies.External.Web/wwwroot/js/a2c.schoolsearch.js
@@ -1,45 +1,63 @@
+/**
+ * Copyright (c) 2022
+ *
+ * School search Javascript to wire up to a local GET endpoint which will then in turn call
+ * and academies API endpoint to perform a school search.
+ * Depends upon components JQuery and accessible-autocomplete which are injected in _layout file.
+ *
+  * Dependencies:-
+ * 1) https://www.npmjs.com/package/accessible-autocomplete
+ *      accessible-autocomplete is a JavaScript autocomplete built from the ground up to be accessible.
+ * 2) JQuery
+  */
+
 var A2C = window.A2C || {};
 
 let debounceTimeout;
 
 $(function () {
     A2C.searchSchools();
+
+    $('#confirm-school-checkbox').css('display', 'none')
+	    .css('margin-bottom', '1.3em');
 });
 
 A2C.searchSchools = function () {
-    accessibleAutocomplete.enhanceSelectElement({
-        source: debounceSuggest,
-        defaultValue: '',
-        displayMenu: 'overlay',
-        confirmOnBlur: false,
-        selectElement: $('#schoolSelect')[0],
-        minLength: 4,
-        onConfirm: (function (selectedValue) {
-            A2C.renderSchoolSearchOption(selectedValue);
+	accessibleAutocomplete.enhanceSelectElement({
+		source: debounceSuggest,
+		defaultValue: '',
+		displayMenu: 'overlay',
+		confirmOnBlur: false,
+		selectElement: $('#schoolSelect')[0],
+		minLength: 4,
+		onConfirm: (function(selectedValue) {
+			A2C.renderSchoolSearchOption(selectedValue);
 
-            setTimeout(() => {
-                $('#btnAdd').addClass('govuk-button--disabled')
-                .attr('aria-disabled', 'true')
-                    .prop('disabled', 'true');
+			setTimeout(() => {
+					$('#btnAdd').addClass('govuk-button--disabled')
+						.attr('aria-disabled', 'true')
+						.prop('disabled', 'true');
 
-                if ($('#confirm-school-checkbox').length == 0) {
-                    $('#btnAdd').removeClass('govuk-button--disabled')
-                        .attr('aria-disabled', 'false')
-                        .removeAttr('disabled');
-                } }, 2000);
-        })
-    })
+					if ($('#confirm-school-checkbox').length === 0) {
+						$('#btnAdd').removeClass('govuk-button--disabled')
+							.attr('aria-disabled', 'false')
+							.removeAttr('disabled');
+					}
+				},
+				2000);
+		})
+	});
 };
 
 A2C.renderSchoolSearchOption = function (selectedValue) {
     // get full school deets from an endpoint, then render
     $.ajax({
-        url: 'DisplaySearchResult',
+        url: 'school/DisplaySearchResult',
         type: 'GET',
         data: { 'SchoolId': selectedValue },
         success: function (response) {
-            //$('#searchResults').html(response); // TODO MR:- amend for partial view
-            $('#confirm-school-checkbox').css('display', 'block')
+            // $('#searchResults').html(response); // TODO MR:- amend to bind partial view to a model
+            $('#confirm-school-checkbox').css('display', 'block');
             $('#confirm-school-checkbox').removeClass('hidden');
         }
     });
@@ -48,34 +66,29 @@ A2C.renderSchoolSearchOption = function (selectedValue) {
 function debounceSuggest(query, syncResults) {
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
-        getSchools(query, syncResults)
+	    getSchools(query, syncResults);
     }, 500);
 }
 
 // call search schools endpoint
 function getSchools(query, syncResults) {
     $.ajax({
-        url: 'Search', // this calls a controller endpont
+        url: 'school/Search', // this calls a controller endpoint
         type: 'GET',
         data: { 'searchQuery': query },
         success: function (response) {
-            syncResults(query
-                ? response.filter(function (result) {
-                    return true
-                })
-                : []
-            )
+	        syncResults(query
+		        ? response.filter(function(result) {
+			        return true;
+		        })
+		        : []
+	        );
         }
     });
 }
 
-$(document).ready(function () {
-    $('#confirm-school-checkbox').css('display', 'none') // This alerts: <a href="#">Services</a>
-        .css('margin-bottom', '1.3em');
-});
-
 function toggleConfirmationCheckbox() {
-    if ($('#confirm-school-cb').prop('checked') == true) {
+    if ($('#confirm-school-cb').prop('checked') === true) {
         $('#btnAdd').removeClass('govuk-button--disabled')
             .attr('aria-disabled', 'false')
             .removeAttr('disabled');
