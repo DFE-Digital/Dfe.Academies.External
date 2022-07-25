c57ab34a81add4ee0ce1191f362d0ec26ba615e4
humboldtsystems <humboldtsystems@gmail.com>
2022-07-25 18:09:18 +0100

    removed un-needed code

--- Dfe.Academies.External.Web/wwwroot/js/site.js
+++ Dfe.Academies.External.Web/wwwroot/js/site.js
@@ -1,16 +1,16 @@
 ﻿// MR:- below from concerns casework
-window.showGlobalError = function () {
-	$("#moj-banner-error").removeClass("govuk-!-display-none");
-};
-window.hideGlobalError = function () {
-	$("#moj-banner-error").addClass("govuk-!-display-none");
-};
-window.showLoader = function () {
-	$(".ccms-loader").removeClass("govuk-!-display-none");
-};
-window.hideLoader = function () {
-	$(".ccms-loader").addClass("govuk-!-display-none");
-};
+//window.showGlobalError = function () {
+//	$("#moj-banner-error").removeClass("govuk-!-display-none");
+//};
+//window.hideGlobalError = function () {
+//	$("#moj-banner-error").addClass("govuk-!-display-none");
+//};
+//window.showLoader = function () {
+//	$(".ccms-loader").removeClass("govuk-!-display-none");
+//};
+//window.hideLoader = function () {
+//	$(".ccms-loader").addClass("govuk-!-display-none");
+//};
 
 // Form validator
 //window.formValidator = function (form) {
