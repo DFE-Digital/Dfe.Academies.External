e29d964733c7d230b8301c891d1e4eb519baa7b9
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 17:40:04 +0100

    fixed inheritance issue

--- Dfe.Academies.External.Web/AcademiesAPIResponseModels/Base/PageSearch.cs
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/Base/PageSearch.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels.Base
 {
-	public sealed class PageSearch
+	public abstract class PageSearch
 	{
 		private int _page = 1;
 		public int Page { get { return _page; } }
