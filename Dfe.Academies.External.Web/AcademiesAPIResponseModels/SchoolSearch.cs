a687cb9a3ce6abefed66c8511360258207b710eb
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 17:51:41 +0100

    moved obj from other folder

---
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/SchoolSearch.cs
@@ -0,0 +1,16 @@
+﻿namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels;
+
+/// <summary>
+/// Container object to pass school search criteria to API layer :-
+/// {{api-host}}/establishments?Name=wise&api-version=V1&Urn=101934&ukprn=10006563
+/// </summary>
+public class SchoolSearch
+{
+	public SchoolSearch(string name, string urn, string ukprn) =>
+		(Name, Urn, Ukprn) = (name, urn, ukprn);
+
+	public string Name { get; }
+	public string Urn { get; }
+
+	public string Ukprn { get; set; }
+}
