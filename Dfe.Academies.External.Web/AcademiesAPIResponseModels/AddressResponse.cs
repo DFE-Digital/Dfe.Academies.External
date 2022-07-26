4e0e88a80dc94e236af53e24035aea3f621bae31
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 17:51:13 +0100

    dot net 6'd it

--- Dfe.Academies.External.Web/AcademiesAPIResponseModels/AddressResponse.cs
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/AddressResponse.cs
@@ -1,19 +1,18 @@
-namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels
-{
-    public class AddressResponse
-    {
-	    public AddressResponse(string street, string town, string fullUkPostcode)
-	    {
-		    Street = street;
-		    Town = town;
-		    Postcode = fullUkPostcode;
-	    }
+namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels;
 
-        public string Street { get; set; }
-        public string? Locality { get; set; }
-        public string? AdditionalLine { get; set; }
-        public string Town { get; set; }
-        public string? County { get; set; }
-        public string Postcode { get; set; }
+public class AddressResponse
+{
+    public AddressResponse(string street, string town, string fullUkPostcode)
+    {
+	    Street = street;
+	    Town = town;
+	    Postcode = fullUkPostcode;
     }
+
+    public string Street { get; set; }
+    public string? Locality { get; set; }
+    public string? AdditionalLine { get; set; }
+    public string Town { get; set; }
+    public string? County { get; set; }
+    public string Postcode { get; set; }
 }
