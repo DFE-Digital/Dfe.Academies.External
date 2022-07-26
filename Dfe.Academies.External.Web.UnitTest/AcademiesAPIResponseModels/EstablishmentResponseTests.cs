b9982e29b530710e1f161e40d0e817f597eb9d13
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 15:21:52 +0100

    just property setter / getter tests

--- Dfe.Academies.External.Web.UnitTest/AcademiesAPIResponseModels/EstablishmentResponseTests.cs
+++ Dfe.Academies.External.Web.UnitTest/AcademiesAPIResponseModels/EstablishmentResponseTests.cs
@@ -1,4 +1,5 @@
 ﻿using AutoFixture;
+using Dfe.Academies.External.Web.AcademiesAPIResponseModels;
 using NUnit.Framework;
 
 namespace Dfe.Academies.External.Web.UnitTest.AcademiesAPIResponseModels;
@@ -8,5 +9,29 @@ internal sealed class EstablishmentResponseTests
 {
 	private static readonly Fixture Fixture = new();
 
+	[Test]
+	public void EstablishmentResponse___PropertyCheck___Success()
+	{
+		// arrange
+		int urn = Fixture.Create<int>();
+		string schoolName = Fixture.Create<string>();
+		string ukprn = Fixture.Create<string>();
+		string street = Fixture.Create<string>();
+		string town = Fixture.Create<string>();
+		string fullUkPostcode = Fixture.Create<string>();
+		
+		var establishmentResponse = new EstablishmentResponse(schoolName, urn, ukprn, street, town, fullUkPostcode);
 
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(establishmentResponse, Is.Not.Null);
+		Assert.That(establishmentResponse.Name, Is.EqualTo(schoolName));
+		Assert.That(establishmentResponse.Urn, Is.EqualTo(urn));
+		Assert.That(establishmentResponse.Ukprn, Is.EqualTo(ukprn));
+		Assert.That(establishmentResponse.Address.Street, Is.EqualTo(street));
+		Assert.That(establishmentResponse.Address.Town, Is.EqualTo(town));
+		Assert.That(establishmentResponse.Address.Postcode, Is.EqualTo(fullUkPostcode));
+	}
 }
