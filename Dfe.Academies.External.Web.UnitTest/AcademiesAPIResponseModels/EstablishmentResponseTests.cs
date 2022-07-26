05108c15322c8a43959c9b4bd9b9552b797c1653
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 13:10:10 +0100

    more tests

---
+++ Dfe.Academies.External.Web.UnitTest/AcademiesAPIResponseModels/EstablishmentResponseTests.cs
@@ -0,0 +1,12 @@
+﻿using AutoFixture;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.AcademiesAPIResponseModels;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class EstablishmentResponseTests
+{
+	private static readonly Fixture Fixture = new();
+
+
+}
