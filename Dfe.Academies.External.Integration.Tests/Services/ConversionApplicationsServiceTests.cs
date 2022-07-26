1f1eb618ed99136ac526bdbe7ba081a21b1a0016
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 16:43:33 +0100

    new integration tests project to throw in API tests into

---
+++ Dfe.Academies.External.Integration.Tests/Services/ConversionApplicationsServiceTests.cs
@@ -0,0 +1,18 @@
+namespace Dfe.Academies.External.Integration.Tests.Services
+{
+	internal sealed class ConversionApplicationsServiceTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+
+		// TODO MR:-
+	}
+}
---
+++ Dfe.Academies.External.Integration.Tests/Services/ReferenceDataRetrievalServiceTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Dfe.Academies.External.Integration.Tests.Services;
+
+internal sealed class ReferenceDataRetrievalServiceTests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Test1()
+	{
+		Assert.Pass();
+	}
+
+	// TODO MR:-
+}
