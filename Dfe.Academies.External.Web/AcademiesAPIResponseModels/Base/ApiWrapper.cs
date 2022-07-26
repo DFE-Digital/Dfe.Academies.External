78d77ec921c31cf016e809464dfe9753a8bad532
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 17:00:30 +0100

    porting base classes from concerns casework

---
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/Base/ApiWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels.Base
+{
+	public sealed class ApiWrapper<T>
+	{
+		[JsonPropertyName("data")]
+		public T Data { get; }
+		
+		[JsonConstructor]
+		public ApiWrapper(T data) => (Data) = (data);
+	}
+}
