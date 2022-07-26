9fe982c116f148fc35ff6ee585ff31d6d17da6d4
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 15:22:01 +0100

    attribute tests

---
+++ Dfe.Academies.External.Web.UnitTest/CustomValidators/ConfirmSelectionAttributeTests.cs
@@ -0,0 +1,49 @@
+﻿using Dfe.Academies.External.Web.CustomValidators;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.CustomValidators;
+
+internal sealed class ConfirmSelectionAttributeTests
+{
+	[Test]
+	public void ConfirmSelectionAttribute___Success___ReturnsTrue()
+	{
+		// arrange
+		var searchQueryRequiredAttribute = new ConfirmSelectionAttribute();
+
+		// act
+		var result = searchQueryRequiredAttribute.IsValid(true);
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(true));
+	}
+
+	[Test]
+	public void ConfirmSelectionAttribute___Success___ReturnsFalse()
+	{
+		// arrange
+		var searchQueryRequiredAttribute = new ConfirmSelectionAttribute();
+
+		// act
+		var result = searchQueryRequiredAttribute.IsValid(false);
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(false));
+	}
+
+	[Test]
+	public void ConfirmSelectionAttribute___NonBoolean___ReturnsFalse()
+	{
+		// arrange
+		var searchQueryRequiredAttribute = new ConfirmSelectionAttribute();
+
+		// act
+		var result = searchQueryRequiredAttribute.IsValid(null);
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(false));
+	}
+}
---
+++ Dfe.Academies.External.Web.UnitTest/CustomValidators/SearchQueryRequiredAttributeTests.cs
@@ -0,0 +1,63 @@
+﻿using Dfe.Academies.External.Web.CustomValidators;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.CustomValidators;
+
+internal sealed class SearchQueryRequiredAttributeTests
+{
+    [Test]
+    public void SearchQueryRequiredAttribute___NullCheck___ReturnsFalse()
+    {
+	    // arrange
+	    var searchQueryRequiredAttribute = new SearchQueryRequiredAttribute();
+
+	    // act
+		var result = searchQueryRequiredAttribute.IsValid(null);
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+	    Assert.That(result, Is.EqualTo(false));
+    }
+	
+	[Test]
+	public void SearchQueryRequiredAttribute___EmptyCheck___ReturnsFalse()
+	{
+		// arrange
+		var searchQueryRequiredAttribute = new SearchQueryRequiredAttribute();
+
+		// act
+		var result = searchQueryRequiredAttribute.IsValid(string.Empty);
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(false));
+	}
+
+	[Test]
+	public void SearchQueryRequiredAttribute___LessThan4CharactersCheck___ReturnsFalse()
+	{
+		// arrange
+		var searchQueryRequiredAttribute = new SearchQueryRequiredAttribute();
+
+		// act
+		var result = searchQueryRequiredAttribute.IsValid("iso");
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(false));
+	}
+
+	[Test]
+	public void SearchQueryRequiredAttribute___MoreThan4CharactersCheck___ReturnsTrue()
+	{
+		// arrange
+		var searchQueryRequiredAttribute = new SearchQueryRequiredAttribute();
+
+		// act
+		var result = searchQueryRequiredAttribute.IsValid("isosceles");
+
+		// assert
+		Assert.That(searchQueryRequiredAttribute, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(true));
+	}
+}
