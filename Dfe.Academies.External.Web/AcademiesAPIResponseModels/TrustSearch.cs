e2359ff5f6b4d0489de57474c177a43f4bf3d1d4
humboldtsystems <humboldtsystems@gmail.com>
2022-07-26 17:51:53 +0100

    added object from concerns casework

---
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/TrustSearch.cs
@@ -0,0 +1,17 @@
+﻿using Dfe.Academies.External.Web.AcademiesAPIResponseModels.Base;
+
+namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels;
+
+/// <summary>
+/// Container object to pass trust search criteria to API layer :-
+/// {{api-host}}/V1/trusts?{BuildRequestUri(trustSearch)}
+/// </summary>
+public sealed class TrustSearch : PageSearch
+{
+	public string GroupName { get; }
+	public string Ukprn { get; }
+	public string CompaniesHouseNumber { get; }
+
+	public TrustSearch(string groupName, string ukprn, string companiesHouseNumber) => 
+		(GroupName, Ukprn, CompaniesHouseNumber) = (groupName, ukprn, companiesHouseNumber);
+}
