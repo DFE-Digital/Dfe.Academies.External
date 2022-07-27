3531b9cde4299b4160d84d683e75d072cc3b8622
humboldtsystems <humboldtsystems@gmail.com>
2022-07-27 10:33:11 +0100

    more tests

--- Dfe.Academies.External.Web.UnitTest/Services/ReferenceDataRetrievalServiceTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Services/ReferenceDataRetrievalServiceTests.cs
@@ -184,19 +184,50 @@ internal sealed class ReferenceDataRetrievalServiceTests
 
 		var mockLogger = new Mock<ILogger<ReferenceDataRetrievalService>>();
 
-		TrustSearch schoolSearch = new TrustSearch(name, ukprn.ToString(), "");
+		TrustSearch trustSearch = new TrustSearch(name, ukprn.ToString(), "");
 
 		// act
 		var referenceDataRetrievalService = new ReferenceDataRetrievalService(mockFactory.Object, mockLogger.Object);
-		var trustsByPagination = await referenceDataRetrievalService.GetTrustsByPagination(schoolSearch);
+		var trustsByPagination = await referenceDataRetrievalService.GetTrustsByPagination(trustSearch);
 
 		// assert
 		Assert.That(trustsByPagination, Is.Not.Null);
 		Assert.AreEqual(true, trustsByPagination.Data.Any());
 		Assert.AreEqual(2, trustsByPagination.Data.Count);
 	}
+	
+	//[Test]
+	public async Task GetTrustsByPagination___ApiReturns500___Failure()
+	{
+		// arrange
+		var expected = @"{ ""foo"": ""bar"" }"; // TODO MR:- will be json from Academies API
+		var mockFactory = new Mock<IHttpClientFactory>();
+		string name = "wise";
+		int ukprn = 587634;
 
-	// TODO MR:- GetTrustsByPagination() - failure
+		var mockMessageHandler = new Mock<HttpMessageHandler>();
+		mockMessageHandler.Protected()
+			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+			.ReturnsAsync(new HttpResponseMessage
+			{
+				StatusCode = HttpStatusCode.OK,
+				Content = new StringContent(expected)
+			});
+
+		var httpClient = new HttpClient(mockMessageHandler.Object);
+		httpClient.BaseAddress = new Uri(TestUrl);
+
+		mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+		var mockLogger = new Mock<ILogger<ReferenceDataRetrievalService>>();
+		TrustSearch trustSearch = new TrustSearch(name, ukprn.ToString(), "");
+
+		// act
+		var referenceDataRetrievalService = new ReferenceDataRetrievalService(mockFactory.Object, mockLogger.Object);
+
+		// assert
+		var ex = Assert.ThrowsAsync<HttpRequestException>(() => referenceDataRetrievalService.GetTrustsByPagination(trustSearch));
+	}
 
 	//[Test]
 	public async Task GetTrustByUkPrn___ApiReturns200___Success()
