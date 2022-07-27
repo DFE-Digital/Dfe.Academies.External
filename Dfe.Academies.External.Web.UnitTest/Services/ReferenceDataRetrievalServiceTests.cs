b263fc545d7268c9cb1941c4a177a105cd376208
humboldtsystems <humboldtsystems@gmail.com>
2022-07-27 16:48:56 +0100

    fixed failure tests

--- Dfe.Academies.External.Web.UnitTest/Services/ReferenceDataRetrievalServiceTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Services/ReferenceDataRetrievalServiceTests.cs
@@ -31,7 +31,7 @@ internal sealed class ReferenceDataRetrievalServiceTests
 		var mockFactory = new Mock<IHttpClientFactory>();
 	    string schoolName = "wise";
 		int urn = 587634;
-
+		
 		var mockMessageHandler = new Mock<HttpMessageHandler>();
 	    mockMessageHandler.Protected()
 		    .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
@@ -64,8 +64,6 @@ internal sealed class ReferenceDataRetrievalServiceTests
 	public async Task SearchSchools___ApiReturns500___Failure()
 	{
 		// arrange
-		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/schoolSearchResponse.json";
-		string expected = await File.ReadAllTextAsync(fullFilePath);
 		var mockFactory = new Mock<IHttpClientFactory>();
 		string schoolName = "wise";
 		int urn = 587634;
@@ -75,8 +73,7 @@ internal sealed class ReferenceDataRetrievalServiceTests
 			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
 			.ReturnsAsync(new HttpResponseMessage
 			{
-				StatusCode = HttpStatusCode.InternalServerError,
-				Content = new StringContent(expected)
+				StatusCode = HttpStatusCode.InternalServerError
 			});
 
 		var httpClient = new HttpClient(mockMessageHandler.Object);
@@ -133,8 +130,6 @@ internal sealed class ReferenceDataRetrievalServiceTests
 	public async Task GetSchool___ApiReturns500___Failure()
 	{
 		// arrange
-		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getSchoolResponse.json";
-		string expected = await File.ReadAllTextAsync(fullFilePath);
 		int urn = 101003; 
 		var mockFactory = new Mock<IHttpClientFactory>();
 
@@ -143,8 +138,7 @@ internal sealed class ReferenceDataRetrievalServiceTests
 			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
 			.ReturnsAsync(new HttpResponseMessage
 			{
-				StatusCode = HttpStatusCode.InternalServerError,
-				Content = new StringContent(expected)
+				StatusCode = HttpStatusCode.InternalServerError
 			});
 
 		var httpClient = new HttpClient(mockMessageHandler.Object);
@@ -161,16 +155,17 @@ internal sealed class ReferenceDataRetrievalServiceTests
 		var ex = Assert.ThrowsAsync<HttpRequestException>(() => referenceDataRetrievalService.GetSchool(urn));
 	}
 
-	//[Test]
-	public async Task GetTrustsByPagination___ApiReturns200___Success()
+	[Test]
+	public async Task GetTrusts___ApiReturns200___Success()
 	{
 		// arrange
 		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getTrustSearchResponse.json";
 		string expected = await File.ReadAllTextAsync(fullFilePath);
+		int expectedCount = 10;
 		var mockFactory = new Mock<IHttpClientFactory>();
 		string name = "wise";
 		int ukprn = 587634;
-
+		
 		var mockMessageHandler = new Mock<HttpMessageHandler>();
 		mockMessageHandler.Protected()
 			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
@@ -191,31 +186,28 @@ internal sealed class ReferenceDataRetrievalServiceTests
 
 		// act
 		var referenceDataRetrievalService = new ReferenceDataRetrievalService(mockFactory.Object, mockLogger.Object);
-		var trustsByPagination = await referenceDataRetrievalService.GetTrustsByPagination(trustSearch);
+		var trustsByPagination = await referenceDataRetrievalService.GetTrusts(trustSearch);
 
 		// assert
 		Assert.That(trustsByPagination, Is.Not.Null);
-		Assert.AreEqual(true, trustsByPagination.Data.Any());
-		Assert.AreEqual(2, trustsByPagination.Data.Count);
+		Assert.AreEqual(true, trustsByPagination.Any());
+		Assert.AreEqual(expectedCount, trustsByPagination.Count);
 	}
 	
-	//[Test]
-	public async Task GetTrustsByPagination___ApiReturns500___Failure()
+	[Test]
+	public async Task GetTrusts___ApiReturns500___Failure()
 	{
 		// arrange
-		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getTrustSearchResponse.json";
-		string expected = await File.ReadAllTextAsync(fullFilePath);
 		var mockFactory = new Mock<IHttpClientFactory>();
 		string name = "wise";
 		int ukprn = 587634;
-
+		
 		var mockMessageHandler = new Mock<HttpMessageHandler>();
 		mockMessageHandler.Protected()
 			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
 			.ReturnsAsync(new HttpResponseMessage
 			{
-				StatusCode = HttpStatusCode.OK,
-				Content = new StringContent(expected)
+				StatusCode = HttpStatusCode.InternalServerError
 			});
 
 		var httpClient = new HttpClient(mockMessageHandler.Object);
@@ -230,7 +222,7 @@ internal sealed class ReferenceDataRetrievalServiceTests
 		var referenceDataRetrievalService = new ReferenceDataRetrievalService(mockFactory.Object, mockLogger.Object);
 
 		// assert
-		var ex = Assert.ThrowsAsync<HttpRequestException>(() => referenceDataRetrievalService.GetTrustsByPagination(trustSearch));
+		var ex = Assert.ThrowsAsync<HttpRequestException>(() => referenceDataRetrievalService.GetTrusts(trustSearch));
 	}
 
 	//[Test]
@@ -271,8 +263,8 @@ internal sealed class ReferenceDataRetrievalServiceTests
 	public async Task GetTrustByUkPrn___ApiReturns500___Failure()
 	{
 		// arrange
-		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getTrustResponse.json";
-		string expected = await File.ReadAllTextAsync(fullFilePath);
+		//string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getTrustResponse.json";
+		//string expected = await File.ReadAllTextAsync(fullFilePath);
 		var mockFactory = new Mock<IHttpClientFactory>();
 		int ukprn = 587634;
 
@@ -281,8 +273,7 @@ internal sealed class ReferenceDataRetrievalServiceTests
 			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
 			.ReturnsAsync(new HttpResponseMessage
 			{
-				StatusCode = HttpStatusCode.OK,
-				Content = new StringContent(expected)
+				StatusCode = HttpStatusCode.InternalServerError
 			});
 
 		var httpClient = new HttpClient(mockMessageHandler.Object);
