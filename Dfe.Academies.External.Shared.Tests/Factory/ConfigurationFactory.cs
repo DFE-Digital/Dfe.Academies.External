3d4edca8923052b21dfdfd506b17b722c45c0928
humboldtsystems <humboldtsystems@gmail.com>
2022-07-27 10:32:55 +0100

    added shared project like concerns casework for helpers

---
+++ Dfe.Academies.External.Shared.Tests/Factory/ConfigurationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Dfe.Academies.External.Shared.Tests.Factory
+{
+	public static class ConfigurationFactory
+	{
+		public static IConfigurationBuilder ConfigurationInMemoryBuilder(this IConfigurationBuilder configurationBuilder, Dictionary<string, string> initialData)
+		{
+			return configurationBuilder.AddInMemoryCollection(initialData);
+		}
+		
+		public static IConfigurationBuilder ConfigurationUserSecretsBuilder(this IConfigurationBuilder configurationBuilder)
+		{
+			return configurationBuilder.AddUserSecrets<Program>();
+		}
+		
+		public static IConfigurationBuilder ConfigurationJsonFile(this IConfigurationBuilder configurationBuilder)
+		{
+			return configurationBuilder.AddJsonFile("appsettings.json");
+		}
+	}
+}
