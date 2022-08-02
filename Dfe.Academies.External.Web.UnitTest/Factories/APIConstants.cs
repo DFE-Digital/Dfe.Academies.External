304446e8abd845f3645c37a6405cacebddc34d8f
humboldtsystems <humboldtsystems@gmail.com>
2022-08-02 11:52:47 +0100

    test URI's needed for tests

---
+++ Dfe.Academies.External.Web.UnitTest/Factories/APIConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfe.Academies.External.Web.UnitTest.Factories;
+
+internal sealed class APIConstants
+{
+	public const string AcademiesAPITestUrl = "http://127.0.0.1:8000/";
+
+	public const string AcademisationAPITestUrl = "http://127.0.0.1:8000/";
+}
