2bcf47a6ecefd2626c3c1952b241b8c67045b93d
humboldtsystems <humboldtsystems@gmail.com>
2022-08-02 11:50:42 +0100

    trams API trust search models

---
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/Trusts/IfdDataDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels.Trusts;
+
+public sealed class IfdDataDto
+{
+	[JsonPropertyName("trustType")]
+	public string TrustType { get; }
+
+	[JsonPropertyName("trustContactPhoneNumber")]
+	public string TrustContactPhoneNumber { get; }
+
+	[JsonPropertyName("trustAddress")]
+	public GroupContactAddressDto GroupContactAddress { get; }
+	
+	[JsonConstructor]
+	public IfdDataDto(string trustType, string trustContactPhoneNumber, GroupContactAddressDto groupContactAddress) => 
+		(TrustType, TrustContactPhoneNumber, GroupContactAddress) = (trustType, trustContactPhoneNumber, groupContactAddress);
+}
