fc04a59d98cbd23eca64d8fc8f3033e7558c0d8b
humboldtsystems <humboldtsystems@gmail.com>
2022-08-02 11:50:49 +0100

    trams API trust search models

---
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/TrustSearch.cs
@@ -0,0 +1,17 @@
+﻿using Dfe.Academies.External.Web.AcademiesAPIResponseModels.Base;
+
+namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels;
+
+/// <summary>
+/// Container object to pass trust search criteria to API layer :-
+/// {{api-host}}/trusts?api-version=V1&groupName=grammar
+/// </summary>
+public sealed class TrustSearch : PageSearch
+{
+	public string GroupName { get; }
+	public string Ukprn { get; }
+	public string CompaniesHouseNumber { get; }
+
+	public TrustSearch(string groupName, string ukprn, string companiesHouseNumber) => 
+		(GroupName, Ukprn, CompaniesHouseNumber) = (groupName, ukprn, companiesHouseNumber);
+}
