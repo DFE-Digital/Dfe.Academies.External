73f3ebd4eaf1182979064b1324084953668bca81
humboldtsystems <humboldtsystems@gmail.com>
2022-08-02 11:33:13 +0100

    removed trust methods & plumbed in request provider

--- Dfe.Academies.External.Web/Services/ReferenceDataRetrievalService.cs
+++ Dfe.Academies.External.Web/Services/ReferenceDataRetrievalService.cs
@@ -1,77 +1,92 @@
 ﻿using Dfe.Academies.External.Web.AcademiesAPIResponseModels;
-using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.AcademiesAPIResponseModels.Schools;
 using Dfe.Academies.External.Web.ViewModels;
+using System.Web;
 
 namespace Dfe.Academies.External.Web.Services;
 
 public sealed class ReferenceDataRetrievalService : BaseService, IReferenceDataRetrievalService
 {
 	private readonly ILogger<ReferenceDataRetrievalService> _logger;
-	private readonly IHttpClientFactory _httpClientFactory;
+	private readonly HttpClient _httpClient;
 	private readonly ResilientRequestProvider _resilientRequestProvider;
 
 	public ReferenceDataRetrievalService(IHttpClientFactory httpClientFactory, ILogger<ReferenceDataRetrievalService> logger) : base(httpClientFactory)
 	{
-		_httpClientFactory = httpClientFactory;
 		_logger = logger;
-		_resilientRequestProvider = new ResilientRequestProvider(httpClientFactory.CreateClient(HttpClientName));
+		_httpClient = httpClientFactory.CreateClient(AcademiesAPIHttpClientName);
+		_resilientRequestProvider = new ResilientRequestProvider(_httpClient);
 	}
 
+	///<inheritdoc/>
 	public async Task<IList<SchoolSearchResultViewModel>> SearchSchools(SchoolSearch schoolSearch)
 	{
-		IList<SchoolSearchResultViewModel> schools = new List<SchoolSearchResultViewModel>();
-
-		// TODO: Get data from Academisation API
-		//// _resilientRequestProvider.Get();
-		/// API returns list<SchoolsSearchDto>
-
-		// **** Mock Demo Data - as per Figma - to be removed ! ****
-		IList<SchoolsSearchDto> schoolsSearchDtos = new List<SchoolsSearchDto>();
-		schoolsSearchDtos.Add(new SchoolsSearchDto("Wise Owl primary school", 587634 , "21 test road", "sheffield", "S1 2JF"));
-		schoolsSearchDtos.Add(new SchoolsSearchDto("Wise Owl secondary school", 368489, "21 test road", "sheffield", "S1 2JF"));
-
-		// do a bit of manual linqage
-		IEnumerable<SchoolsSearchDto> schoolsSearchResults = new List<SchoolsSearchDto>();
-		if (!string.IsNullOrWhiteSpace(schoolSearch.SchoolName))
+		try
 		{
-			schoolsSearchResults =
-				schoolsSearchDtos.Where(s => s.SchoolName.ToLower().Trim().Contains(schoolSearch.SchoolName) 
-														|| s.SchoolName.ToLower().Trim().EndsWith(schoolSearch.SchoolName)).ToList();
+			//{{api-host}}/establishments?api-version=V1&Urn=101934&ukprn=10006563&Name=wise
+			string apiurl = $"{_httpClient.BaseAddress}/establishments?{BuildSchoolSearchRequestUri(schoolSearch, "V1")}";
+
+			IList<SchoolSearchResultViewModel> schools = new List<SchoolSearchResultViewModel>();
+
+			//// API returns list<SchoolsSearchDto>
+			var schoolsSearchResults = await _resilientRequestProvider.GetAsync<List<SchoolsSearchDto>>(apiurl);
+
+			// convert SchoolsSearchDto -> view model
+			if (schoolsSearchResults.Any())
+				schools = schoolsSearchResults.Select(c =>
+					new SchoolSearchResultViewModel(name: c.Name, urn: int.Parse(c.Urn), ukprn: c.Ukprn)).ToList();
+
+			return schools;
 		}
-		else if (!string.IsNullOrWhiteSpace(schoolSearch.Urn) && !schoolsSearchResults.Any())
+		catch (Exception ex)
 		{
-			schoolsSearchResults =
-				schoolsSearchDtos.Where(s => s.Urn == int.Parse(schoolSearch.Urn.ToLower().Trim())).ToList();
+			_logger.LogError("ReferenceDataRetrievalService::SearchSchools::Exception - {Message}", ex.Message);
+			throw;
 		}
-
-		// Map SchoolsSearchDto to view model
-		if (schoolsSearchResults.Any())
-			schools = schoolsSearchResults.Select(c =>
-				new SchoolSearchResultViewModel(schoolName: c.SchoolName, urn: c.Urn, street: c.Street, town: c.Town, fullUkPostcode: c.FullUkPostcode)
-				{
-					// TODO MR:- others?? depends what we get back from API
-				}).ToList();
-
-		return schools;
 	}
 
+	///<inheritdoc/>
 	public async Task<EstablishmentResponse> GetSchool(int urn)
 	{
-		// TODO: Get data from Academisation API
-		// _resilientRequestProvider.Get
+		try
+		{
+			// {{api-host}}/establishment/urn/101934?api-version=V1
+			string apiurl = $"{_httpClient.BaseAddress}/establishment/urn/{urn}?api-version=V1";
 
-		// **** Mock Demo Data - as per Figma ****
-		if (urn == 587634)
-		{
-			return new(name: "Wise owl primary school", urn: urn, ukprn: "GAT00123", "94 Forest Road", "Manchester", "MC4 3TR");
+			//// API returns EstablishmentResponse
+			var APIresult = await _resilientRequestProvider.GetAsync<EstablishmentResponse>(apiurl);
+
+			return APIresult;
 		}
-		else if (urn == 368489)
+		catch (Exception ex)
 		{
-			return new(name: "Wise owl secondary school", urn: urn, ukprn: "GAT00124", "99 Forest Road", "Manchester", "MC4 3TR");
+			_logger.LogError("ReferenceDataRetrievalService::GetSchool::Exception - {Message}", ex.Message);
+			throw;
 		}
-		else
+	}
+
+	//// Public method, so can write unit tests !!!!
+	public string BuildSchoolSearchRequestUri(SchoolSearch schoolSearch, string apiVersionNumber)
+	{
+		var queryParams = HttpUtility.ParseQueryString(string.Empty);
+
+		if (!string.IsNullOrEmpty(schoolSearch.Name))
 		{
-			return new(name: "Chesterton primary school", urn: 101003, ukprn: null, "94 Forest Road", "stoke-on-trent", "ST4 3TR");
+			queryParams.Add("name", schoolSearch.Name);
 		}
+
+		if (!string.IsNullOrEmpty(schoolSearch.Urn))
+		{
+			queryParams.Add("Urn", schoolSearch.Urn);
+		}
+
+		if (!string.IsNullOrEmpty(schoolSearch.Ukprn))
+		{
+			queryParams.Add("ukprn", schoolSearch.Ukprn);
+		}
+
+		queryParams.Add("api-version", apiVersionNumber);
+
+		return HttpUtility.UrlEncode(queryParams.ToString());
 	}
 }
