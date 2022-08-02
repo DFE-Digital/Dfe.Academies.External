08d1f2af1d7b85a157fa5a5949f9d1a5652b4bf8
humboldtsystems <humboldtsystems@gmail.com>
2022-08-02 11:29:58 +0100

    moved response definition & amended defintition

---
+++ Dfe.Academies.External.Web/AcademiesAPIResponseModels/Schools/EstablishmentResponse.cs
@@ -0,0 +1,43 @@
+using System.Text.Json.Serialization;
+
+namespace Dfe.Academies.External.Web.AcademiesAPIResponseModels.Schools;
+
+public class EstablishmentResponse
+{
+	/// <summary>
+	/// System.Text de-serialization requires this !!!
+	/// </summary>
+	public EstablishmentResponse()
+	{
+		Address = new AddressResponse();
+	}
+
+	public EstablishmentResponse(string name, string urn, string ukprn, string street, string town, string fullUkPostcode)
+	{
+		Urn = urn;
+		Name = name;
+		Ukprn = ukprn;
+		Address = new AddressResponse(street:street, town:town, fullUkPostcode: fullUkPostcode);
+	}
+
+	/// <summary>
+	/// This is ONLY a school identifier
+	/// </summary>
+	[JsonPropertyName("urn")]
+	public string Urn { get; set; }
+
+	[JsonPropertyName("establishmentNumber")]
+	public string Number { get; set; }
+
+	[JsonPropertyName("establishmentName")]
+	public string Name { get; set; }
+
+	[JsonInclude]
+	public AddressResponse Address { get; set; }
+
+	[JsonPropertyName("uprn")]
+	public string UPRN { get; set; }
+
+	[JsonPropertyName("ukprn")]
+	public string Ukprn { get; set; }
+}
