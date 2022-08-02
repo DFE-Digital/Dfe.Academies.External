8e0adde807ef21d21a378b44cb0f0845e93b6403
humboldtsystems <humboldtsystems@gmail.com>
2022-08-02 12:20:19 +0100

    new controller for behind trust search JavaScript

---
+++ Dfe.Academies.External.Web/Controllers/TrustController.cs
@@ -0,0 +1,89 @@
+﻿using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Dfe.Academies.External.Web.Controllers
+{
+	// TODO MR:- baseController
+	// [Authorize]
+	public class TrustController : Controller
+	{
+		private const int SearchQueryMinLength = 3;
+		private readonly ILogger<TrustController> _logger;
+		private readonly IReferenceDataRetrievalService _referenceDataRetrievalService;
+
+		public TrustController(ILogger<TrustController> logger,
+			IReferenceDataRetrievalService referenceDataRetrievalService)
+		{
+			_logger = logger;
+			_referenceDataRetrievalService = referenceDataRetrievalService;
+		}
+
+		[HttpGet]
+		[Route("trust/search")]
+		[Route("trust/trust/search")]
+		public async Task<IEnumerable<string>> Search(string searchQuery)
+		{
+			try
+			{
+				_logger.LogInformation("TrustController::Search");
+
+				// Double check search query.
+				if (string.IsNullOrEmpty(searchQuery) || searchQuery.Length < SearchQueryMinLength)
+				{
+					return Enumerable.Empty<string>();
+				}
+
+				var schoolSearch = new TrustSearch(searchQuery, searchQuery, searchQuery);
+				var schoolSearchResponse = await _referenceDataRetrievalService.SearchTrusts(schoolSearch);
+
+				if (schoolSearchResponse.Any())
+				{
+					return schoolSearchResponse.Select(x => x.DisplayName).AsEnumerable();
+				}
+				else
+				{
+					return Enumerable.Empty<string>();
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("TrustController::Search::Exception - {Message}", ex.Message);
+
+				return Enumerable.Empty<string>();
+			}
+		}
+
+		[HttpGet]
+		[Route("trust/ReturnTrustDetailsPartialViewPopulated")]
+		[Route("trust/trust/ReturnSchoolDetailsPartialViewPopulated")]
+		public async Task<IActionResult> ReturnTrustDetailsPartialViewPopulated(string selectedTrust)
+		{
+			try
+			{
+				// Remove whitespace and trailing ) then split removing empty entries
+				var schoolSplit = selectedTrust
+					.Trim()
+					.Replace(")", string.Empty)
+					.Split('(', StringSplitOptions.RemoveEmptyEntries);
+
+				int ukprn = Convert.ToInt32(schoolSplit[^1]);
+				var result = await _referenceDataRetrievalService.GetTrust(ukprn);
+
+				var vm = new TrustDetailsViewModel(trustName: result.Name,
+					ukprn: ukprn,
+					street: result.Address.Street,
+					town: result.Address.Town,
+					fullUkPostcode: result.Address.Postcode);
+
+				return PartialView("_TrustDetails", vm);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("SchoolController::ReturnTrustDetailsPartialViewPopulated::Exception - {Message}", ex.Message);
+				return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+			}
+		}
+	}
+}
