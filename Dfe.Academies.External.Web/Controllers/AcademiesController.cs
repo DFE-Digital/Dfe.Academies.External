7c1596ff3f264ec77b3deb2cc6537e6d9da805fb
humboldtsystems <humboldtsystems@gmail.com>
2022-08-03 17:21:17 +0100

    add error handling controller / method

---
+++ Dfe.Academies.External.Web/Controllers/AcademiesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Controllers
+{
+	public class AcademiesController : Controller
+	{
+		public IActionResult Error()
+		{
+			// TODO MR:- return a generic error page / view
+			return View();
+		}
+	}
+}
