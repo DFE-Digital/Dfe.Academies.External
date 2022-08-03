ae0714aa1ece53dd4b449f95ad89dbf190dd7257
humboldtsystems <humboldtsystems@gmail.com>
2022-08-03 22:53:42 +0100

    another passing urn issue

--- Dfe.Academies.External.Web/Pages/Base/BasePageEditModel.cs
+++ Dfe.Academies.External.Web/Pages/Base/BasePageEditModel.cs
@@ -37,12 +37,12 @@ public abstract class BasePageEditModel : BasePageModel
 
 		if (schoolDetails != null)
 		{
-			SchoolCacheValuesViewModel cachedValuesViewModel = new(int.Parse(schoolDetails.Urn), schoolDetails.Name);
+			SchoolCacheValuesViewModel cachedValuesViewModel = new(urn, schoolDetails.Name);
 
 			ViewDataHelper.StoreSerialisedValue(nameof(SchoolCacheValuesViewModel), ViewData, cachedValuesViewModel);
 		}
 
-		return new SchoolApplyingToConvert(schoolDetails.Name, int.Parse(schoolDetails.Urn), applicationId, schoolDetails.UPRN);
+		return new SchoolApplyingToConvert(schoolDetails.Name, urn, applicationId, schoolDetails.UPRN);
 	}
 
 	protected string SetSchoolApplicationComponentUriFromName(string componentName)
