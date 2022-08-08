25077062aa6b77869d626543e426c7371f9973b2
humboldtsystems <humboldtsystems@gmail.com>
2022-08-08 17:09:47 +0100

    adding props as per A2c-sip

--- Dfe.Academies.External.Web/ViewModels/SchoolConversionComponentSectionViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/SchoolConversionComponentSectionViewModel.cs
@@ -3,19 +3,39 @@
 public sealed class SchoolConversionComponentSectionViewModel
 {
 	public const string NoInfoAnswer = "You have not added any information";
+	// section 1 - school joining trust
+	public const string NameOfSchoolSectionName = "The name of the school";
+	// section 2 - contact details section
+	public const string ContactDetailsHeadteacherNameSectionName = "Name of headteacher";
+	public const string ContactDetailsHeadteacherEmailSectionName = "Headteacher's email address";
+	public const string ContactDetailsHeadteacherTelNoSectionName = "Headteacher's telephone number";
+	public const string ContactDetailsChairNameSectionName = "Name of the chair of the governing body";
+	public const string ContactDetailsChairEmailSectionName = "Chair's email address";
+	public const string ContactDetailsChairTelNoSectionName = "Chair's telephone number";
+	public const string ContactDetailsMainContactWhomSectionName = "Who is the main contact for the conversion?";
+	public const string ContactDetailsApproversFullNameSectionName = "Approver's full name";
+	public const string ContactDetailsApproversEmailSectionName = "Approver's email address";
+	// section 3 - conversion date section
+	public const string ApplicationConversionTargetDateSectionName = "Do you want the conversion to happen on a particular date?";
+	// section 4 - reasons for joining trust section
+	public const string ReasonsForJoiningTrustSectionName = "Why does the school want to join this trust in particular?";
+	// section 5 - school changing name section
+	public const string NameOfSchoolChangingSectionName = "Is the school planning to change its name when it becomes an academy?";
 
-	public SchoolConversionComponentSectionViewModel(string answer)
+	public SchoolConversionComponentSectionViewModel(string name, string answer)
 	{
+		Name = name;
 		Answer = answer;
 	}
 
-	// Question = "Name",
+	/// <summary>
+	/// Name of section e.g. 'name of headteacher'
+	/// </summary>
+	public string Name { get; set; }
+
 	//// MR:- below sets text to public const string NoInfo = "You have not added any information";
-	
-	// Answer = ((string) ViewData[FieldConstants.SipSchoolConversionMainContactOtherName]).DisplayNoInfoIfNullOrEmpty(),
+	//// Answer = ((string) ViewData[FieldConstants.SipSchoolConversionMainContactOtherName]).DisplayNoInfoIfNullOrEmpty(),
 	public string Answer { get; set; }
 
 	// FieldName = FieldConstants.SipSchoolConversionMainContactOtherName // MR:- ??
-
-	// TODO MR:- need a URI similar to ApplicationComponentViewModel - to render change link
 }
