d8ccdc7930b8ce61373f4f391f90e0a276ed7b0b
humboldtsystems <humboldtsystems@gmail.com>
2022-08-08 18:13:17 +0100

    stub test classes for view models

---
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/SchoolConversionComponentSectionViewModelTests.cs
@@ -0,0 +1,11 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class SchoolConversionComponentSectionViewModelTests
+{
+	// TODO MR:-
+}
