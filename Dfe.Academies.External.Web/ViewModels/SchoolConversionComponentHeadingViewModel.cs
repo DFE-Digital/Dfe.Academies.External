323823f53bb9246bac386e1b8ccdf3ba68653f8d
humboldtsystems <humboldtsystems@gmail.com>
2022-08-08 17:09:47 +0100

    adding props as per A2c-sip

--- Dfe.Academies.External.Web/ViewModels/SchoolConversionComponentHeadingViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/SchoolConversionComponentHeadingViewModel.cs
@@ -4,21 +4,35 @@ namespace Dfe.Academies.External.Web.ViewModels;
 
 public sealed class SchoolConversionComponentHeadingViewModel
 {
-	public SchoolConversionComponentHeadingViewModel()
+	public const string HeadingApplicationSchool = "The school joining the trust";
+	public const string HeadingApplicationContactDetails = "Contact details";
+	public const string HeadingApplicationPreferredDateForConversion = "Date for conversion";
+	public const string HeadingApplicationJoinTrustReason = "Reasons for joining";
+	public const string HeadingApplicationSchoolNameChange = "Changing the name of the school";
+
+	public SchoolConversionComponentHeadingViewModel(string title, string uRi)
 	{
+		Title = title;
 		Sections = new();
+		URI = uRi;
 	}
 
-	// Question = "The name of the school", // MR:- this will change dependent on whether 'formamat' / 'joinamat'
+	/// <summary>
+	/// e.g. Title = "The school joining the trust"
+	/// </summary>
+	public string Title { get; set; }
+	
 	// Answer = ((string) ViewData[FieldConstants.SchoolName]).DisplayNoInfoIfNullOrEmpty(),
-	// FieldName = FieldConstants.SchoolName, // TODO MR:- sort out some constants. Drop in here !
 
-	// Status = (Enums.SchoolConversionComponentStatus) ViewData[$"status-{reviewSectionMainContact.ChangeReference}"];
+	//// Status = (Enums.SchoolConversionComponentStatus) ViewData[$"status-{reviewSectionMainContact.ChangeReference}"];
 
 	/// <summary>
 	/// MR:- this is overall status i.e. if headteacher && chair && main contact not filled in !!
 	/// </summary>
 	public SchoolConversionComponentStatus Status { get; set; }
+
+	// MR:- need a URI similar to ApplicationComponentViewModel - to render 'start section' button
+	public string URI { get; set; }
 
 	public List<SchoolConversionComponentSectionViewModel> Sections { get; set; }
 }
