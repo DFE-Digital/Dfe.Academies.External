6b98bbc1b012b48040f49a0bc72f17d0499c87d5
humboldtsystems <humboldtsystems@gmail.com>
2022-08-09 09:50:20 +0100

    shell of new page including test class

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/ApplicationConversionTargetDateModelTests.cs
@@ -0,0 +1,42 @@
+﻿using Dfe.Academies.External.Web.Pages.School;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages.School
+{
+	internal sealed class ApplicationConversionTargetDateModelTests
+	{
+		// TODO MR:- OnGet
+
+		// TODO MR:- OnPostAsync___ModelIsValid___InValid
+
+		// TODO MR:- OnPostAsync___ModelIsValid___Valid
+
+		private static ApplicationConversionTargetDateModel SetupApplicationConversionTargetDateModel(
+			ILogger<ApplicationConversionTargetDateModel> mockLogger,
+			IConversionApplicationCreationService mockConversionApplicationCreationService,
+			IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+			IReferenceDataRetrievalService mockReferenceDataRetrievalService,
+			bool isAuthenticated = false)
+		{
+			(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+			return new ApplicationConversionTargetDateModel(mockLogger, mockConversionApplicationRetrievalService,
+				mockReferenceDataRetrievalService, mockConversionApplicationCreationService)
+			{
+				PageContext = pageContext,
+				TempData = tempData,
+				Url = new UrlHelper(actionContext),
+				MetadataProvider = pageContext.ViewData.ModelMetadata
+			};
+		}
+	}
+}
---
+++ Dfe.Academies.External.Web/Pages/School/ApplicationConversionTargetDate.cshtml.cs
@@ -0,0 +1,111 @@
+using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages.School
+{
+    public class ApplicationConversionTargetDateModel : BasePageEditModel
+    {
+	    private readonly ILogger<ApplicationConversionTargetDateModel> _logger;
+	    private readonly IConversionApplicationCreationService _academisationCreationService;
+	    private const string NextStepPage = "/SchoolOverview";
+
+	    //// MR:- selected school props for UI rendering
+	    [BindProperty]
+	    public int ApplicationId { get; set; }
+
+	    [BindProperty]
+	    public int Urn { get; private set; }
+
+	    public string SchoolName { get; private set; } = string.Empty;
+
+		//// MR:- VM props to capture data
+
+		public ApplicationConversionTargetDateModel(ILogger<ApplicationConversionTargetDateModel> logger,
+			IConversionApplicationRetrievalService conversionApplicationRetrievalService,
+			IReferenceDataRetrievalService referenceDataRetrievalService,
+			IConversionApplicationCreationService academisationCreationService)
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+		{
+			_logger = logger;
+			_academisationCreationService = academisationCreationService;
+		}
+
+		public async Task OnGetAsync(int urn, int appId)
+		{
+			try
+			{
+				LoadAndStoreCachedConversionApplication();
+
+				var selectedSchool = await LoadAndSetSchoolDetails(appId, urn);
+
+				// Grab other values from API
+				if (selectedSchool != null)
+				{
+					// TODO MR:- grab existing data from API endpoint to populate VM - applicationId && SchoolId combination !
+
+
+					PopulateUiModel(selectedSchool);
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("School::ApplicationConversionTargetDateModel::OnGetAsync::Exception - {Message}", ex.Message);
+			}
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				// error messages component consumes ViewData["Errors"]
+				PopulateValidationMessages();
+				return Page();
+			}
+
+			try
+			{
+				//// grab draft application from temp= null
+				var draftConversionApplication = TempDataHelper.GetSerialisedValue<ConversionApplication>(TempDataHelper.DraftConversionApplicationKey, TempData) ?? new ConversionApplication();
+
+				// TODO MR:- call API endpoint to log data
+				//await _academisationCreationService.UpdateSchoolConversionDate(draftConversionApplication);
+
+				// update temp store for next step - application overview
+				TempDataHelper.StoreSerialisedValue(TempDataHelper.DraftConversionApplicationKey, TempData, draftConversionApplication);
+
+				return RedirectToPage(NextStepPage);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("School::ApplicationConversionTargetDateModel::OnPostAsync::Exception - {Message}", ex.Message);
+				return Page();
+			}
+		}
+
+		public override void PopulateValidationMessages()
+		{
+			ViewData["Errors"] = ConvertModelStateToDictionary();
+
+			if (!ModelState.IsValid)
+			{
+				foreach (var modelStateError in ConvertModelStateToDictionary())
+				{
+					// MR:- add friendly message for validation summary
+					if (!this.ValidationErrorMessagesViewModel.ValidationErrorMessages.ContainsKey(modelStateError.Key))
+					{
+						this.ValidationErrorMessagesViewModel.ValidationErrorMessages.Add(modelStateError.Key, modelStateError.Value);
+					}
+				}
+			}
+		}
+
+		private void PopulateUiModel(SchoolApplyingToConvert selectedSchool)
+		{
+			ApplicationId = selectedSchool.ApplicationId;
+			Urn = selectedSchool.URN;
+			SchoolName = selectedSchool.SchoolName;
+		}
+	}
+}
