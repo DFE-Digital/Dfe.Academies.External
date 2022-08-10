fe0842942264d8daea36edd014c98bf9b16292a7
humboldtsystems <humboldtsystems@gmail.com>
2022-08-10 10:55:32 +0100

    amended to use CultureInfo.DefaultThreadCurrentCulture

--- Dfe.Academies.External.Web/Helpers/DateTimeMultiFormatParser.cs
+++ Dfe.Academies.External.Web/Helpers/DateTimeMultiFormatParser.cs
@@ -5,7 +5,7 @@ public static class DateTimeMultiFormatParser
 {
 	public static bool TryParse(string input, out DateTime date) =>
 		DateTime.TryParse(input, null, DateTimeStyles.RoundtripKind, out date) ||
-		DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date) ||
-		DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date) ||
-		DateTime.TryParseExact(input, "dd-MM-yyyyThh:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+		DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.None, out date) ||
+		DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.None, out date) ||
+		DateTime.TryParseExact(input, "dd-MM-yyyyThh:mm:ss", CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.None, out date);
 }
