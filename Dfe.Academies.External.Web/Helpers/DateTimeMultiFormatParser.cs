043aee8cd477e389324c783c6d31ef9619eff348
humboldtsystems <humboldtsystems@gmail.com>
2022-08-10 10:58:26 +0100

    another culture mod

--- Dfe.Academies.External.Web/Helpers/DateTimeMultiFormatParser.cs
+++ Dfe.Academies.External.Web/Helpers/DateTimeMultiFormatParser.cs
@@ -4,7 +4,7 @@ namespace Dfe.Academies.External.Web.Helpers;
 public static class DateTimeMultiFormatParser
 {
 	public static bool TryParse(string input, out DateTime date) =>
-		DateTime.TryParse(input, null, DateTimeStyles.RoundtripKind, out date) ||
+		DateTime.TryParse(input, CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.RoundtripKind, out date) ||
 		DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.None, out date) ||
 		DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.None, out date) ||
 		DateTime.TryParseExact(input, "dd-MM-yyyyThh:mm:ss", CultureInfo.DefaultThreadCurrentCulture, DateTimeStyles.None, out date);
