5e9c105a966eaea2dfb8179cf9ab78d0369633d6
humboldtsystems <humboldtsystems@gmail.com>
2022-08-11 14:27:09 +0100

    new view models for pupil number summary page & tests

---
+++ Dfe.Academies.External.Web.UnitTest/ViewModels/SchoolPupilNumbersSummaryHeadingViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.ViewModels;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.ViewModels;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class SchoolPupilNumbersSummaryHeadingViewModelTests
+{
+	private static readonly Fixture Fixture = new();
+
+	[Test]
+	public void Constructor___PropertiesSet()
+	{
+		// arrange
+		string title = Fixture.Create<string>();
+		string uri = Fixture.Create<string>();
+
+		var numbersSummaryHeadingViewModel = new SchoolPupilNumbersSummaryHeadingViewModel(title, uri);
+
+		// act
+		// nothing!
+
+		// assert
+		Assert.That(numbersSummaryHeadingViewModel, Is.Not.Null);
+		Assert.That(numbersSummaryHeadingViewModel.Title, Is.EqualTo(title));
+		Assert.That(numbersSummaryHeadingViewModel.URI, Is.EqualTo(uri));
+		Assert.That(numbersSummaryHeadingViewModel.Status, Is.EqualTo(SchoolConversionComponentStatus.NotStarted));
+	}
+}
---
+++ Dfe.Academies.External.Web/ViewModels/SchoolPupilNumbersSummarySectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels;
+
+public class SchoolPupilNumbersSummarySectionViewModel : SchoolQuestionAndAnswerViewModel
+{
+	public const string PupilNumberYr1 = "Projected pupil numbers on roll in the year the academy opens (year 1)";
+	public const string PupilNumberYr2 = "Projected pupil numbers on roll in the following year after the academy has opened (year 2)";
+	public const string PupilNumberYr3 = "Projected pupil numbers on roll in the following year (year 3)";
+	public const string NumbersBasedUpon = "What do you base these projected numbers on?";
+	public const string PAN = "What is the school's published admissions number (PAN)?";
+
+	public SchoolPupilNumbersSummarySectionViewModel(string name, string answer) : base(name, answer)
+	{
+	}
+}
