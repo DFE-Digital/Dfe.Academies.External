db2c1d93455c8a2a2fa1e7f79abbc61c0d047d5e
humboldtsystems <humboldtsystems@gmail.com>
2022-08-12 14:36:46 +0100

    fixes after service method changes

--- Dfe.Academies.External.Web/Pages/School/ApplicationChangeSchoolName.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/School/ApplicationChangeSchoolName.cshtml.cs
@@ -95,7 +95,7 @@ namespace Dfe.Academies.External.Web.Pages.School
 				var draftConversionApplication = TempDataHelper.GetSerialisedValue<ConversionApplication>(TempDataHelper.DraftConversionApplicationKey, TempData) ?? new ConversionApplication();
 
 				// MR:- save away ApplicationJoinTrustReason
-				await _academisationCreationService.ApplicationChangeSchoolNameAndReason(draftConversionApplication, ChangeName, ChangeSchoolNameReason);
+				await _academisationCreationService.ApplicationChangeSchoolNameAndReason(draftConversionApplication, ChangeName, ChangeSchoolNameReason, Urn);
 
 				// update temp store for next step - application overview as last step in process
 				TempDataHelper.StoreSerialisedValue(TempDataHelper.DraftConversionApplicationKey, TempData, draftConversionApplication);
