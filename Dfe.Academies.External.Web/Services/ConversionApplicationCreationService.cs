403d0bc7d4307a0eef32fee1c29688fc764d5c56
humboldtsystems <humboldtsystems@gmail.com>
2022-08-12 15:13:33 +0100

    xml comments

--- Dfe.Academies.External.Web/Services/ConversionApplicationCreationService.cs
+++ Dfe.Academies.External.Web/Services/ConversionApplicationCreationService.cs
@@ -16,32 +16,51 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 		_resilientRequestProvider = new ResilientRequestProvider(_httpClient);
 	}
 
+	///<inheritdoc/>
 	public async Task<ConversionApplication> CreateNewApplication(ConversionApplication application)
     {
-		//https://academies-academisation-api-dev.azurewebsites.net/application/99
-		string apiurl = $"{_httpClient.BaseAddress}/application/?api-version=V1";
+	    try
+	    {
+		    //https://academies-academisation-api-dev.azurewebsites.net/application/99
+		    string apiurl = $"{_httpClient.BaseAddress}/application/?api-version=V1";
 
-		// TODO: wire up Academisation API
-		// var result = await _resilientRequestProvider.PostAsync<ConversionApplicationApiPostResult, ConversionApplication>(apiurl, application);
-		application.ApplicationId = int.MaxValue; // result.ApplicationId;
+		    // TODO: wire up Academisation API
+		    // var result = await _resilientRequestProvider.PostAsync<ConversionApplicationApiPostResult, ConversionApplication>(apiurl, application);
+		    application.ApplicationId = int.MaxValue; // result.ApplicationId;
 
-        return application;
+		    return application;
+		}
+	    catch (Exception ex)
+	    {
+		    _logger.LogError("ConversionApplicationCreationService::CreateNewApplication::Exception - {Message}", ex.Message);
+		    throw;
+		}
     }
 
-    public async Task UpdateDraftApplication(ConversionApplication application)
+	///<inheritdoc/>
+	public async Task UpdateDraftApplication(ConversionApplication application)
     {
-		// MR:- may need to call GetApplication() first within ConversionApplicationRetrievalService()
-		// to grab current application data
-		// before then patching ConversionApplication returned with data from application object
+	    try
+	    {
+		    // MR:- may need to call GetApplication() first within ConversionApplicationRetrievalService()
+		    // to grab current application data
+		    // before then patching ConversionApplication returned with data from application object
 
-		//https://academies-academisation-api-dev.azurewebsites.net/application/99
-		string apiurl = $"{_httpClient.BaseAddress}/application/{application.ApplicationId}?api-version=V1";
+		    //https://academies-academisation-api-dev.azurewebsites.net/application/99
+		    string apiurl = $"{_httpClient.BaseAddress}/application/{application.ApplicationId}?api-version=V1";
 
-		// TODO: wire up Academisation API / what object does a PUT return
-		// var result = await _resilientRequestProvider.PutAsync<ConversionApplicationApiPostResult, ConversionApplication>(apiurl, application);
-    }
+		    // TODO: wire up Academisation API / what object does a PUT return
+		    // var result = await _resilientRequestProvider.PutAsync<ConversionApplicationApiPostResult, ConversionApplication>(apiurl, application);
+	    }
+	    catch (Exception ex)
+	    {
+		    _logger.LogError("ConversionApplicationCreationService::AddSchoolToApplication::Exception - {Message}", ex.Message);
+		    throw;
+	    }
+	}
 
-    public async Task AddSchoolToApplication(int applicationId, int schoolUrn, string name)
+	///<inheritdoc/>
+	public async Task AddSchoolToApplication(int applicationId, int schoolUrn, string name)
     {
 	    try
 	    {
@@ -66,7 +85,8 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 	    }
     }
 
-    public async Task ApplicationAddJoinTrustReasons(int applicationId, string applicationJoinTrustReason, int schoolUrn)
+	///<inheritdoc/>
+	public async Task ApplicationAddJoinTrustReasons(int applicationId, string applicationJoinTrustReason, int schoolUrn)
     {
 	    try
 	    {
@@ -91,7 +111,8 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 	    }
     }
 
-    public async Task AddTrustToApplication(int applicationId, int trustUkPrn, string name)
+	///<inheritdoc/>
+	public async Task AddTrustToApplication(int applicationId, int trustUkPrn, string name)
     {
 	    try
 	    {
@@ -115,7 +136,8 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 	    }
     }
 
-    public async Task ApplicationChangeSchoolNameAndReason(ConversionApplication application, SelectOption changeName,
+	///<inheritdoc/>
+	public async Task ApplicationChangeSchoolNameAndReason(ConversionApplication application, SelectOption changeName,
 	    string changeSchoolNameReason, int schoolUrn)
     {
 	    try
@@ -139,7 +161,8 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 	    }
     }
 
-    public async Task ApplicationSchoolTargetConversionDate(int applicationId, int schoolUrn, DateTime targetDate,
+	///<inheritdoc/>
+	public async Task ApplicationSchoolTargetConversionDate(int applicationId, int schoolUrn, DateTime targetDate,
 	    string targetDateExplained)
     {
 	    try
@@ -163,7 +186,8 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 	    }
 	}
 
-    public async Task ApplicationSchoolFuturePupilNumbers(int applicationId, int schoolUrn, int projectedPupilNumbersYear1,
+	///<inheritdoc/>
+	public async Task ApplicationSchoolFuturePupilNumbers(int applicationId, int schoolUrn, int projectedPupilNumbersYear1,
 	    int projectedPupilNumbersYear2, int projectedPupilNumbersYear3, string schoolCapacityAssumptions,
 	    int schoolCapacityPublishedAdmissionsNumber)
     {
@@ -191,7 +215,8 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 	    }
 	}
 
-    public async Task ApplicationSchoolContacts(ApplicationSchoolContacts schoolContacts)
+	///<inheritdoc/>
+	public async Task ApplicationSchoolContacts(ApplicationSchoolContacts schoolContacts)
     {
 	    try
 	    {
