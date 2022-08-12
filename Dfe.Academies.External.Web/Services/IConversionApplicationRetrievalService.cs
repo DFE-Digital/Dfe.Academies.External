8f3413f17aa388512753bb2e1b0c87b063abc03b
humboldtsystems <humboldtsystems@gmail.com>
2022-08-12 11:43:57 +0100

    amended methods to have new params & general code cleanup

--- Dfe.Academies.External.Web/Services/IConversionApplicationRetrievalService.cs
+++ Dfe.Academies.External.Web/Services/IConversionApplicationRetrievalService.cs
@@ -4,14 +4,36 @@ using Dfe.Academies.External.Web.Models;
 namespace Dfe.Academies.External.Web.Services;
 public interface IConversionApplicationRetrievalService
 {
+	/// <summary>
+	/// This depends on the concept of contributor and filtering by their username
+	/// </summary>
+	/// <param name="username"></param>
+	/// <returns></returns>
 	List<ConversionApplication> GetCompletedApplications(string? username);
+
+	/// <summary>
+	/// This depends on the concept of contributor and filtering by their username
+	/// </summary>
+	/// <param name="username"></param>
+	/// <returns></returns>
 	List<ConversionApplication> GetPendingApplications(string? username);
 
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="applicationId"></param>
+	/// <returns></returns>
 	Task<List<ConversionApplicationAuditEntry>> GetConversionApplicationAuditEntries(int applicationId);
 
-	Task<List<ConversionApplicationComponent>> GetSchoolApplicationComponents(int schoolId);
+	/// <summary>
+	/// Return a list<viewmodel> to render application progress on screen
+	/// </summary>
+	/// <param name="schoolId"></param>
+	/// <returns></returns>
+	Task<List<ConversionApplicationComponent>> GetSchoolApplicationComponents(int applicationId, int schoolId);
+
 
 	Task<List<ConversionApplicationContributor>> GetConversionApplicationContributors(int applicationId);
 	
-	Task<ConversionApplication> GetApplication(int applicationId, ApplicationTypes applicationType);
+	Task<ConversionApplication> GetApplication(int applicationId);
 }
