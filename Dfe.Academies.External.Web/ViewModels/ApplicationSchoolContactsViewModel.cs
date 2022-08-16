c52ec6eaced115220cd36ef6fdac462d7f10fc3b
Mark ROBINSON <Mark.ROBINSON@EDUCATION.GOV.UK>
2022-08-16 10:54:52 +0100

    added validation as per ticket

--- Dfe.Academies.External.Web/ViewModels/ApplicationSchoolContactsViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/ApplicationSchoolContactsViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace Dfe.Academies.External.Web.ViewModels;
+﻿using Dfe.Academies.External.Web.Attributes;
+using Dfe.Academies.External.Web.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dfe.Academies.External.Web.ViewModels;
 
 public sealed class ApplicationSchoolContactsViewModel
 {
@@ -10,20 +14,46 @@ public sealed class ApplicationSchoolContactsViewModel
 
 	public int ApplicationId { get; set; }
 	public int Urn { get; set; }
+
 	// contact details
+
+	[Required(ErrorMessage = "You must provide details")]
 	public string ContactHeadName { get; set; }
+
+	[Required(ErrorMessage = "You must provide details")]
 	public string ContactHeadEmail { get; set; }
+
+	[Required(ErrorMessage = "You must provide a number")]
 	public string ContactHeadTel { get; set; }
+
+	[Required(ErrorMessage = "You must provide details")]
 	public string ContactChairName { get; set; }
+
+	[Required(ErrorMessage = "You must provide details")]
 	public string ContactChairEmail { get; set; }
+
+	[Required(ErrorMessage = "You must provide a number")]
 	public string ContactChairTel { get; set; }
 
-	// "headteacher", "chair of governing body", "someone else"
-	public string ContactRole { get; set; }
+	[RequiredEnum(ErrorMessage = "You must provide a main contact")]
+	public MainConversionContact ContactRole { get; set; }
+
+	[Required(ErrorMessage = "You must provide details")]
 	public string MainContactOtherName { get; set; }
+
+	[Required(ErrorMessage = "You must provide details")]
 	public string MainContactOtherEmail { get; set; }
+
+	[Required(ErrorMessage = "You must provide a number")]
 	public string MainContactOtherTelephone { get; set; }
-	public string MainContactOtherRole { get; set; }
+
+	/// <summary>
+	/// only applicable if ContactRole = other
+	/// </summary>
+	public string? MainContactOtherRole { get; set; }
+
+	// TODO MR:- no validation on ticket?
+
 	public string ApproverContactName { get; set; }
 	public string ApproverContactEmail { get; set; }
 }
