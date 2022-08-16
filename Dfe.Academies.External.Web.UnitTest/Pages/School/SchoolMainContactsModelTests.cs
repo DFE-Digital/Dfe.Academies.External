3d292c16ca3ba5008c5fd4bbd167417476e1134b
humboldtsystems <humboldtsystems@gmail.com>
2022-08-16 09:05:52 +0100

    stub of page & tests

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/SchoolMainContactsModelTests.cs
@@ -0,0 +1,45 @@
+﻿using Dfe.Academies.External.Web.Pages.School;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages.School;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class SchoolMainContactsModelTests
+{
+	// TODO MR:- public async Task OnGetAsync___Valid___NullErrors()
+
+
+	// TODO MR:- OnPostAsync___ModelIsValid___Invalid
+	// when academisation API is implemented, will need to mock ResilientRequestProvider for http client API responses
+
+	// TODO MR:- OnPostAsync___ModelIsValid___Valid
+	// when academisation API is implemented, will need to mock ResilientRequestProvider for http client API responses
+
+	private static SchoolMainContactsModel SetupSchoolMainContactsModel(
+		ILogger<SchoolMainContactsModel> mockLogger,
+		IConversionApplicationCreationService mockConversionApplicationCreationService,
+		IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+		IReferenceDataRetrievalService mockReferenceDataRetrievalService,
+		bool isAuthenticated = false)
+	{
+		(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+		return new SchoolMainContactsModel(mockLogger, mockConversionApplicationRetrievalService,
+			mockReferenceDataRetrievalService, mockConversionApplicationCreationService)
+		{
+			PageContext = pageContext,
+			TempData = tempData,
+			Url = new UrlHelper(actionContext),
+			MetadataProvider = pageContext.ViewData.ModelMetadata
+		};
+	}
+}
