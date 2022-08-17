7add06935ca79f3bbed322930ae4fa702faa23e3
humboldtsystems <humboldtsystems@gmail.com>
2022-08-17 16:06:50 +0100

    fixes from other branch

--- Dfe.Academies.External.Web/Services/ConversionApplicationCreationService.cs
+++ Dfe.Academies.External.Web/Services/ConversionApplicationCreationService.cs
@@ -26,7 +26,7 @@ public sealed class ConversionApplicationCreationService : BaseService, IConvers
 
 		    // TODO: wire up Academisation API
 		    // var result = await _resilientRequestProvider.PostAsync<ConversionApplicationApiPostResult, ConversionApplication>(apiurl, application);
-		    application.ApplicationId = int.MaxValue; // result.ApplicationId;
+		    application.ApplicationId = (int)application.ApplicationType; // result.ApplicationId;
 
 		    return application;
 		}
