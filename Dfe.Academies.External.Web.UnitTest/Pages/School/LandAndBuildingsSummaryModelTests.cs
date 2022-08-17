6a8dce64fbc4f24867d4a7ff3deffa4a906e1a2f
humboldtsystems <humboldtsystems@gmail.com>
2022-08-17 15:23:45 +0100

    unit test for OnGet()

--- Dfe.Academies.External.Web.UnitTest/Pages/School/LandAndBuildingsSummaryModelTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/LandAndBuildingsSummaryModelTests.cs
@@ -1,6 +1,66 @@
-﻿namespace Dfe.Academies.External.Web.UnitTest.Pages.School;
+﻿using Dfe.Academies.External.Web.Pages.School;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
+namespace Dfe.Academies.External.Web.UnitTest.Pages.School;
+
+[Parallelizable(ParallelScope.All)]
 internal sealed class LandAndBuildingsSummaryModelTests
 {
-	// TODO MR:-
+	/// <summary>
+	/// "draftConversionApplication" in temp storage
+	/// from previous step in the new application wizard
+	/// </summary>
+	/// <returns></returns>
+	[Test]
+	public async Task OnGetAsync___Valid___NullErrors()
+	{
+		// arrange
+		var draftConversionApplicationStorageKey = TempDataHelper.DraftConversionApplicationKey;
+		var mockConversionApplicationRetrievalService = new Mock<IConversionApplicationRetrievalService>();
+		var mockReferenceDataRetrievalService = new Mock<IReferenceDataRetrievalService>();
+		var mockLogger = new Mock<ILogger<LandAndBuildingsSummaryModel>>();
+		int urn = 101934;
+		int applicationId = int.MaxValue;
+
+		var conversionApplication = ConversionApplicationTestDataFactory.BuildNewConversionApplicationWithChairRole();
+
+		// act
+		var pageModel = SetupLandAndBuildingsSummaryModel(mockLogger.Object,
+			mockConversionApplicationRetrievalService.Object,
+			mockReferenceDataRetrievalService.Object);
+		TempDataHelper.StoreSerialisedValue(draftConversionApplicationStorageKey, pageModel.TempData, conversionApplication);
+
+		// act
+		await pageModel.OnGetAsync(urn, applicationId);
+
+		// assert
+		Assert.That(pageModel.TempData["Errors"], Is.EqualTo(null));
+	}
+
+	private static LandAndBuildingsSummaryModel SetupLandAndBuildingsSummaryModel(
+		ILogger<LandAndBuildingsSummaryModel> mockLogger,
+		IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+		IReferenceDataRetrievalService mockReferenceDataRetrievalService,
+		bool isAuthenticated = false)
+	{
+		(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+		return new LandAndBuildingsSummaryModel(mockLogger, mockConversionApplicationRetrievalService,
+			mockReferenceDataRetrievalService)
+		{
+			PageContext = pageContext,
+			TempData = tempData,
+			Url = new UrlHelper(actionContext),
+			MetadataProvider = pageContext.ViewData.ModelMetadata
+		};
+	}
 }
