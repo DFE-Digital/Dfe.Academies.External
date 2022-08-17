11d4d2c15890079464206fc479a20f945f497830
humboldtsystems <humboldtsystems@gmail.com>
2022-08-17 14:55:44 +0100

    new view models for new land and buildings summary page

---
+++ Dfe.Academies.External.Web/ViewModels/SchoolLandAndBuildingsSummarySectionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels;
+
+public class SchoolLandAndBuildingsSummarySectionViewModel : SchoolQuestionAndAnswerViewModel
+{
+	// 7 sections / questions
+	public SchoolLandAndBuildingsSummarySectionViewModel(string name, string answer) : base(name, answer)
+	{
+	}
+}
