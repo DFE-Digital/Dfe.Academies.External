2bbba11aaec9faab703262bbc564335d6378b0c8
humboldtsystems <humboldtsystems@gmail.com>
2022-08-18 11:39:19 +0100

    changes after working on L & B summary page

--- Dfe.Academies.External.Web/Models/SchoolLandAndBuildings.cs
+++ Dfe.Academies.External.Web/Models/SchoolLandAndBuildings.cs
@@ -3,14 +3,15 @@
 public record SchoolLandAndBuildings(int ApplicationId,
 	int URN,
 	string? SchoolBuildLandOwnerExplained,
-	bool? SchoolBuildLandSharedFacilities, // should this be y/n enum ??
 	bool? SchoolBuildLandWorksPlanned,  // should this be y/n enum ??
 	string? SchoolBuildLandWorksPlannedExplained,
 	DateTime? SchoolBuildLandWorksPlannedDate,
+	bool? SchoolBuildLandSharedFacilities, // should this be y/n enum ??
+	string? SchoolBuildLandSharedFacilitiesExplained,
 	bool? SchoolBuildLandGrants, // should this be y/n enum ??
-	string? SchoolBuildLandGrantsBody,
-	bool? SchoolBuildLandPriorityBuildingProgramme, // should this be y/n enum ??
-	bool? SchoolBuildLandFutureProgramme, // should this be y/n enum ??
+	string? SchoolBuildLandGrantsBodies,
 	bool? SchoolBuildLandPFIScheme, // should this be y/n enum ??
-	string? SchoolBuildLandPFISchemeType
+	string? SchoolBuildLandPFISchemeType,
+	bool? SchoolBuildLandPriorityBuildingProgramme, // should this be y/n enum ??
+	bool? SchoolBuildLandFutureProgramme // should this be y/n enum ??
 );
