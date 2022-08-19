84ca536bd94ae29d54bb07748da6f5bf31f1c575
humboldtsystems <humboldtsystems@gmail.com>
2022-08-19 10:02:03 +0100

    new view models for page

---
+++ Dfe.Academies.External.Web/ViewModels/SchoolConsultationSummarySectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels;
+
+public class SchoolConsultationSummarySectionViewModel : SchoolQuestionAndAnswerViewModel
+{
+	public const string PAN = "What is the school's published admissions number (PAN)?";
+
+	public SchoolConsultationSummarySectionViewModel(string name, string answer) : base(name, answer)
+	{
+	}
+}
