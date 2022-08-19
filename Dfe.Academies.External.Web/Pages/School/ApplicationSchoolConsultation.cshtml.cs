d18f1b5d3ffd30af2ca40e4d1b34d5d394adbe5a
humboldtsystems <humboldtsystems@gmail.com>
2022-08-19 09:43:12 +0100

    stub page & tests

---
+++ Dfe.Academies.External.Web/Pages/School/ApplicationSchoolConsultation.cshtml.cs
@@ -0,0 +1,84 @@
+using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Dfe.Academies.External.Web.ViewModels;
+
+namespace Dfe.Academies.External.Web.Pages.School
+{
+    public class ApplicationSchoolConsultationModel : BasePageEditModel
+	{
+		private readonly ILogger<ApplicationSchoolConsultationModel> _logger;
+
+		//// MR:- selected school props for UI rendering
+		[BindProperty]
+		public int ApplicationId { get; set; }
+
+		[BindProperty]
+		public int Urn { get; set; }
+
+		public string SchoolName { get; private set; } = string.Empty;
+
+		//// MR:- VM props to show school conversion data
+
+		public ApplicationSchoolConsultationModel(ILogger<ApplicationSchoolConsultationModel> logger,
+			IConversionApplicationRetrievalService conversionApplicationRetrievalService,
+			IReferenceDataRetrievalService referenceDataRetrievalService)
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+		{
+			_logger = logger;
+		}
+
+		public async Task OnGetAsync(int urn, int appId)
+		{
+			try
+			{
+				LoadAndStoreCachedConversionApplication();
+
+				var selectedSchool = await LoadAndSetSchoolDetails(appId, urn);
+
+				// Grab other values from API
+				if (selectedSchool != null)
+				{
+					// TODO MR:- grab data from API endpoint - applicationId && SchoolId combination !
+
+
+					PopulateUiModel(selectedSchool);
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("School::ApplicationSchoolConsultationModel::OnGetAsync::Exception - {Message}", ex.Message);
+			}
+		}
+
+		public override void PopulateValidationMessages()
+		{
+			PopulateViewDataErrorsWithModelStateErrors();
+		}
+
+		private void PopulateUiModel(SchoolApplyingToConvert selectedSchool)
+		{
+			ApplicationId = selectedSchool.ApplicationId;
+			Urn = selectedSchool.URN;
+			SchoolName = selectedSchool.SchoolName;
+
+			//SchoolPupilNumbersSummaryHeadingViewModel heading1 = new(SchoolPupilNumbersSummaryHeadingViewModel.Heading,
+			//	"/school/PupilNumbers");
+
+			//// TODO MR:- for answer, consume QuestionAndAnswerConstants.NoInfoAnswer if string.IsNullOrWhiteSpace()
+			//// OR data from API
+
+			//heading1.Sections.Add(new(SchoolPupilNumbersSummarySectionViewModel.PupilNumberYr1, "TBC"));
+			//heading1.Sections.Add(new(SchoolPupilNumbersSummarySectionViewModel.PupilNumberYr2, "TBC"));
+			//heading1.Sections.Add(new(SchoolPupilNumbersSummarySectionViewModel.PupilNumberYr3, "TBC"));
+			//heading1.Sections.Add(new(SchoolPupilNumbersSummarySectionViewModel.NumbersBasedUpon, "TBC"));
+			//heading1.Sections.Add(new(SchoolPupilNumbersSummarySectionViewModel.PAN, "TBC"));
+
+			//var vm = new List<SchoolPupilNumbersSummaryHeadingViewModel> { heading1 };
+
+			//ViewModel = vm;
+		}
+	}
+}
