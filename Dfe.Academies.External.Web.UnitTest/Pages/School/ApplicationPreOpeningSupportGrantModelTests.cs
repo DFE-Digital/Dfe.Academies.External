30afde3515f76b0c2a52c892df8940c485468d25
mark robinson <mark.robinson@education.gov.uk>
2022-08-22 16:27:14 +0100

    flesh out tests

--- Dfe.Academies.External.Web.UnitTest/Pages/School/ApplicationPreOpeningSupportGrantModelTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/ApplicationPreOpeningSupportGrantModelTests.cs
@@ -15,5 +15,61 @@ namespace Dfe.Academies.External.Web.UnitTest.Pages.School;
 [Parallelizable(ParallelScope.All)]
 internal sealed class ApplicationPreOpeningSupportGrantModelTests
 {
-	// TODO MR:-
+	/// <summary>
+	/// "draftConversionApplication" in temp storage
+	/// from previous step in the new application wizard
+	/// </summary>
+	/// <returns></returns>
+	[Test]
+	public async Task OnGetAsync___Valid___NullErrors()
+	{
+		// arrange
+		var draftConversionApplicationStorageKey = TempDataHelper.DraftConversionApplicationKey;
+		var mockConversionApplicationCreationService = new Mock<IConversionApplicationCreationService>();
+		var mockConversionApplicationRetrievalService = new Mock<IConversionApplicationRetrievalService>();
+		var mockReferenceDataRetrievalService = new Mock<IReferenceDataRetrievalService>();
+		var mockLogger = new Mock<ILogger<ApplicationPreOpeningSupportGrantModel>>();
+		int urn = 101934;
+		int applicationId = int.MaxValue;
+
+		var conversionApplication = ConversionApplicationTestDataFactory.BuildNewConversionApplicationWithChairRole();
+
+		// act
+		var pageModel = SetupApplicationPreOpeningSupportGrantModel(mockLogger.Object,
+			mockConversionApplicationCreationService.Object,
+			mockConversionApplicationRetrievalService.Object,
+			mockReferenceDataRetrievalService.Object);
+		TempDataHelper.StoreSerialisedValue(draftConversionApplicationStorageKey, pageModel.TempData, conversionApplication);
+
+		// act
+		await pageModel.OnGetAsync(urn, applicationId);
+
+		// assert
+		Assert.That(pageModel.TempData["Errors"], Is.EqualTo(null));
+	}
+
+	// TODO MR:- OnPostAsync___ModelIsValid___Invalid
+	// when academisation API is implemented, will need to mock ResilientRequestProvider for http client API responses
+
+	// TODO MR:- OnPostAsync___ModelIsValid___Valid
+	// when academisation API is implemented, will need to mock ResilientRequestProvider for http client API responses
+
+	private static ApplicationPreOpeningSupportGrantModel SetupApplicationPreOpeningSupportGrantModel(
+		ILogger<ApplicationPreOpeningSupportGrantModel> mockLogger,
+		IConversionApplicationCreationService mockConversionApplicationCreationService,
+		IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+		IReferenceDataRetrievalService mockReferenceDataRetrievalService,
+		bool isAuthenticated = false)
+	{
+		(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+		return new ApplicationPreOpeningSupportGrantModel(mockLogger, mockConversionApplicationRetrievalService,
+			mockReferenceDataRetrievalService, mockConversionApplicationCreationService)
+		{
+			PageContext = pageContext,
+			TempData = tempData,
+			Url = new UrlHelper(actionContext),
+			MetadataProvider = pageContext.ViewData.ModelMetadata
+		};
+	}
 }
