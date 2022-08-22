42346022786b1811e68d1f8765c85cecddd5a409
mark robinson <mark.robinson@education.gov.uk>
2022-08-22 16:25:46 +0100

    new page & tests

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/ApplicationPreOpeningSupportGrantModelTests.cs
@@ -0,0 +1,19 @@
+﻿using Dfe.Academies.External.Web.Pages.School;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages.School;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ApplicationPreOpeningSupportGrantModelTests
+{
+	// TODO MR:-
+}
---
+++ Dfe.Academies.External.Web/Pages/School/ApplicationPreOpeningSupportGrant.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using Dfe.Academies.External.Web.Attributes;
+using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages.School;
+
+public class ApplicationPreOpeningSupportGrantModel : BasePageEditModel
+{
+    private readonly ILogger<ApplicationPreOpeningSupportGrantModel> _logger;
+    private readonly IConversionApplicationCreationService _academisationCreationService;
+
+    //// MR:- selected school props for UI rendering
+    [BindProperty]
+    public int ApplicationId { get; set; }
+
+    [BindProperty]
+    public int Urn { get; set; }
+
+    public string SchoolName { get; private set; } = string.Empty;
+
+	//// MR:- VM props to capture data
+	// enum - to school / to trust
+	[BindProperty]
+	[RequiredEnum(ErrorMessage = "You must provide details")]
+	public PayFundsTo PayFundsTo { get; set; }
+
+	public ApplicationPreOpeningSupportGrantModel(ILogger<ApplicationPreOpeningSupportGrantModel> logger,
+		IConversionApplicationRetrievalService conversionApplicationRetrievalService,
+		IReferenceDataRetrievalService referenceDataRetrievalService,
+		IConversionApplicationCreationService academisationCreationService)
+		: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+	{
+		_logger = logger;
+		_academisationCreationService = academisationCreationService;
+	}
+
+	public async Task OnGetAsync(int urn, int appId)
+	{
+		try
+		{
+			LoadAndStoreCachedConversionApplication();
+
+			var selectedSchool = await LoadAndSetSchoolDetails(appId, urn);
+
+			// Grab other values from API
+			if (selectedSchool != null)
+			{
+				// TODO MR:- grab data from API endpoint - applicationId && SchoolId combination !
+				// data stored against the school ?????????????? not implemented 22/08/2022
+
+
+				PopulateUiModel(selectedSchool);
+			}
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError("School::ApplicationPreOpeningSupportGrantModel::OnGetAsync::Exception - {Message}", ex.Message);
+		}
+	}
+
+	// TODO MR:- Post()
+
+	public override void PopulateValidationMessages()
+	{
+		PopulateViewDataErrorsWithModelStateErrors();
+	}
+
+	private void PopulateUiModel(SchoolApplyingToConvert selectedSchool)
+	{
+		ApplicationId = selectedSchool.ApplicationId;
+		Urn = selectedSchool.URN;
+		SchoolName = selectedSchool.SchoolName;
+		// TODO MR:- populate other props from API - not implemented 22/08/2022
+		//PayFundsTo = selectedSchool.PayFundsTo;
+	}
+}
+
