732e3735cc3288d235e7739e503b17000e310958
mark robinson <mark.robinson@education.gov.uk>
2022-08-22 12:01:57 +0100

    new page & tests beginning

---
+++ Dfe.Academies.External.Web/Pages/School/ApplicationSchoolConsultation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dfe.Academies.External.Web.Pages.School
+{
+    public class ApplicationSchoolConsultationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
