20f5007df71737a714fd5bad544870349a8b1511
mark robinson <mark.robinson@education.gov.uk>
2022-08-30 15:40:09 +0100

    removed un-needed obj

--- Dfe.Academies.External.Web/Models/ConversionApplicationApiPostResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dfe.Academies.External.Web.Enums;
-
-namespace Dfe.Academies.External.Web.Models;
-
-public class ConversionApplicationApiPostResult
-{
-	public int ApplicationId { get; set; }
-	public ApplicationTypes ApplicationType { get; set; }
-}
