174934e8e0b31c9474f80f97131b8da7fb8d5027
mark robinson <mark.robinson@education.gov.uk>
2022-08-30 15:26:46 +0100

    new model to mimic what API expects for serilization

---
+++ Dfe.Academies.External.Web/Models/CreateApplicationApiModel.cs
@@ -0,0 +1,10 @@
+﻿using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.Models;
+
+public record CreateApplicationApiModel(ApplicationTypes ApplicationType, 
+										string FirstName, 
+										string Surname, 
+										string EmailAddress, 
+										SchoolRoles Role, 
+										string? OtherRoleName);
