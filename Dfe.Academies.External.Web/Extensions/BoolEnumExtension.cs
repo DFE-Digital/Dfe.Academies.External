b2130e3d4135c8e6154cf8575dd71ac8ab2313d4
mark robinson <mark.robinson@education.gov.uk>
2022-09-07 15:02:34 +0100

    new extension to convert bool to enum

---
+++ Dfe.Academies.External.Web/Extensions/BoolEnumExtension.cs
@@ -0,0 +1,21 @@
+﻿using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.Extensions;
+
+internal static class BoolEnumExtension
+{
+	public static SelectOption? GetEnumValue(this bool? value)
+	{
+		if (!value.HasValue)
+			return null;
+
+		if (value.Value)
+		{
+			return SelectOption.Yes;
+		}
+		else
+		{
+			return SelectOption.No;
+		}
+	}
+}
