f5f9a9018d101319f88fcd0c0c5c89fea621dc90
mark robinson <mark.robinson@education.gov.uk>
2022-09-22 17:47:30 +0100

    103176 - finances summary page started here !

---
+++ Dfe.Academies.External.Web/ViewModels/FinancesReviewHeadingViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels
+{
+	public class FinancesReviewHeadingViewModel: SchoolQuestionAndAnswerSectionViewModel
+
+	{
+		public const string Heading = "Finances";
+		public const string HeadingPreviousFinancialYear = "Previous financial year";
+		public const string HeadingCurrentFinancialYear = "Current financial year";
+		public const string HeadingNextFinancialYear = "Next financial year";
+		public const string HeadingLoans = "Loans";
+		public const string HeadingLeases = "Financial Leases";
+		public const string HeadingFinancialInvestigations = "Financial investigations";
+
+		public FinancesReviewHeadingViewModel(string title, string uRi) : base(title, uRi)
+		{
+			Sections = new();
+		}
+
+		public List<FinancesReviewSectionViewModel> Sections { get; set; }
+	}
+}
