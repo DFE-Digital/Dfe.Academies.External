23bb1af35f49d5afa69423323f2fabdcbaa6609a
mark robinson <mark.robinson@education.gov.uk>
2022-09-23 10:55:30 +0100

    added test fies

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/PreviousFinancialYearModelTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages.School;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class PreviousFinancialYearModelTests
+{
+	// TODO MR:- flesh out std tests
+}
