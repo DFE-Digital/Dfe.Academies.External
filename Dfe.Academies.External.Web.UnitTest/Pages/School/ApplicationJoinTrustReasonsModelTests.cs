d98e20df6052c56b227959d33fa307e4e6cf308f
mark robinson <mark.robinson@education.gov.uk>
2022-09-29 12:26:47 +0100

    took my name off tests

--- Dfe.Academies.External.Web.UnitTest/Pages/School/ApplicationJoinTrustReasonsModelTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Pages/School/ApplicationJoinTrustReasonsModelTests.cs
@@ -48,10 +48,10 @@ internal sealed class ApplicationJoinTrustReasonsModelTests
 		Assert.AreEqual(null, pageModel.TempData["Errors"]);
 	}
 
-	// TODO MR:- OnPostAsync___ModelIsValid___Invalid
+	// TODO :- OnPostAsync___ModelIsValid___Invalid
 	// when academisation API is implemented, will need to mock ResilientRequestProvider for http client API responses
 
-	// TODO MR:- OnPostAsync___ModelIsValid___Valid
+	// TODO :- OnPostAsync___ModelIsValid___Valid
 	// when academisation API is implemented, will need to mock ResilientRequestProvider for http client API responses
 
 	private static ApplicationJoinTrustReasonsModel SetupApplicationJoinTrustReasonsModel(
