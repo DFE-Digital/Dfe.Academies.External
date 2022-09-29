6db2c165d9500e8fe1e6a738e8a7cdad85b9a4e8
mark robinson <mark.robinson@education.gov.uk>
2022-09-29 11:03:47 +0100

    104225 - https://dfe-gov-uk.visualstudio.com.mcas.ms/Academies-and-Free-Schools-SIP/_workitems/edit/104225?McasTsid=26110

---
+++ Dfe.Academies.External.Web/ViewModels/DeclarationSummaryHeadingViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels
+{
+	public class DeclarationSummaryHeadingViewModel : SchoolQuestionAndAnswerSectionViewModel
+	{
+		public const string Heading = "Finances";
+		public const string HeadingDetails = "Details";
+
+		public DeclarationSummaryHeadingViewModel(string title, string uRi) : base(title, uRi)
+		{
+			Sections = new();
+		}
+
+		public List<DeclarationSummarySectionViewModel> Sections { get; set; }
+	}
+}
