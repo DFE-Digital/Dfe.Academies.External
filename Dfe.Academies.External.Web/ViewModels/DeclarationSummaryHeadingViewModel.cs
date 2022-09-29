1c2ac49cc9d12ec44f3f8636fed72fe280e9becc
mark robinson <mark.robinson@education.gov.uk>
2022-09-29 12:35:12 +0100

    fixed header text

--- Dfe.Academies.External.Web/ViewModels/DeclarationSummaryHeadingViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/DeclarationSummaryHeadingViewModel.cs
@@ -2,8 +2,8 @@
 {
 	public class DeclarationSummaryHeadingViewModel : SchoolQuestionAndAnswerSectionViewModel
 	{
-		public const string Heading = "Finances";
-		public const string HeadingDetails = "Details";
+		public const string Heading = "Details";
+		public const string HeadingDetails = "Q";
 
 		public DeclarationSummaryHeadingViewModel(string title, string uRi) : base(title, uRi)
 		{
