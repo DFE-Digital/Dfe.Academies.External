5cd7e8d07ee63641618399f5117c97e08afc29b8
mark robinson <mark.robinson@education.gov.uk>
2022-10-13 16:21:06 +0100

    added global exception middleware

---
+++ Dfe.Academies.External.Web/Models/Responses/BaseErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Dfe.Academies.External.Web.Models.Responses;
+
+[DataContract]
+public class BaseErrorResponse : BaseResponse
+{
+    /// <summary>
+    /// The details of the error experienced.
+    /// </summary>
+    [DataMember]
+    public string Error { get; set; }
+
+    public BaseErrorResponse(string error)
+    {
+        Error = error;
+    }
+}
---
+++ Dfe.Academies.External.Web/Models/Responses/Default500InternalServerErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Dfe.Academies.External.Web.Models.Responses;
+
+[DataContract]
+public class Default500InternalServerErrorResponse : BaseErrorResponse
+{
+    public Default500InternalServerErrorResponse(string errorMessage)
+        : base(errorMessage)
+    {
+    }
+}
