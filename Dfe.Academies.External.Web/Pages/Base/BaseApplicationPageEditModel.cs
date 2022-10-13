1e046eec3d9424db01c53618f10316b3a53b0b55
mark robinson <mark.robinson@education.gov.uk>
2022-10-13 14:46:39 +0100

    created 2 new base classes

---
+++ Dfe.Academies.External.Web/Pages/Base/BaseApplicationPageEditModel.cs
@@ -0,0 +1,34 @@
+﻿using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages.Base;
+
+public abstract class BaseApplicationPageEditModel : BasePageEditModel
+{
+	public readonly IConversionApplicationCreationService ConversionApplicationCreationService;
+
+	[BindProperty] public int ApplicationId { get; set; }
+
+	public string NextStepPage { get; private set; }
+
+	protected BaseApplicationPageEditModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
+										IReferenceDataRetrievalService referenceDataRetrievalService,
+										IConversionApplicationCreationService conversionApplicationCreationService,
+										string nextStepPage)
+		: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+	{
+		ConversionApplicationCreationService = conversionApplicationCreationService;
+		NextStepPage = nextStepPage;
+	}
+
+	// TODO MR:- public async Task OnGetAsync(int appId)
+
+	// TODO MR:- public async Task<IActionResult> OnPostAsync()
+
+	/// <summary>
+	/// take application data from API and populate UI controls
+	/// </summary>
+	/// <param name="conversionApplication"></param>
+	public abstract void PopulateUiModel(ConversionApplication? conversionApplication);
+}
---
+++ Dfe.Academies.External.Web/Pages/Base/BaseSchoolPageEditModel.cs
@@ -0,0 +1,87 @@
+﻿using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages.Base;
+
+public abstract class BaseSchoolPageEditModel : BasePageEditModel
+{
+	public readonly IConversionApplicationCreationService ConversionApplicationCreationService;
+
+	[BindProperty]
+	public int ApplicationId { get; set; }
+
+	[BindProperty]
+	public int Urn { get; set; }
+
+	public string SchoolName { get; private set; } = string.Empty;
+
+	public string NextStepPage { get; private set; }
+
+	protected BaseSchoolPageEditModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
+									IReferenceDataRetrievalService referenceDataRetrievalService,
+									IConversionApplicationCreationService conversionApplicationCreationService,
+									string nextStepPage) 
+		: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+	{
+		ConversionApplicationCreationService = conversionApplicationCreationService;
+		NextStepPage = nextStepPage;
+	}
+
+	//1) Create OnGetAsync() func in new base class - call PopulateUiModel() method, that will be overridden in each page
+	public async Task OnGetAsync(int urn, int appId)
+	{
+		try
+		{
+			LoadAndStoreCachedConversionApplication();
+
+			var selectedSchool = await LoadAndSetSchoolDetails(appId, urn);
+			ApplicationId = appId;
+			Urn = urn;
+
+			// Grab other values from API
+			if (selectedSchool != null)
+			{
+				PopulateUiModel(selectedSchool);
+				SchoolName = selectedSchool.SchoolName;
+			}
+		}
+		catch (Exception ex)
+		{
+			// TODO MR:- fix !!
+			//_logger.LogError("School::CurrentFinancialYearModel::OnGetAsync::Exception - {Message}", ex.Message);
+		}
+	}
+
+
+	//2) Create OnPostAsync() func in new base class - call PopulateUiModel() method, that will be overridden in each page
+	public async Task<IActionResult> OnPostAsync()
+	{
+		try
+		{
+			//// grab draft application from temp= null
+			var draftConversionApplication = TempDataHelper.GetSerialisedValue<ConversionApplication>(TempDataHelper.DraftConversionApplicationKey, TempData) ?? new ConversionApplication();
+
+			var dictionaryMapper = PopulateUpdateDictionary();
+
+			await ConversionApplicationCreationService.PutSchoolApplicationDetails(ApplicationId, Urn, dictionaryMapper);
+
+			// update temp store for next step - application overview
+			TempDataHelper.StoreSerialisedValue(TempDataHelper.DraftConversionApplicationKey, TempData, draftConversionApplication);
+
+			return RedirectToPage(NextStepPage, new { appId = ApplicationId, urn = Urn });
+		}
+		catch (Exception ex)
+		{
+			// TODO MR:- fix !!
+			//_logger.LogError("School::CurrentFinancialYearModel::OnPostAsync::Exception - {Message}", ex.Message);
+			return Page();
+		}
+	}
+
+	/// <summary>
+	/// take school data from API and populate UI controls
+	/// </summary>
+	/// <param name="selectedSchool"></param>
+	public abstract void PopulateUiModel(SchoolApplyingToConvert selectedSchool);
+}
