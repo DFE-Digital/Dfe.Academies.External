c0636e9549e236b1b12246ac36db927d874902c6
mark robinson <mark.robinson@education.gov.uk>
2022-10-17 10:40:19 +0100

    amended page to consume BaseSchoolPageEditModel

--- Dfe.Academies.External.Web/Pages/School/ApplicationJoinTrustReasons.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/School/ApplicationJoinTrustReasons.cshtml.cs
@@ -6,68 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.Academies.External.Web.Pages.School
 {
-	public class ApplicationJoinTrustReasonsModel : BasePageEditModel
+	public class ApplicationJoinTrustReasonsModel : BaseSchoolPageEditModel
 	{
-		private readonly IConversionApplicationCreationService _academisationCreationService;
-		private const string NextStepPage = "ApplicationChangeSchoolName";
-
-		//// MR:- selected school props for UI rendering
-		[BindProperty]
-		public int ApplicationId { get; set; }
-
-		[BindProperty]
-		public int Urn { get; set; }
-
-		//// MR:- VM props to capture pupil numbers data
+		// MR:- VM props to capture pupil numbers data
 		[BindProperty]
 		[Required(ErrorMessage = "You must provide details")]
 		public string? ApplicationJoinTrustReason { get; set; } = string.Empty;
 
-		public ApplicationJoinTrustReasonsModel(ILogger<ApplicationJoinTrustReasonsModel> logger,
-			IConversionApplicationRetrievalService conversionApplicationRetrievalService,
+		public ApplicationJoinTrustReasonsModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService,
 			IReferenceDataRetrievalService referenceDataRetrievalService,
 			IConversionApplicationCreationService academisationCreationService)
-			: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
-		{
-			_academisationCreationService = academisationCreationService;
-		}
-
-		public async Task OnGetAsync(int urn, int appId)
-		{
-			LoadAndStoreCachedConversionApplication();
-
-			var selectedSchool = await LoadAndSetSchoolDetails(appId, urn);
-			ApplicationId = appId;
-			Urn = urn;
-
-			// Grab other values from API
-			if (selectedSchool != null)
-			{
-				PopulateUiModel(selectedSchool);
-			}
-		}
-
-		public async Task<IActionResult> OnPostAsync()
-		{
-			if (!RunUiValidation())
-			{
-				return Page();
-			}
-
-			// grab draft application from temp= null
-			var draftConversionApplication =
-				TempDataHelper.GetSerialisedValue<ConversionApplication>(
-					TempDataHelper.DraftConversionApplicationKey, TempData) ?? new ConversionApplication();
-
-			var dictionaryMapper = PopulateUpdateDictionary();
-			await _academisationCreationService.PutSchoolApplicationDetails(ApplicationId, Urn, dictionaryMapper);
-
-			// update temp store for next step
-			TempDataHelper.StoreSerialisedValue(TempDataHelper.DraftConversionApplicationKey, TempData, draftConversionApplication);
-
-			return RedirectToPage(NextStepPage, new { appId = ApplicationId, urn = Urn });
-		}
-
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService, 
+				academisationCreationService, "ApplicationChangeSchoolName")
+		{}
+		
 		///<inheritdoc/>
 		public override bool RunUiValidation()
 		{
@@ -97,7 +49,8 @@ namespace Dfe.Academies.External.Web.Pages.School
 			};
 		}
 
-		private void PopulateUiModel(SchoolApplyingToConvert selectedSchool)
+		///<inheritdoc/>
+		public override void PopulateUiModel(SchoolApplyingToConvert selectedSchool)
 		{
 			ApplicationJoinTrustReason = selectedSchool.ApplicationJoinTrustReason;
 		}
