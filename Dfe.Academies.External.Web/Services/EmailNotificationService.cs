713520b0fe8be6228e908db350085fa1cba17773
mark robinson <mark.robinson@education.gov.uk>
2022-10-28 11:05:10 +0100

    gov uk notify send email implementation - stub

---
+++ Dfe.Academies.External.Web/Services/EmailNotificationService.cs
@@ -0,0 +1,30 @@
+﻿using Dfe.Academies.External.Web.Models.Notifications;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Notify.Client;
+using Notify.Models;
+using Notify.Models.Responses;
+
+namespace Dfe.Academies.External.Web.Services;
+
+public class EmailNotificationService : IEmailNotificationService
+{
+	public EmailNotificationService()
+	{
+		// TODO:- grab api key from "emailnotifications":"key"
+		//var client = new NotificationClient(apiKey);
+
+		var httpClientWithProxy = new HttpClientWrapper(new HttpClient(...));
+		var client = new NotificationClient(httpClientWithProxy, apiKey);
+	}
+
+    public Task SendAsync(MessageDto message)
+    {
+		// TODO MR:-
+		//EmailNotificationResponse response = client.SendEmail(emailAddress, templateId, personalisation, reference, emailReplyToId);
+		return Task.CompletedTask;
+    }
+
+	// TODO MR:- wham template Id's somewhere e.g. constants here ??
+	// Template ID - template ID:858e5bea-9d49-442e-a89e-aaed2fb4ade6 // 'Invitation to contribute - chair'
+	// Template ID - template ID:03a0ae16-27fe-425d-8aa7-cac43d79f040 // 'Invitation to contribute - someone else'
+}
