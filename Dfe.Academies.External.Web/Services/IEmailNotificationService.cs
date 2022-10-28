bf2ea8b3ba70c1873f25a39835ab2051c9a0b931
mark robinson <mark.robinson@education.gov.uk>
2022-10-28 11:04:54 +0100

    send email interface

---
+++ Dfe.Academies.External.Web/Services/IEmailNotificationService.cs
@@ -0,0 +1,9 @@
+﻿using Dfe.Academies.External.Web.Models.Notifications;
+
+namespace Dfe.Academies.External.Web.Services
+{
+	public interface IEmailNotificationService
+	{
+		Task SendAsync(MessageDto message);
+	}
+}
