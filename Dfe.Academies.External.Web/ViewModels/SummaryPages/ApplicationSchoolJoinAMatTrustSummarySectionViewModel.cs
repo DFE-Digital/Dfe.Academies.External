d647aadf0675b79e7fd4588859ec10d19c4b94d7
mark robinson <mark.robinson@education.gov.uk>
2022-11-07 09:19:44 +0000

    new view models

---
+++ Dfe.Academies.External.Web/ViewModels/SummaryPages/ApplicationSchoolJoinAMatTrustSummarySectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels.SummaryPages
+{
+	public sealed class ApplicationSchoolJoinAMatTrustSummarySectionViewModel : SchoolQuestionAndAnswerViewModel
+	{
+		public const string NameOfTheTrust = "The name of the trust";
+		public const string TrustConsentEvidenceDoc = "Upload evidence that the trust consents to the school joining";
+
+		//ChangesToLaGovernance
+
+		public ApplicationSchoolJoinAMatTrustSummarySectionViewModel(string name, string answer) : base(name, answer)
+		{
+		}
+	}
+}
