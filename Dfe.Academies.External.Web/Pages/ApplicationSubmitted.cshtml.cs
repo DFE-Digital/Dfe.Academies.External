255eef85c184fca6375c9d7efedadf2a76c09fdb
mark robinson <mark.robinson@education.gov.uk>
2022-11-09 10:53:13 +0000

    stub page based upon existing  Submitted.chtml

---
+++ Dfe.Academies.External.Web/Pages/ApplicationSubmitted.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages
+{
+    public class ApplicationSubmittedModel : BasePageEditModel
+	{
+		//// Below are props for UI display
+		// TODO:- model props
+		public string ApplicationReferenceNumber { get; private set; } = string.Empty;
+
+		// List<ApplicationMilestone> = to replicate @ViewData["MilestonesConfig"]"/>
+
+		public ApplicationSubmittedModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, IReferenceDataRetrievalService referenceDataRetrievalService) 
+	        : base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+        {
+        }
+
+		public async Task<ActionResult> OnGetAsync(int appId)
+		{
+			// check user access
+			var checkStatus = await CheckApplicationPermission(appId);
+
+			if (checkStatus is ForbidResult)
+			{
+				return RedirectToPage("ApplicationAccessException");
+			}
+
+			var draftConversionApplication = await LoadAndSetApplicationDetails(appId);
+
+			if (draftConversionApplication == null)
+			{
+				return Page();
+			}
+
+			PopulateUiModel(draftConversionApplication);
+
+			return Page();
+		}
+
+		private void PopulateUiModel(ConversionApplication? conversionApplication)
+		{
+			if (conversionApplication != null)
+			{
+				//ApplicationId = conversionApplication.ApplicationId;
+				//ApplicationType = conversionApplication.ApplicationType;
+				ApplicationReferenceNumber = conversionApplication.ApplicationReference;
+				//NameOfTrustToJoin = conversionApplication.TrustName;
+			}
+		}
+
+		///<inheritdoc/>
+		public override bool RunUiValidation()
+		{
+			// does not apply on this page
+			return true;
+		}
+
+		///<inheritdoc/>
+		public override void PopulateValidationMessages()
+		{
+			PopulateViewDataErrorsWithModelStateErrors();
+		}
+
+		///<inheritdoc/>
+		public override Dictionary<string, dynamic> PopulateUpdateDictionary()
+		{
+			// does not apply on this page
+			return new();
+		}
+	}
+}
