2110fb27a83cd0d8ba12d7af8e9281d0e8f095d2
mark robinson <mark.robinson@education.gov.uk>
2022-11-09 11:41:52 +0000

    configured milestones from as-is app

--- Dfe.Academies.External.Web/ViewModels/ApplicationMilestonesViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/ApplicationMilestonesViewModel.cs
@@ -4,69 +4,19 @@ public sealed class ApplicationMilestonesViewModel
 {
 	public ApplicationMilestonesViewModel()
 	{
-		ApplicationMilestones = new();
-
-		// TODO:- setup each milestone here as they are hard coded !!!!
-		//ApplicationMilestones.Add(new ApplicationMilestoneViewModel());
-
-		//'Sections':[
-
-		//			{
-		//	'SectionName':'ApplicationSubmission',
-		//                      'SectionDisplayName':'Application submission',
-		//			},
-		//                  {
-		//	'SectionName':'RSCDecision',
-		//                      'SectionDisplayName':'Regional schools commissioner makes a decision with advice from the Headteacher Board',
-		//			},
-		//                  {
-		//	'SectionName':'AcademyOrder',
-		//                      'SectionDisplayName':'Academy order is issued',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'SubmitLandQuestionnaire',
-		//                      'SectionDisplayName':'School’s solicitor submits a land questionnaire, including site plan',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'SubmitDraftFundingAgreement',
-		//                      'SectionDisplayName':'School’s solicitor submits draft funding agreement (and memorandum and articles of association for new trusts)',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'ConfirmCTA',
-		//                      'SectionDisplayName':'School’s solicitor confirms that the commercial transfer agreement (CTA) and land arrangements are agreed',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'SignFundingAgreement',
-		//                      'SectionDisplayName':'School signs and submits the funding agreement',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'ConfirmTUPE',
-		//                      'SectionDisplayName':'School’s solicitor confirms TUPE and stakeholder consultation are complete',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'SubmitBankDetails',
-		//                      'SectionDisplayName':'School submits the academy bank details to ESFA',
-		//                      'DynamicsFields':''
-
-		//			},
-		//                  {
-		//	'SectionName':'AcademyOpens',
-		//                      'SectionDisplayName':'Academy opens',
-		//                      'DynamicsFields':''
-
-		//			}]
+		ApplicationMilestones = new()
+			{
+				new ApplicationMilestoneViewModel("ApplicationSubmission", "Application submission"),
+				new ApplicationMilestoneViewModel("RSCDecision","Regional schools commissioner makes a decision with advice from the Headteacher Board"),
+				new ApplicationMilestoneViewModel("AcademyOrder","Academy order is issued"),
+				new ApplicationMilestoneViewModel("SubmitLandQuestionnaire","School’s solicitor submits a land questionnaire, including site plan"),
+				new ApplicationMilestoneViewModel("SubmitDraftFundingAgreement","School’s solicitor submits draft funding agreement (and memorandum and articles of association for new trusts)"),
+				new ApplicationMilestoneViewModel("ConfirmCTA","School’s solicitor confirms that the commercial transfer agreement (CTA) and land arrangements are agreed"),
+				new ApplicationMilestoneViewModel("SignFundingAgreement","School signs and submits the funding agreement"),
+				new ApplicationMilestoneViewModel("ConfirmTUPE","School’s solicitor confirms TUPE and stakeholder consultation are complete"),
+				new ApplicationMilestoneViewModel("SubmitBankDetails","School submits the academy bank details to ESFA"),
+				new ApplicationMilestoneViewModel("AcademyOpens","Academy Opens")
+			};
 	}
 
 	public List<ApplicationMilestoneViewModel> ApplicationMilestones { get; set; }
