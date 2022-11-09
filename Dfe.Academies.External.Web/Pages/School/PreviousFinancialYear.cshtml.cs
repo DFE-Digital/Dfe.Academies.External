0cfc98dbc87bd426ed7d0ff31a1575ec4b4a2483
plockwood <paul.lockwood@education.gov.uk>
2022-11-09 14:28:59 +0000

    AB#111385 - validate file types and file size

--- Dfe.Academies.External.Web/Pages/School/PreviousFinancialYear.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/School/PreviousFinancialYear.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Dfe.Academies.External.Web.Attributes;
+using Dfe.Academies.External.Web.CustomValidators;
 using Dfe.Academies.External.Web.Enums;
 using Dfe.Academies.External.Web.Helpers;
 using Dfe.Academies.External.Web.Models;
@@ -41,11 +42,17 @@ namespace Dfe.Academies.External.Web.Pages.School
 		[BindProperty]
 		public string? PFYRevenueStatusExplained { get; set; }
 
+		[DataType(DataType.Upload)]
+		[MaxFileSize(5 * 1024 * 1024)]
+		[AllowedExtensions(new[] { ".doc", ".docx", "ppt", ".pptx", "pdf" })]
 		public List<IFormFile>? SchoolPFYRevenueStatusFiles { get; set; } = new();
 
 		[BindProperty]
 		public List<string> SchoolPFYRevenueStatusFileNames { get; set; }
 
+		[DataType(DataType.Upload)]
+		[MaxFileSize(5 * 1024 * 1024)]
+		[AllowedExtensions(new[] { ".doc", ".docx", "ppt", ".pptx", "pdf" })]
 		public List<IFormFile>? SchoolPFYCapitalForwardStatusFiles { get; set; } = new();
 
 		[BindProperty]
