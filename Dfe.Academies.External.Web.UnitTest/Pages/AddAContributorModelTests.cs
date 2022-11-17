4f33b5c09f355247dab3077989979119abcd76bf
mark robinson <mark.robinson@education.gov.uk>
2022-11-17 10:56:12 +0000

    adding usings

--- Dfe.Academies.External.Web.UnitTest/Pages/AddAContributorModelTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Pages/AddAContributorModelTests.cs
@@ -1,4 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System.Threading.Tasks;
+using Dfe.Academies.External.Web.Pages;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
 
 namespace Dfe.Academies.External.Web.UnitTest.Pages;
 
