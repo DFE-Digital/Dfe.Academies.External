a5e3c58c4eb3b60a74885a7cc9ebd8c8cebd206c
mark robinson <mark.robinson@education.gov.uk>
2022-11-18 18:15:57 +0000

    beginning of tests

---
+++ Dfe.Academies.External.Web.UnitTest/Services/ConversionApplicationRetrievalServiceTrustStatusLogicTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Moq.Protected;
+using System.Net.Http;
+using System.Threading;
+using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.UnitTest.Services;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ConversionApplicationRetrievalServiceTrustStatusLogicTests
+{
+	[Test]
+	public async Task CalculateTrustStatus___ConversionApplicationNullReturns___NotStarted()
+	{
+		// arrange
+		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getApplicationResponse.json";
+		string expectedJson = await File.ReadAllTextAsync(fullFilePath);
+		var mockFactory = SetupMockHttpClientFactory(HttpStatusCode.OK, expectedJson);
+
+		var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+		var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+
+		// act
+		var trustStatus = applicationRetrievalService.CalculateTrustStatus(null);
+
+		// assert
+		Assert.That(trustStatus, Is.EqualTo(Status.NotStarted));
+	}
+
+	// TODO:- conversionApplication != null && conversionApplication.JoinTrustDetails == null
+
+	[Test]
+	public async Task CalculateTrustStatus___JoinTrustDetailsNullReturns___NotStarted()
+	{
+		// arrange
+		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getApplicationResponse.json";
+		string expectedJson = await File.ReadAllTextAsync(fullFilePath);
+		var mockFactory = SetupMockHttpClientFactory(HttpStatusCode.OK, expectedJson);
+
+		var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+		var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+		
+		var conversionApplication = ConversionApplicationTestDataFactory.BuildNewConversionApplicationNoRoles();
+
+		// act
+		var trustStatus = applicationRetrievalService.CalculateTrustStatus(conversionApplication);
+
+		// assert
+		Assert.That(trustStatus, Is.EqualTo(Status.NotStarted));
+	}
+
+	// TODO:- conversionApplication != null && conversionApplication.JoinTrustDetails != null (but empty)
+
+	// TODO:- Status.InProgress - section 1 = done = conversionApplication.JoinTrustDetails.TrustName ONLY
+
+	// TODO:- Status.InProgress - section 2 = done = conversionApplication.JoinTrustDetails!.ChangesToTrust.HasValue  
+
+	// TODO:- Status.InProgress - section 3 = done conversionApplication.JoinTrustDetails!.ChangesToLaGovernance.HasValue ONLY
+
+	// TODO:- Status.Completed - all 3 sections complete
+
+	private static Mock<IHttpClientFactory> SetupMockHttpClientFactory(HttpStatusCode expectedStatusCode, string expectedJson)
+	{
+		var mockFactory = new Mock<IHttpClientFactory>();
+
+		var mockMessageHandler = new Mock<HttpMessageHandler>();
+		mockMessageHandler.Protected()
+			.Setup<Task<HttpResponseMessage>>("SendAsync",
+				ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+			.ReturnsAsync(new HttpResponseMessage
+			{
+				StatusCode = expectedStatusCode,
+				Content = new StringContent(expectedJson)
+			});
+
+		var httpClient = new HttpClient(mockMessageHandler.Object);
+		httpClient.BaseAddress = new Uri(APIConstants.AcademiesAPITestUrl);
+
+		mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+		return mockFactory;
+	}
+}
