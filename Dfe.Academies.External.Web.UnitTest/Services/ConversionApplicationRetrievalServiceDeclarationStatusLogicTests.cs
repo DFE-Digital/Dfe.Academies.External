d28fc65610bac37d363017ca9ec5fcd5e0458536
mark robinson <mark.robinson@education.gov.uk>
2022-11-21 10:40:43 +0000

    tests for CalculateApplicationDeclarationStatus

---
+++ Dfe.Academies.External.Web.UnitTest/Services/ConversionApplicationRetrievalServiceDeclarationStatusLogicTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Dfe.Academies.External.Web.Enums;
+
+namespace Dfe.Academies.External.Web.UnitTest.Services;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ConversionApplicationRetrievalServiceDeclarationStatusLogicTests
+{
+	/// <summary>
+	/// conversionApplication == null
+	/// </summary>
+	/// <returns></returns>
+	[Test]
+	public async Task CalculateDeclarationStatus___ConversionApplicationNullReturns___NotStarted()
+	{
+		// arrange
+		string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getApplicationResponse.json";
+		string expectedJson = await File.ReadAllTextAsync(fullFilePath);
+		var mockFactory = MockHttpClientFactory.SetupMockHttpClientFactory(HttpStatusCode.OK, expectedJson);
+
+		var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+		var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
+
+		// act
+		var declarationStatus = applicationRetrievalService.CalculateApplicationDeclarationStatus(null);
+
+		// assert
+		Assert.That(declarationStatus, Is.EqualTo(Status.NotStarted));
+	}
+
+	// TODO:- conversionApplication.ApplicationType == ApplicationTypes.JoinAMat - without school
+
+	// TODO:-conversionApplication.ApplicationType == ApplicationTypes.JoinAMat - with school
+
+	// TODO:- add tests for CalculateApplicationDeclarationStatus() = returns Completed = selectedSchool?.DeclarationBodyAgree.HasValue 
+
+	// TODO:-conversionApplication.ApplicationType == ApplicationTypes.FormAMat
+}
