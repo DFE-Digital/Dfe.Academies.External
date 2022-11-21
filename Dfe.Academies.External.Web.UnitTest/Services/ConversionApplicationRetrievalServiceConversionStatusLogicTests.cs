c65f5626304bddfbc141db7a45c75bb47d06f0a5
mark robinson <mark.robinson@education.gov.uk>
2022-11-21 14:50:34 +0000

    added a new test

--- Dfe.Academies.External.Web.UnitTest/Services/ConversionApplicationRetrievalServiceConversionStatusLogicTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Services/ConversionApplicationRetrievalServiceConversionStatusLogicTests.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Threading.Tasks;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace Dfe.Academies.External.Web.UnitTest.Services
 {
@@ -76,7 +77,7 @@ namespace Dfe.Academies.External.Web.UnitTest.Services
 			var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
 			var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
 
-			var conversionApplication = ConversionApplicationTestDataFactory.BuildJoinAMatConversionApplicationWithContributorWithSchool();
+			var conversionApplication = ConversionApplicationTestDataFactory.BuildJoinAMatConversionApplicationWithContributorWithSchool(null);
 
 			// act
 			var applicationStatus = applicationRetrievalService.CalculateApplicationStatus(conversionApplication);
@@ -84,10 +85,35 @@ namespace Dfe.Academies.External.Web.UnitTest.Services
 			// assert
 			Assert.That(applicationStatus, Is.EqualTo(Status.NotStarted));
 		}
+		
+		/// <summary>
+		/// school.SchoolApplicationComponents.any = true
+		/// ApplicationType == ApplicationTypes.JoinAMat && trust status = not started && conversion status = not started
+		/// </summary>
+		/// <returns></returns>
+		[Test]
+		public async Task CalculateApplicationStatus___ApplicationTypeJoinAMatAndSchoolAndComponents___Returns___NotStarted()
+		{
+			// arrange
+			string fullFilePath = @$"{AppDomain.CurrentDomain.BaseDirectory}ExampleJsonResponses/getApplicationResponseBasicJoinAMat.json";
+			string expectedJson = await File.ReadAllTextAsync(fullFilePath);
+			var mockFactory = MockHttpClientFactory.SetupMockHttpClientFactory(HttpStatusCode.OK, expectedJson);
+			var mockLogger = new Mock<ILogger<ConversionApplicationRetrievalService>>();
+			var applicationRetrievalService = new ConversionApplicationRetrievalService(mockFactory.Object, mockLogger.Object);
 
-		// school.SchoolApplicationComponents.any = true
-		// TODO:- ApplicationType == ApplicationTypes.JoinAMat && trust status = not started && conversion status = not started
+			int applicationId = 25; // hard coded as per example JSON
+			int URN = 113537;
 
+			var conversionApplication = ConversionApplicationTestDataFactory.BuildJoinAMatConversionApplicationWithContributorWithSchool(applicationId);
+			var school = conversionApplication.Schools.FirstOrDefault();
+			school!.SchoolApplicationComponents = await applicationRetrievalService.GetSchoolApplicationComponents(applicationId, URN);
+
+			// act
+			var applicationStatus = applicationRetrievalService.CalculateApplicationStatus(conversionApplication);
+
+			// assert
+			Assert.That(applicationStatus, Is.EqualTo(Status.NotStarted));
+		}
 
 
 		// TODO:- ApplicationType == ApplicationTypes.JoinAMat && trust status = not started && conversion status = in prog
