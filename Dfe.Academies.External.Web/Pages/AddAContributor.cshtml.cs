7a3c3e31a4fdcf08d9323e23eb5f19fb2f0892b8
mark robinson <mark.robinson@education.gov.uk>
2022-11-22 13:03:26 +0000

    fianlised validation message

--- Dfe.Academies.External.Web/Pages/AddAContributor.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/AddAContributor.cshtml.cs
@@ -170,7 +170,7 @@ namespace Dfe.Academies.External.Web.Pages
 			string currentUserEmail = User.FindFirst(ClaimTypes.Email)?.Value ?? "";
 			if (EmailAddress?.Trim().ToLower() == currentUserEmail.Trim().ToLower())
 			{
-				ModelState.AddModelError("InvitedYourself", "You can't invite yourself");
+				ModelState.AddModelError("InvitedYourself", "You cannot invite yourself as a contributor");
 				PopulateValidationMessages();
 				// MR:- need to call below otherwise will lose ExistingContributors()
 				PopulateUiModel(draftConversionApplication);
