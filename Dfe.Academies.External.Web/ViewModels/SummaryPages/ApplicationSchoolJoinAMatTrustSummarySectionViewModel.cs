bd2aecfc3b1b5f5bc6dd3a964713ea8dcad3d39a
mark robinson <mark.robinson@education.gov.uk>
2022-11-24 11:37:25 +0000

    removed TODO

--- Dfe.Academies.External.Web/ViewModels/SummaryPages/ApplicationSchoolJoinAMatTrustSummarySectionViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/SummaryPages/ApplicationSchoolJoinAMatTrustSummarySectionViewModel.cs
@@ -7,8 +7,6 @@
 		public const string ChangesToTrustGovernance = "Will there be any changes to the governance of the trust due to the school joining?";
 		public const string ChangesToLaGovernance = "Will there be any changes at a local level due to this school joining?";
 
-		//ChangesToLaGovernance
-
 		public ApplicationSchoolJoinAMatTrustSummarySectionViewModel(string name, string answer) : base(name, answer)
 		{
 		}
