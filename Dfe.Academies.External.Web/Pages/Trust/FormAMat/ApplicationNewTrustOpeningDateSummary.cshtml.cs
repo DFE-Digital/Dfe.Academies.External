de10fb9ce0910f4bf1585d9dc7c2d30cf85b94f7
mark robinson <mark.robinson@education.gov.uk>
2022-11-24 11:27:59 +0000

    more stubbing

--- Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummary.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummary.cshtml.cs
@@ -41,9 +41,27 @@ namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 	        if (conversionApplication != null && conversionApplication.FormTrustDetails != null)
 	        {
 		        TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
-		        
+
 				// TODO:- pop summary VM - same as other summary pages
-	        }
+				//ApplicationPreOpeningSupportGrantHeadingViewModel heading1 = new(ApplicationPreOpeningSupportGrantHeadingViewModel.Heading, // heading = 'Details'
+				//	"/Trust/FormAMat/ApplicationNewTrustOpeningDate")
+				//{
+				//	Status = conversionApplication.FormTrustDetails.FormTrustOpeningDate.HasValue ?
+				//		SchoolConversionComponentStatus.Complete
+				//		: SchoolConversionComponentStatus.NotStarted
+				//};
+
+				//heading1.Sections.Add(new(
+				//	ApplicationPreOpeningSupportGrantSectionViewModel.FundsSchoolOrTrust,
+				//	!conversionApplication.FormTrustDetails.FormTrustOpeningDate.HasValue ?
+				//		QuestionAndAnswerConstants.NoInfoAnswer :
+				//		conversionApplication.FormTrustDetails.FormTrustOpeningDate.Value.ToString("dd/MM/yyyy") ?? string.Empty
+				//));
+
+				//var vm = new List<ApplicationPreOpeningSupportGrantHeadingViewModel> { heading1 };
+
+				//ViewModel = vm;
+			}
 		}
 	}
 }
