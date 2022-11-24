34fd2fede086cd736f7cfd58a2bff485f4b52499
mark robinson <mark.robinson@education.gov.uk>
2022-11-24 11:17:25 +0000

    new FAM opening date summary page - https://dfe-gov-uk.visualstudio.com.mcas.ms/Academies-and-Free-Schools-SIP/_workitems/edit/107013

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummaryModelTests.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Dfe.Academies.External.Web.Pages.Trust.FormAMat;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages.Trust.FormAMat;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ApplicationNewTrustOpeningDateSummaryModelTests
+{
+	// TODO:-
+}
---
+++ Dfe.Academies.External.Web/Pages/Base/BaseTrustFAMApplicationSummaryPageModel.cs
@@ -0,0 +1,48 @@
+﻿using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages.Base
+{
+	public abstract class BaseTrustFamApplicationSummaryPageModel : BasePageEditModel
+	{
+		[BindProperty]
+		public int ApplicationId { get; set; }
+
+		public string TrustName { get; set; } = string.Empty;
+
+		protected BaseTrustFamApplicationSummaryPageModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService,
+			IReferenceDataRetrievalService referenceDataRetrievalService)
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+		{
+		}
+
+		public virtual async Task<ActionResult> OnGetAsync(int urn, int appId)
+		{
+			// MR:- don't need try/catch anymore as we have exception middleware
+			LoadAndStoreCachedConversionApplication();
+
+			// check user access
+			var checkStatus = await CheckApplicationPermission(appId);
+
+			if (checkStatus is ForbidResult)
+			{
+				return RedirectToPage("../ApplicationAccessException");
+			}
+
+			ApplicationId = appId;
+			
+			var conversionApplication = TempDataHelper.GetSerialisedValue<ConversionApplication>(TempDataHelper.DraftConversionApplicationKey, TempData) ?? new ConversionApplication();
+
+			PopulateUiModel(conversionApplication);
+
+			return Page();
+		}
+
+		/// <summary>
+		/// take application data from API and populate UI controls
+		/// </summary>
+		/// <param name="conversionApplication"></param>
+		public abstract void PopulateUiModel(ConversionApplication? conversionApplication);
+	}
+}
