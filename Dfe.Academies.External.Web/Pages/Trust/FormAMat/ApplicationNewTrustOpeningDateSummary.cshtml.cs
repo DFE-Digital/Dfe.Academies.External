36e653e7a2151b3c2ba8f572eeac81d511de4d70
mark robinson <mark.robinson@education.gov.uk>
2022-11-24 11:38:57 +0000

    view models for ApplicationNewTrustOpeningDateSummary page

--- Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummary.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummary.cshtml.cs
@@ -1,6 +1,9 @@
-﻿using Dfe.Academies.External.Web.Models;
+﻿using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.Models;
 using Dfe.Academies.External.Web.Pages.Base;
 using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.ViewModels;
+using Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
 
 namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 {
@@ -9,7 +12,10 @@ namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 	/// </summary>
 	public class ApplicationNewTrustOpeningDateSummaryModel : BaseTrustFamApplicationSummaryPageModel
 	{
-        public ApplicationNewTrustOpeningDateSummaryModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
+		//// MR:- VM props to show school conversion data
+		public List<ApplicationNewTrustOpeningDateHeadingViewModel> ViewModel { get; set; } = new();
+
+		public ApplicationNewTrustOpeningDateSummaryModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
 													IReferenceDataRetrievalService referenceDataRetrievalService) 
 	        : base(conversionApplicationRetrievalService, referenceDataRetrievalService)
         {
@@ -42,25 +48,24 @@ namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 	        {
 		        TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
 
-				// TODO:- pop summary VM - same as other summary pages
-				//ApplicationPreOpeningSupportGrantHeadingViewModel heading1 = new(ApplicationPreOpeningSupportGrantHeadingViewModel.Heading, // heading = 'Details'
-				//	"/Trust/FormAMat/ApplicationNewTrustOpeningDate")
-				//{
-				//	Status = conversionApplication.FormTrustDetails.FormTrustOpeningDate.HasValue ?
-				//		SchoolConversionComponentStatus.Complete
-				//		: SchoolConversionComponentStatus.NotStarted
-				//};
+				ApplicationNewTrustOpeningDateHeadingViewModel heading1 = new(ApplicationNewTrustOpeningDateHeadingViewModel.Heading, // heading = 'Details'
+					"/Trust/FormAMat/ApplicationNewTrustOpeningDate")
+				{
+					Status = conversionApplication.FormTrustDetails.FormTrustOpeningDate.HasValue ?
+						SchoolConversionComponentStatus.Complete
+						: SchoolConversionComponentStatus.NotStarted
+				};
 
-				//heading1.Sections.Add(new(
-				//	ApplicationPreOpeningSupportGrantSectionViewModel.FundsSchoolOrTrust,
-				//	!conversionApplication.FormTrustDetails.FormTrustOpeningDate.HasValue ?
-				//		QuestionAndAnswerConstants.NoInfoAnswer :
-				//		conversionApplication.FormTrustDetails.FormTrustOpeningDate.Value.ToString("dd/MM/yyyy") ?? string.Empty
-				//));
+				heading1.Sections.Add(new(
+					ApplicationNewTrustOpeningDateSectionViewModel.OpeningDate,
+					!conversionApplication.FormTrustDetails.FormTrustOpeningDate.HasValue ?
+						QuestionAndAnswerConstants.NoInfoAnswer :
+						conversionApplication.FormTrustDetails.FormTrustOpeningDate.Value.ToString("dd/MM/yyyy")
+				));
 
-				//var vm = new List<ApplicationPreOpeningSupportGrantHeadingViewModel> { heading1 };
+				var vm = new List<ApplicationNewTrustOpeningDateHeadingViewModel> { heading1 };
 
-				//ViewModel = vm;
+				ViewModel = vm;
 			}
 		}
 	}
---
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustOpeningDateSectionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels.TrustSummaryPages
+{
+	public sealed class ApplicationNewTrustOpeningDateSectionViewModel : SchoolQuestionAndAnswerViewModel
+	{
+		public const string OpeningDate = "The name of the trust"; // TODO:- fix up
+
+		public ApplicationNewTrustOpeningDateSectionViewModel(string name, string answer) : base(name, answer)
+		{
+		}
+	}
+}
