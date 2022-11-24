8e18aca2aeae97b69a3ec07a53c6350d9cb3a5ba
mark robinson <mark.robinson@education.gov.uk>
2022-11-24 11:30:08 +0000

    add tests

--- Dfe.Academies.External.Web.UnitTest/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummaryModelTests.cs
+++ Dfe.Academies.External.Web.UnitTest/Pages/Trust/FormAMat/ApplicationNewTrustOpeningDateSummaryModelTests.cs
@@ -15,5 +15,49 @@ namespace Dfe.Academies.External.Web.UnitTest.Pages.Trust.FormAMat;
 [Parallelizable(ParallelScope.All)]
 internal sealed class ApplicationNewTrustOpeningDateSummaryModelTests
 {
-	// TODO:-
+	private static readonly Fixture Fixture = new();
+
+	/// <summary>
+	/// "draftConversionApplication" in temp storage
+	/// from previous step in the new application wizard
+	/// </summary>
+	/// <returns></returns>
+	[Test]
+	public async Task OnGetAsync___Valid___NullErrors()
+	{
+		// arrange
+		var draftConversionApplicationStorageKey = TempDataHelper.DraftConversionApplicationKey;
+		var mockConversionApplicationRetrievalService = new Mock<IConversionApplicationRetrievalService>();
+		var mockReferenceDataRetrievalService = new Mock<IReferenceDataRetrievalService>();
+		int applicationId = Fixture.Create<int>();
+
+		var conversionApplication = ConversionApplicationTestDataFactory.BuildNewConversionApplicationWithChairRole();
+
+		// act
+		var pageModel = SetupApplicationNewTrustOpeningDateModel(mockConversionApplicationRetrievalService.Object,
+			mockReferenceDataRetrievalService.Object);
+		TempDataHelper.StoreSerialisedValue(draftConversionApplicationStorageKey, pageModel.TempData, conversionApplication);
+
+		// act
+		await pageModel.OnGetAsync(applicationId);
+
+		// assert
+		Assert.That(pageModel.TempData["Errors"], Is.EqualTo(null));
+	}
+
+	private static ApplicationNewTrustOpeningDateSummaryModel SetupApplicationNewTrustOpeningDateModel(
+		IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+		IReferenceDataRetrievalService mockReferenceDataRetrievalService,
+		bool isAuthenticated = false)
+	{
+		(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+		return new ApplicationNewTrustOpeningDateSummaryModel(mockConversionApplicationRetrievalService, mockReferenceDataRetrievalService)
+		{
+			PageContext = pageContext,
+			TempData = tempData,
+			Url = new UrlHelper(actionContext),
+			MetadataProvider = pageContext.ViewData.ModelMetadata
+		};
+	}
 }
