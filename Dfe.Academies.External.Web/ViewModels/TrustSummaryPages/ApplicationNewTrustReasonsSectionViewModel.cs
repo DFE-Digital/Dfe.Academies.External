f1028c4cc942764a1065962861f8e7bacae3446c
mark robinson <mark.robinson@education.gov.uk>
2022-11-29 16:48:54 +0000

    finalised section headings

--- Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustReasonsSectionViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustReasonsSectionViewModel.cs
@@ -2,9 +2,11 @@
 {
 	public class ApplicationNewTrustReasonsSectionViewModel : SchoolQuestionAndAnswerViewModel
 	{
-		public const string OpeningDate = "When do the schools plan to establish the new multi-academy trust?";
-		public const string ApproverFullname = "Approver full name";
-		public const string ApproverEmail = "Approver email address";
+		public const string WhyForming = "Why are the schools forming the trust";
+		public const string Vision = "What vision and aspiration have the schools agreed for the trust?";
+		public const string GeoAreas = "What geographical areas and communities will the trust service?";
+		public const string Freedom = "How will the schools make the most of the freedoms that academies have?";
+		public const string ImproveTeaching = "How will the schools work together to improve teaching and learning in the trust and potentially support other schools beyond the trust?";
 
 		public ApplicationNewTrustReasonsSectionViewModel(string name, string answer) : base(name, answer)
 		{
