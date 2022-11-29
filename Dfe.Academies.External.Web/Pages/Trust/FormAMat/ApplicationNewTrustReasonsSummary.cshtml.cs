3f11652602ec0809fe0918d1557f5baf3449a664
mark robinson <mark.robinson@education.gov.uk>
2022-11-29 16:49:03 +0000

    wire up page to VM

--- Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustReasonsSummary.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustReasonsSummary.cshtml.cs
@@ -1,14 +1,17 @@
-﻿using Dfe.Academies.External.Web.Models;
+﻿using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.Models;
 using Dfe.Academies.External.Web.Pages.Base;
 using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.ViewModels;
+using Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
 
 namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 {
     public class ApplicationNewTrustReasonsSummaryModel : BaseTrustFamApplicationSummaryPageModel
 	{
 		//// MR:- VM props to show data
-		// TODO:-
-		
+		public List<ApplicationNewTrustReasonsHeadingViewModel> ViewModel { get; set; } = new();
+
 		public ApplicationNewTrustReasonsSummaryModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
 												IReferenceDataRetrievalService referenceDataRetrievalService) 
 	        : base(conversionApplicationRetrievalService, referenceDataRetrievalService)
@@ -42,8 +45,34 @@ namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 	        {
 		        TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
 
-				// TODO:- setup VM
-	        }
+		        ApplicationNewTrustReasonsHeadingViewModel heading1 = new(ApplicationNewTrustReasonsHeadingViewModel.Heading, // heading = 'Details'
+					"/Trust/FormAMat/ApplicationNewTrustReasons")
+		        {
+			        Status = !string.IsNullOrWhiteSpace(conversionApplication.FormTrustDetails.FormTrustReasonForming) ?
+				        SchoolConversionComponentStatus.Complete
+				        : SchoolConversionComponentStatus.NotStarted
+		        };
+
+		        heading1.Sections.Add(new(
+			        ApplicationNewTrustReasonsSectionViewModel.WhyForming,
+					(!string.IsNullOrWhiteSpace(conversionApplication.FormTrustDetails.FormTrustReasonForming) ?
+						conversionApplication.FormTrustDetails.FormTrustReasonForming :
+						QuestionAndAnswerConstants.NoInfoAnswer)
+				));
+
+		        heading1.Sections.Add(new(
+			        ApplicationNewTrustReasonsSectionViewModel.WhyForming,
+			        (!string.IsNullOrWhiteSpace(conversionApplication.FormTrustDetails.FormTrustReasonForming) ?
+				        conversionApplication.FormTrustDetails.FormTrustReasonForming :
+				        QuestionAndAnswerConstants.NoInfoAnswer)
+		        ));
+
+				// TODO:- setup VM - 5 sub sections - 1 header
+
+				var vm = new List<ApplicationNewTrustReasonsHeadingViewModel> { heading1 };
+
+				ViewModel = vm;
+			}
         }
 	}
 }
