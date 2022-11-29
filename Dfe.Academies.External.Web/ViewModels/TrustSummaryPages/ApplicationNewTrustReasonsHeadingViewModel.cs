f2800936b4503141f0f25d6de002e59f0357fd25
mark robinson <mark.robinson@education.gov.uk>
2022-11-29 16:37:24 +0000

    new view models for summary page

---
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustReasonsHeadingViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels.TrustSummaryPages
+{
+	public class ApplicationNewTrustReasonsHeadingViewModel : SchoolQuestionAndAnswerSectionViewModel
+	{
+		public const string Heading = "Details";
+
+		public ApplicationNewTrustReasonsHeadingViewModel(string title, string uRi) : base(title, uRi)
+		{
+			Sections = new();
+		}
+
+		public List<ApplicationNewTrustReasonsSectionViewModel> Sections { get; set; }
+	}
+}
