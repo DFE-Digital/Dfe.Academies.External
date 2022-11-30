80fa097226337303b3fda8c17bd8139e9581e6b7
mark robinson <mark.robinson@education.gov.uk>
2022-11-30 13:00:23 +0000

    new view models for new summary page

---
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustImprovementStrategySectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
+
+public sealed class ApplicationNewTrustImprovementStrategySectionViewModel : SchoolQuestionAndAnswerViewModel
+{
+	// TODO:- fix text !
+	public const string WhyForming = "Why are the schools forming the trust";
+	public const string Vision = "What vision and aspiration have the schools agreed for the trust?";
+	public const string GeoAreas = "What geographical areas and communities will the trust service?";
+
+	public ApplicationNewTrustImprovementStrategySectionViewModel(string title, string uRi) : base(title, uRi)
+	{
+	}
+}
