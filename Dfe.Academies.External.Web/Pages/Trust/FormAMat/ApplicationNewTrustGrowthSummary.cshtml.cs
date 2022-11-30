796d835e3b43bda3d0274579611b3858f10b37d7
mark robinson <mark.robinson@education.gov.uk>
2022-11-30 11:01:50 +0000

    see ticket :- https://dfe-gov-uk.visualstudio.com.mcas.ms/Academies-and-Free-Schools-SIP/_workitems/edit/107020?McasTsid=26110

---
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustGrowthSummary.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+
+namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
+{
+    public class ApplicationNewTrustGrowthSummaryModel : BaseTrustFamApplicationSummaryPageModel
+	{
+		//// MR:- VM props to show data
+		
+		// TODO:-
+
+		public ApplicationNewTrustGrowthSummaryModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
+													IReferenceDataRetrievalService referenceDataRetrievalService) 
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+		{
+		}
+
+		///<inheritdoc/>
+		public override void PopulateValidationMessages()
+		{
+			PopulateViewDataErrorsWithModelStateErrors();
+		}
+
+		///<inheritdoc/>
+		public override bool RunUiValidation()
+		{
+			// does not apply on this page
+			return true;
+		}
+
+		///<inheritdoc/>
+		public override Dictionary<string, dynamic> PopulateUpdateDictionary()
+		{
+			// does not apply on this page
+			return new();
+		}
+
+		public override void PopulateUiModel(ConversionApplication? conversionApplication)
+		{
+			if (conversionApplication != null && conversionApplication.FormTrustDetails != null)
+			{
+				TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
+
+				// TODO:-
+			}
+		}
+	}
+}
