8b0564a1d6815e89a66efd63d6fd925b9f56cbf9
mark robinson <mark.robinson@education.gov.uk>
2022-11-30 12:56:05 +0000

    code tidy up

--- Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustOpeningDateSectionViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustOpeningDateSectionViewModel.cs
@@ -1,13 +1,12 @@
-﻿namespace Dfe.Academies.External.Web.ViewModels.TrustSummaryPages
-{
-	public sealed class ApplicationNewTrustOpeningDateSectionViewModel : SchoolQuestionAndAnswerViewModel
-	{
-		public const string OpeningDate = "When do the schools plan to establish the new multi-academy trust?";
-		public const string ApproverFullname = "Approver full name";
-		public const string ApproverEmail = "Approver email address";
+﻿namespace Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
 
-		public ApplicationNewTrustOpeningDateSectionViewModel(string name, string answer) : base(name, answer)
-		{
-		}
+public sealed class ApplicationNewTrustOpeningDateSectionViewModel : SchoolQuestionAndAnswerViewModel
+{
+	public const string OpeningDate = "When do the schools plan to establish the new multi-academy trust?";
+	public const string ApproverFullname = "Approver full name";
+	public const string ApproverEmail = "Approver email address";
+
+	public ApplicationNewTrustOpeningDateSectionViewModel(string name, string answer) : base(name, answer)
+	{
 	}
 }
