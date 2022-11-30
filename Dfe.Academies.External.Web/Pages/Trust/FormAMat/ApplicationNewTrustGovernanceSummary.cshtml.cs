c9d989a9b9553e2a7241c5f4d6c4fa21a6b54c4f
mark robinson <mark.robinson@education.gov.uk>
2022-11-30 15:53:26 +0000

    wired up VM, roughly

--- Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustGovernanceSummary.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustGovernanceSummary.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Dfe.Academies.External.Web.Models;
+﻿using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.Models;
 using Dfe.Academies.External.Web.Pages.Base;
 using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.ViewModels;
 using Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
 
 namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
@@ -43,7 +45,26 @@ namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
 			{
 				TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
 
-				// TODO MR:- setup VM
+				ApplicationNewTrustGovernanceHeadingViewModel heading1 = new(ApplicationNewTrustGovernanceHeadingViewModel.Heading, // heading = 'Details'
+					"/Trust/FormAMat/ApplicationNewTrustGovernance")
+				{
+					Status = !string.IsNullOrWhiteSpace(conversionApplication.FormTrustDetails.FormTrustReasonForming) ?
+						SchoolConversionComponentStatus.Complete
+						: SchoolConversionComponentStatus.NotStarted
+				};
+
+				// TODO MR:- only upload doc, how to check?
+
+				heading1.Sections.Add(new(
+					ApplicationNewTrustGovernanceSectionViewModel.StructureDocument,
+					(!string.IsNullOrWhiteSpace(conversionApplication.FormTrustDetails.FormTrustReasonForming) ?
+						conversionApplication.FormTrustDetails.FormTrustReasonForming :
+						QuestionAndAnswerConstants.NoInfoAnswer))
+				);
+
+				var vm = new List<ApplicationNewTrustGovernanceHeadingViewModel> { heading1 };
+
+				ViewModel = vm;
 			}
 		}
 	}
