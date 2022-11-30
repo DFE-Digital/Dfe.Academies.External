732d59bae60482e18a2265b54b514a3d8d336c30
mark robinson <mark.robinson@education.gov.uk>
2022-11-30 15:42:11 +0000

    see ticket:- https://dfe-gov-uk.visualstudio.com.mcas.ms/Academies-and-Free-Schools-SIP/_workitems/edit/107025

---
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustGovernanceSummary.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
+
+namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
+{
+    public class ApplicationNewTrustGovernanceSummaryModel : BaseTrustFamApplicationSummaryPageModel
+	{
+		//// MR:- VM props to show data
+		public List<ApplicationNewTrustGovernanceHeadingViewModel> ViewModel { get; set; } = new();
+
+		public ApplicationNewTrustGovernanceSummaryModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService, 
+			IReferenceDataRetrievalService referenceDataRetrievalService) 
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService)
+		{
+		}
+
+		///<inheritdoc/>
+		public override void PopulateValidationMessages()
+		{
+			PopulateViewDataErrorsWithModelStateErrors();
+		}
+
+		///<inheritdoc/>
+		public override bool RunUiValidation()
+		{
+			// does not apply on this page
+			return true;
+		}
+
+		///<inheritdoc/>
+		public override Dictionary<string, dynamic> PopulateUpdateDictionary()
+		{
+			// does not apply on this page
+			return new();
+		}
+
+		///<inheritdoc/>
+		public override void PopulateUiModel(ConversionApplication? conversionApplication)
+		{
+			if (conversionApplication != null && conversionApplication.FormTrustDetails != null)
+			{
+				TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
+
+				// TODO MR:- setup VM
+			}
+		}
+	}
+}
---
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustGovernanceSectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.Academies.External.Web.ViewModels.TrustSummaryPages;
+
+public sealed class ApplicationNewTrustGovernanceSectionViewModel : SchoolQuestionAndAnswerViewModel
+{
+	public const string StructureDocument = "Upload an A4 diagram of the proposed governance structure of the trust";
+
+	public ApplicationNewTrustGovernanceSectionViewModel(string name, string answer) : base(name, answer)
+	{
+	}
+}
