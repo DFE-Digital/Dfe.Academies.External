c72eb87498ca4b0403e9fabb489d42128118b062
plockwood <paul.lockwood@education.gov.uk>
2022-12-01 14:34:12 +0000

    Added unit test

---
+++ Dfe.Academies.External.Web.UnitTest/Pages/Trust/FormAMat/ApplicationNewTrustImprovementStrategyModelTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoFixture;
+using Dfe.Academies.External.Web.Pages.Trust.FormAMat;
+using Dfe.Academies.External.Web.Services;
+using Dfe.Academies.External.Web.UnitTest.Factories;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dfe.Academies.External.Web.UnitTest.Pages.Trust.FormAMat;
+
+[Parallelizable(ParallelScope.All)]
+internal sealed class ApplicationNewTrustImprovementStrategyModelTests
+{
+	private static readonly Fixture Fixture = new();
+
+	/// <summary>
+	/// "draftConversionApplication" in temp storage
+	/// from previous step in the new application wizard
+	/// </summary>
+	/// <returns></returns>
+	[Test]
+	public async Task OnGetAsync___Valid___NullErrors()
+	{
+		// arrange
+		var draftConversionApplicationStorageKey = TempDataHelper.DraftConversionApplicationKey;
+		var mockConversionApplicationRetrievalService = new Mock<IConversionApplicationRetrievalService>();
+		var mockReferenceDataRetrievalService = new Mock<IReferenceDataRetrievalService>();
+		var mockConversionApplicationCreationService = new Mock<IConversionApplicationCreationService>();
+		int applicationId = Fixture.Create<int>();
+
+		var conversionApplication = ConversionApplicationTestDataFactory.BuildNewConversionApplicationWithChairRole();
+
+		// act
+		var pageModel = SetupApplicationNewTrustImprovementStrategyModel(mockConversionApplicationRetrievalService.Object,
+			mockReferenceDataRetrievalService.Object,
+			mockConversionApplicationCreationService.Object);
+		TempDataHelper.StoreSerialisedValue(draftConversionApplicationStorageKey, pageModel.TempData, conversionApplication);
+
+		// act
+		await pageModel.OnGetAsync(applicationId);
+
+		// assert
+		Assert.That(pageModel.TempData["Errors"], Is.EqualTo(null));
+	}
+
+	private static ApplicationNewTrustImprovementStrategyModel SetupApplicationNewTrustImprovementStrategyModel(
+		IConversionApplicationRetrievalService mockConversionApplicationRetrievalService,
+		IReferenceDataRetrievalService mockReferenceDataRetrievalService,
+		IConversionApplicationCreationService mockConversionApplicationCreationService,
+		bool isAuthenticated = false)
+	{
+		(PageContext pageContext, TempDataDictionary tempData, ActionContext actionContext) = PageContextFactory.PageContextBuilder(isAuthenticated);
+
+		return new ApplicationNewTrustImprovementStrategyModel(mockConversionApplicationRetrievalService, mockReferenceDataRetrievalService,
+			mockConversionApplicationCreationService)
+		{
+			PageContext = pageContext,
+			TempData = tempData,
+			Url = new UrlHelper(actionContext),
+			MetadataProvider = pageContext.ViewData.ModelMetadata
+		};
+	}
+}
---
+++ Dfe.Academies.External.Web/Pages/Trust/FormAMat/ApplicationNewTrustImprovementStrategy.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Dfe.Academies.External.Web.Enums;
+using Dfe.Academies.External.Web.Models;
+using Dfe.Academies.External.Web.Pages.Base;
+using Dfe.Academies.External.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.Academies.External.Web.Pages.Trust.FormAMat
+{
+	public class ApplicationNewTrustImprovementStrategyModel : BaseTrustFamApplicationPageEditModel
+	{
+		public string TrustName { get; private set; } = string.Empty;
+
+		[BindProperty]
+		[Required(ErrorMessage = "You must provide support details")]
+		public string ImprovementSupport { get; set; } = string.Empty;
+
+		[BindProperty]
+		[Required(ErrorMessage = "You must provide strategy details")]
+		public string ImprovementStrategy { get; set; } = string.Empty;
+
+		[BindProperty]
+		[Required(ErrorMessage = "You must provide sponsor details")]
+		public string ImprovementApprovedSponsor { get; set; } = string.Empty;
+
+		public ApplicationNewTrustImprovementStrategyModel(IConversionApplicationRetrievalService conversionApplicationRetrievalService,
+												IReferenceDataRetrievalService referenceDataRetrievalService,
+												IConversionApplicationCreationService conversionApplicationCreationService)
+			: base(conversionApplicationRetrievalService, referenceDataRetrievalService, conversionApplicationCreationService,
+				"ApplicationNewTrustImprovementStrategySummary")
+		{
+		}
+
+		///<inheritdoc/>
+		public override void PopulateValidationMessages()
+		{
+			PopulateViewDataErrorsWithModelStateErrors();
+		}
+
+		///<inheritdoc/>
+		public override bool RunUiValidation()
+		{
+			if (!ModelState.IsValid)
+			{
+				PopulateValidationMessages();
+				return false;
+			}
+
+			return true;
+		}
+
+		public override Dictionary<string, dynamic> PopulateUpdateDictionary()
+		{
+			return new Dictionary<string, dynamic>
+			{
+				{ nameof(NewTrust.FormTrustImprovementSupport), ImprovementSupport },
+				{ nameof(NewTrust.FormTrustImprovementStrategy), ImprovementStrategy },
+				{ nameof(NewTrust.FormTrustImprovementApprovedSponsor), ImprovementApprovedSponsor },
+			};
+		}
+
+		public override void PopulateUiModel(ConversionApplication? conversionApplication)
+		{
+			if (conversionApplication != null && conversionApplication.FormTrustDetails != null)
+			{
+				TrustName = conversionApplication.FormTrustDetails.FormTrustProposedNameOfTrust;
+				ImprovementSupport = conversionApplication.FormTrustDetails.FormTrustImprovementSupport ?? string.Empty;
+				ImprovementStrategy = conversionApplication.FormTrustDetails.FormTrustImprovementStrategy ?? string.Empty;
+				ImprovementApprovedSponsor = conversionApplication.FormTrustDetails.FormTrustImprovementApprovedSponsor ?? string.Empty;
+			}
+		}
+	}
+}
