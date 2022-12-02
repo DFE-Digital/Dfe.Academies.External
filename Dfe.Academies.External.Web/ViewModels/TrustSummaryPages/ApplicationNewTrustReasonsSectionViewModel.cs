287366490a220d5b8a4f346d61da76b7d367f7f5
mark robinson <mark.robinson@education.gov.uk>
2022-12-02 11:43:23 +0000

    fixed wording

--- Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustReasonsSectionViewModel.cs
+++ Dfe.Academies.External.Web/ViewModels/TrustSummaryPages/ApplicationNewTrustReasonsSectionViewModel.cs
@@ -4,7 +4,7 @@ public sealed class ApplicationNewTrustReasonsSectionViewModel : SchoolQuestionA
 {
 	public const string WhyForming = "Why are the schools forming the trust";
 	public const string Vision = "What vision and aspiration have the schools agreed for the trust?";
-	public const string GeoAreas = "What geographical areas and communities will the trust service?";
+	public const string GeoAreas = "What geographical areas and communities will the trust serve?";
 	public const string Freedom = "How will the schools make the most of the freedoms that academies have?";
 	public const string ImproveTeaching = "How will the schools work together to improve teaching and learning in the trust and potentially support other schools beyond the trust?";
 
