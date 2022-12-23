1bcd1b56d07a0af88a06304bb27e2ff9365737e1
Dan Good <dangood84@me.com>
2022-12-23 13:38:50 +0000

    Adding more login tests

--- Dfe.Academies.External.Web/CypressTests/cypress/e2e/login.spec.cy.js
+++ Dfe.Academies.External.Web/CypressTests/cypress/e2e/login.spec.cy.js
@@ -3,19 +3,53 @@ import CookieHeaderModal from '../page-objects/components/CookieHeaderModal'
 import A2BHome from '../page-objects/pages/A2BHome'
 import A2BLogin from '../page-objects/pages/A2BLogin'
 
-describe('Login Success Test', () => {
+describe('Login Tests', () => {
 
-  before(function() {
+  beforeEach(function() {
     cy.visit(url)
     CookieHeaderModal.clickAcceptAnalyticsCookies()
     A2BHome.StartNowVisible()
     A2BHome.clickStartNow()
   })
 
-  it('should login into application', () => {
-   // cy.origin('https://test-interactions.signin.education.gov.uk', () => {
+  it('should NOT login into application WITH NO PASSWORD', () => {
+    A2BLogin.loginWithNoPassword(login_username, '')
+    A2BLogin.loginErrorVisibleWithNoPassword()
+  })
+
+  it('should NOT login into application WITH NO USERNAME', () => {
+    A2BLogin.loginWithNoUsername(login_username, login_password)
+    A2BLogin.loginErrorVisibleWithNoUsername()
+  })
+
+  it('should NOT login into application WITH NO USERNAME OR PASSWORD', () => {
+    A2BLogin.loginWithNoUsernameOrPassword(login_username, login_password)
+    
+    A2BLogin.loginErrorVisibleWithNoPassword()
+  })
+
+  it('should NOT login into application WITH INCORRECT PASSWORD', () => {
+    A2BLogin.loginWithWrongPassword(login_username, 'POTATO')
+    A2BLogin.loginErrorVisibleWithWrongPassword()
+  })
+
+  it('should NOT login into application WITH INCORRECT USERNAME', () => {
+    A2BLogin.loginWithWrongUsername('rachel.riley@msn.com', login_password)
+    A2BLogin.loginErrorVisibleWithWrongUsername()
+  })
+
+  it('should NOT login into application WITH SQL INJECTION IN FIELDS / INVALID USERNAME WITH PASSWORD', () => {
+    A2BLogin.sqlInjectionAndInvalidUsername(login_username, login_password)
+    A2BLogin.loginErrorVisibleWithSqlInjectionAttemptAndInvalidUsername()
+})
+
+it('should NOT login into application WITH CROSS-SITE SCRIPTING ATTEMPT / INVALID USERNAME WITH PASSWORD', () => {
+  A2BLogin.crossSiteScriptAndInvalidUsername(login_username, login_password)
+  A2BLogin.loginErrorVisibleWithCrossSiteScriptAndInvalidUsername()
+})
+
+  it('should login into application WITH CORRECT CREDENTIALS', () => {
    A2BLogin.login(login_username, login_password)
   })
 
-  })
-//})
+})
--- Dfe.Academies.External.Web/CypressTests/cypress/support/commands.js
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/commands.js
@@ -24,6 +24,70 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+Cypress.Commands.add('loginWithWrongUsername', (username, password) => {
+    //cy.get('#form-signin').should('be.visible')
+    cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
+    username = 'rachel.riley@msn.com'
+    password = 'P1ng0*1984'
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.contains('Sign in').click()
+})
+})
+
+Cypress.Commands.add('loginWithNoPassword', (username, password) => {
+    //cy.get('#form-signin').should('be.visible')
+    cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
+    username = 'dan.good@education.gov.uk'
+    password = ''
+    cy.get('#username').type(username)
+    cy.contains('Sign in').click()
+})
+})
+
+Cypress.Commands.add('loginWithNoUsername', (username, password) => {
+    //cy.get('#form-signin').should('be.visible')
+    cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
+    username = ''
+    password = 'P1ngO*1984'
+    cy.get('#password').type(password)
+    cy.contains('Sign in').click()
+})
+})
+
+
+
+Cypress.Commands.add('sqlInjectionAndInvalidUsername', (username, password) => {
+    //cy.get('#form-signin').should('be.visible')
+    cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
+    username = '\' or 1=1 --'
+    password = ''
+    cy.get('#username').type(username)
+    cy.contains('Sign in').click()
+})
+})
+
+Cypress.Commands.add('crossSiteScriptAndInvalidUsername', (username, password) => {
+    //cy.get('#form-signin').should('be.visible')
+    cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
+    username = '<script>window.alert("Hello World!")</script>'
+    password = ''
+    cy.get('#username').type(username)
+    cy.contains('Sign in').click()
+})
+})
+
+Cypress.Commands.add('loginWithWrongPassword', (username, password) => {
+    //cy.get('#form-signin').should('be.visible')
+    cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
+    username = 'dan.good@education.gov.uk'
+    password = 'POTATO'
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.contains('Sign in').click()
+})
+})
+
 Cypress.Commands.add('login', (username, password) => {
     //cy.get('#form-signin').should('be.visible')
     cy.origin('https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword', () => {
