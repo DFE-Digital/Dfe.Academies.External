d491f05e75bcfe082a0a17f622e4ff8f0d681ba4
Dan Good <dangood84@me.com>
2023-01-03 18:57:05 +0000

    Adding functions to A2BCreateAnAccount Page Object

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BCreateAnAccount.js
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BCreateAnAccount.js
@@ -1,7 +1,19 @@
 import BasePage from "../BasePage"
+import DataGenerator from "../../fixtures/data-generator"
 export default class A2BCreateAnAccount extends BasePage {
     static createAccountElementsVisible()
     {
         cy.createAccountElementsVisible()
+    }
+
+    static createAccountFailsWithNoData()
+    {
+        cy.createAccountFailsWithNoData()
+    }
+
+    static createAccountSuccessful()
+    {
+
+        cy.createAccountSuccessful()
     }
 }
