d2d502bab861327fda8d2373059c48358de5da0f
Dan Good <dangood84@me.com>
2023-01-03 19:02:19 +0000

    Renamed createAccount Spec File And Updated Tests

--- Dfe.Academies.External.Web/CypressTests/cypress/e2e/createAccount.spec.cy.js
+++ Dfe.Academies.External.Web/CypressTests/cypress/e2e/createAccount.spec.cy.js
@@ -3,6 +3,8 @@ import CookieHeaderModal from '../page-objects/components/CookieHeaderModal'
 import A2BHome from '../page-objects/pages/A2BHome'
 import A2BLogin from '../page-objects/pages/A2BLogin'
 import A2BCreateAnAccount from '../page-objects/pages/A2BCreateAnAccount'
+import DataGenerator from '../fixtures/data-generator'
+import A2BCreateAnAccountConfirm from '../page-objects/pages/A2BCreateAnAccountConfirm'
 
 describe('Create Account Tests', () => {
 
@@ -13,9 +15,17 @@ describe('Create Account Tests', () => {
     A2BHome.clickStartNow()
   })
 
-  it('should go to Create Account Page', () => {
+  it('should fail to create an account just clicking submit', () => {
     A2BLogin.createAccount()
     A2BCreateAnAccount.createAccountElementsVisible()
+    A2BCreateAnAccount.createAccountFailsWithNoData()
+  })
+
+  it('should Create an Account Successfully', () => {
+    A2BLogin.createAccount()
+    A2BCreateAnAccount.createAccountElementsVisible()
+    A2BCreateAnAccount.createAccountSuccessful()
+    A2BCreateAnAccountConfirm.createAccountConfirmElementsVisible()
    })
 
 
