c0f2f02565154a2908e44727b2a2b9f919263343
Dan Good <dangood84@me.com>
2023-01-03 18:51:38 +0000

    Making change to cypress.config.js to include option for experimentalOriginDependencies

--- Dfe.Academies.External.Web/CypressTests/cypress.config.js
+++ Dfe.Academies.External.Web/CypressTests/cypress.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
     url: 'https://webapp-t1dv-sip-a2c.azurewebsites.net',
     login_username: 'dan.good@education.gov.uk',
     login_password: 'P1ngO*1984',
+    experimentalOriginDependencies: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
