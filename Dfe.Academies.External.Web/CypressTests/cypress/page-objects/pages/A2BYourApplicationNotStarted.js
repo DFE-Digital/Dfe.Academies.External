199137e1f9a64801c0e3abc393676f760d669cec
Dan Good <dangood84@me.com>
2023-01-04 16:46:48 +0000

    Adding A2BYourApplicationNoStarted page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BYourApplicationNotStarted.js
@@ -0,0 +1,9 @@
+import BasePage from "../BasePage"
+export default class A2BYourApplicationNotStarted extends BasePage {
+    static yourApplicationNotStartedElementsVisible()
+    {
+        cy.yourApplicationNotStartedElementsVisible()
+    }
+
+
+}
