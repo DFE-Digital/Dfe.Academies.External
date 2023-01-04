b4b10277adafdf3fa20584fbcaf88cd76c3425af
Dan Good <dangood84@me.com>
2023-01-04 13:48:00 +0000

    Updating A2BYourApplications Page Object to work with yourApplicationsElementsVisible() custom command

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BYourApplications.js
@@ -0,0 +1,7 @@
+import BasePage from "../BasePage"
+export default class A2BYourApplications extends BasePage {
+    static yourApplicationsElementsVisible()
+    {
+        cy.yourApplicationsElementsVisible()
+    }
+}
