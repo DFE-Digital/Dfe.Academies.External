a83899a5bf4b2854262f5438b06a9e814c46271d
jarednimble <jared.prest@education.gov.uk>
2023-01-19 15:57:12 +0000

    Refactored email service and added join a mat templating

---
+++ Dfe.Academies.External.Web/Helpers/NotifyTemplateSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.Academies.External.Web.Helpers
+{
+	public class NotifyTemplateSettings
+	{
+		public string JamChairTemplateId { get; set; }
+		public string JamNonChairTemplateId { get; set; }
+		public string FamChairTemplateId { get; set; }
+		public string FamNonChairTemplateId { get; set; }
+	}
+}
