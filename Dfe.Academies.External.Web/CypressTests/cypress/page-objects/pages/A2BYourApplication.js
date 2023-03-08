0f2d05ed17b7807490fa3ab34b78f11b95a44f3d
Dan Good <dangood84@me.com>
2023-03-08 22:15:15 +0000

    Adding in additional functions for A2BYourApplication page object

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BYourApplication.js
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BYourApplication.js
@@ -1,9 +1,35 @@
 import BasePage from "../BasePage"
 export default class A2BYourApplication extends BasePage {
+    
+    static yourApplicationNotStartedElementsVisible()
+    {
+        cy.yourApplicationNotStartedElementsVisible()
+    }
+    
     static yourApplicationNotStartedButSchoolAddedElementsVisible()
     {
         cy.yourApplicationNotStartedButSchoolAddedElementsVisible()
     }
+
+    static selectAddATrust()
+    {
+        cy.selectAddATrust()
+    }
+
+    static yourApplicationNotStartedSelectAddASchool()
+    {
+        cy.yourApplicationNotStartedSelectAddASchool()
+    }
+
+    static selectTrustDetails() {
+        cy.selectTrustDetails()
+    }
+
+    static selectChangeSchool() {
+        cy.selectChangeSchool()
+    }
+
+
 
 
 }
