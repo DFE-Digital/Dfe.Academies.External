90d1459bc18f4c864b34abde1b9ba8050c73a723
Dan Good <dangood84@me.com>
2023-03-09 13:53:13 +0000

    Dfe.Academies.External.Web/CypressTests/config.js

--- Dfe.Academies.External.Web/CypressTests/config.js
+++ Dfe.Academies.External.Web/CypressTests/config.js
@@ -1,3 +1,5 @@
 export const url = "https://webapp-t1dv-sip-a2c.azurewebsites.net"
 export const login_username = "dan.good@education.gov.uk"
 export const login_password = "P1ngO*1984"
+
+export const unAuthUrl = "https://s184t01-a2bextcdnendpoint-ezfhhdbpembpanh5.z01.azurefd.net/application-overview?appId=10021"
