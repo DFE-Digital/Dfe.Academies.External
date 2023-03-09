699ab419ead5be231b85152d90a24b9c72e9d96a
Dan Good <dangood84@me.com>
2023-03-09 14:30:48 +0000

    Adding in SecondHalfCreateNewJAMApp.cy.js (not the finished article)

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/e2e/SecondHalfCreateNewJAMApp.cy.js
@@ -0,0 +1,125 @@
+import { url, login_username, login_password } from '../../config'
+import Header from '../page-objects/components/Header'
+import CookieHeaderModal from '../page-objects/components/CookieHeaderModal'
+import A2BHome from '../page-objects/pages/A2BHome'
+import A2BLogin from '../page-objects/pages/A2BLogin'
+import A2BYourApplication from '../page-objects/pages/A2BYourApplication'
+import A2BYourApplications from '../page-objects/pages/A2BYourApplications'
+import Footer from '../page-objects/components/Footer'
+import A2BWhichTrustIsSchoolJoining from '../page-objects/pages/A2BWhichTrustIsSchoolJoining'
+import A2BWhatIsTheNameOfTheSchool from '../page-objects/pages/A2BWhatIsTheNameOfTheSchool'
+
+describe('View Application Tests', () => {
+
+    beforeEach(function() {
+        cy.visit(url)
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BHome.h1ApplyToBecomeAnAcademyVisible()
+        A2BHome.p3Visible()
+        A2BHome.p4Visible()
+        A2BHome.checkSpecialSchoolsLinkVisible()
+        A2BHome.checkPupilReferralUnitsLinkVisible()
+        A2BHome.p6Visible()
+        A2BHome.h2Visible()
+        A2BHome.p7Visible()
+        A2BHome.completeAnEqualityImpactAssessmentVisible()
+        A2BHome.consultationWithStakeholdersLinkVisible()
+        A2BHome.p8Visible()
+        A2BHome.contactYourRegionalDirectorLinkVisible()
+        A2BHome.allInformationAndEvidenceYouWillNeedLinkVisible()
+        
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+        
+        CookieHeaderModal.clickAcceptAnalyticsCookies()
+        A2BHome.StartNowVisible()
+        A2BHome.clickStartNow()
+      })
+
+    it('should be able to add a school and a trust', () => {
+        A2BLogin.login(login_username, login_password)
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BYourApplications.yourApplicationsElementsVisible()
+
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+
+        A2BYourApplications.selectTempSecondHalfCreateNewJAMApplication()
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        //A2BYourApplication.yourApplicationNotStartedElementsVisible()
+   
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+
+        // Click Add a Trust
+      // A2BYourApplication.selectAddATrust()
+
+      // Header.govUkHeaderVisible()
+//       Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+       //A2BWhichTrustIsSchoolJoining.whichTrustIsSchoolJoiningElementsVisible()
+
+       //Footer.accessibilityStatementLinkVisible()
+       //Footer.cookiesLinkVisible()
+       //Footer.termsAndConditionsLinkVisible()
+       //Footer.privacyLinkVisible()
+       //Footer.oglLogoVisible()
+       //Footer.allContentTextVisible()
+       //Footer.openGovernmentLicence3LinkVisible()
+       //Footer.crownCopyrightLinkVisible()
+
+       A2BWhichTrustIsSchoolJoining.enterTrustNameSelectAndSubmit()
+       A2BWhatIsTheNameOfTheSchool.enterSchoolNameSelectAndSubmit()
+
+       cy.reload() // reload page and check for correct page attributes
+
+
+       Header.govUkHeaderVisible()
+       Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+       A2BYourApplication.yourApplicationNotStartedButSchoolAddedElementsVisible()
+
+       Footer.accessibilityStatementLinkVisible()
+       Footer.cookiesLinkVisible()
+       Footer.termsAndConditionsLinkVisible()
+       Footer.privacyLinkVisible()
+       Footer.oglLogoVisible()
+       Footer.allContentTextVisible()
+       Footer.openGovernmentLicence3LinkVisible()
+       Footer.crownCopyrightLinkVisible()
+
+       // PROCEED TO TRUST DETAILS SECTION
+       A2BYourApplication.selectTrustDetails()
+
+       // CHECK TRUST DETAILS SUMMARY PAGE
+       
+
+    })
+})
