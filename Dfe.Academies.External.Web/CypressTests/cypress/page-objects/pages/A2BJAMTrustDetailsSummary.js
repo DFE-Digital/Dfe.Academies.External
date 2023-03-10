8c2e64f8aae9450a28aa3dba87369865e76dbef2
Dan Good <dangood84@me.com>
2023-03-10 15:44:08 +0000

    Added content to A2BJAMTrustDetailsSummary page object including JAMTrustDetailsSummaryElementsVisible() and JAMTrustDetailsSummarySelectStartSection() functions

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BJAMTrustDetailsSummary.js
@@ -0,0 +1,12 @@
+import BasePage from "../BasePage"
+export default class A2BJAMTrustDetailsSummary extends BasePage {
+    
+static JAMTrustDetailsSummaryElementsVisible() {
+    cy.JAMTrustDetailsSummaryElementsVisible()
+}
+
+static JAMTrustDetailsSummarySelectStartSection() {
+    cy.JAMTrustDetailsSummarySelectStartSection()
+}
+
+}
