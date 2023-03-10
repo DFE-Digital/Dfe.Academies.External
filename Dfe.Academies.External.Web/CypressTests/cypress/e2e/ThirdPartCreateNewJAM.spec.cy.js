f599e1b9efe0245ad5139f4deca25bc680138806
Dan Good <dangood84@me.com>
2023-03-10 15:36:23 +0000

    Dropping in ThirdPartCreateNewJAM.spec.cy.js test focussing on Trust Details section

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/e2e/ThirdPartCreateNewJAM.spec.cy.js
@@ -0,0 +1,129 @@
+import { url, login_username, login_password } from '../../config'
+import Header from '../page-objects/components/Header'
+import CookieHeaderModal from '../page-objects/components/CookieHeaderModal'
+import A2BHome from '../page-objects/pages/A2BHome'
+import A2BLogin from '../page-objects/pages/A2BLogin'
+import A2BYourApplication from '../page-objects/pages/A2BYourApplication'
+import A2BYourApplications from '../page-objects/pages/A2BYourApplications'
+import Footer from '../page-objects/components/Footer'
+import A2BJAMTrustDetailsSummary from '../page-objects/pages/A2BJAMTrustDetailsSummary'
+import A2BJAMTrustConsent from '../page-objects/pages/A2BJAMTrustConsent'
+
+describe('Third Part Create New JAM App - Complete Trust Details', () => {
+
+    beforeEach(function() {
+        cy.visit(url)
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BHome.h1ApplyToBecomeAnAcademyVisible()
+        A2BHome.p3Visible()
+        A2BHome.p4Visible()
+        A2BHome.checkSpecialSchoolsLinkVisible()
+        A2BHome.checkPupilReferralUnitsLinkVisible()
+        A2BHome.p6Visible()
+        A2BHome.h2Visible()
+        A2BHome.p7Visible()
+        A2BHome.completeAnEqualityImpactAssessmentVisible()
+        A2BHome.consultationWithStakeholdersLinkVisible()
+        A2BHome.p8Visible()
+        A2BHome.contactYourRegionalDirectorLinkVisible()
+        A2BHome.allInformationAndEvidenceYouWillNeedLinkVisible()
+        
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+        
+        CookieHeaderModal.clickAcceptAnalyticsCookies()
+        A2BHome.StartNowVisible()
+        A2BHome.clickStartNow()
+      })
+
+      it('Navigate to JAM App With Trust Name and School Name Added And Complete Trust details Section', () => {
+        A2BLogin.login(login_username, login_password)
+
+        cy.wait(5000) // Adding wait as Cypress is too quick for the app here sometimes
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+        
+        A2BYourApplications.yourApplicationsElementsVisible()
+
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+    
+        A2BYourApplications.selectJAMNotStartedApplicationButSchoolAdded()
+        
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+        
+        A2BYourApplication.yourApplicationNotStartedButSchoolAddedElementsVisible()
+
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+
+        // PROCEED TO TRUST DETAILS SUMMARY PAGE!!!!
+        // -----------------------------------------
+        // PROCEED TO TRUST DETAILS SECTION
+        A2BYourApplication.selectTrustDetails()
+
+       // CHECK TRUST DETAILS SUMMARY PAGE
+       Header.govUkHeaderVisible()
+       Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+       A2BJAMTrustDetailsSummary.JAMTrustDetailsSummaryElementsVisible()
+
+       Footer.accessibilityStatementLinkVisible()
+       Footer.cookiesLinkVisible()
+       Footer.termsAndConditionsLinkVisible()
+       Footer.privacyLinkVisible()
+       Footer.oglLogoVisible()
+       Footer.allContentTextVisible()
+       Footer.openGovernmentLicence3LinkVisible()
+       Footer.crownCopyrightLinkVisible()
+
+       // CLICK ON START SECTION
+       A2BJAMTrustDetailsSummary.JAMTrustDetailsSummarySelectStartSection()
+
+       // CHECK ELEMENTS VISIBLE ON JAM TRUST CONSENT PAGE
+       Header.govUkHeaderVisible()
+       Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+       A2BJAMTrustConsent.JAMTrustConsentElementsVisible()
+
+       Footer.accessibilityStatementLinkVisible()
+       Footer.cookiesLinkVisible()
+       Footer.termsAndConditionsLinkVisible()
+       Footer.privacyLinkVisible()
+       Footer.oglLogoVisible()
+       Footer.allContentTextVisible()
+       Footer.openGovernmentLicence3LinkVisible()
+       Footer.crownCopyrightLinkVisible()
+
+       // ATTEMPT TO UPLOAD A FILE
+       A2BJAMTrustConsent.JAMTrustConsentFileUpload()
+
+       // ATTEMPT TO SUBMIT TRUST CONSENT FORM
+       A2BJAMTrustConsent.JAMTrustConsentSubmit()
+
+    })
+      
+})
