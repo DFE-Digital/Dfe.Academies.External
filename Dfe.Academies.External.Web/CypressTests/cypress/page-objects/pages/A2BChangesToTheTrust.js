89992b3c4b16dab0bab6acfc6a156961dce13789
Dan Good <dangood84@me.com>
2023-03-21 16:50:15 +0000

    Adding content and functions to A2BChangesToTheTrust page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BChangesToTheTrust.js
@@ -0,0 +1,22 @@
+import BasePage from "../BasePage"
+export default class A2BChangesToTheTrust extends BasePage {
+    static changesToTheTrustElementsVisible()
+    {
+        cy.changesToTheTrustElementsVisible()
+    }
+
+    static changesToTheTrustClickYes()
+    {
+        cy.changesToTheTrustClickYes()
+    }
+
+    static enterChangesToTheTrust()
+    {
+        cy.enterChangesToTheTrust()
+    }
+
+    static changesToTheTrustSubmit()
+    {
+        cy.changesToTheTrustSubmit()
+    }
+}
