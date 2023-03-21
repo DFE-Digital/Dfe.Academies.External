168dbf3ed89c2d3e7ea5f7944844d9eddb2e6868
Dan Good <dangood84@me.com>
2023-03-21 16:32:30 +0000

    Added localGovernanceArrangementsElementsVisible function to commands.js

--- Dfe.Academies.External.Web/CypressTests/cypress/support/commands.js
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/commands.js
@@ -544,3 +544,17 @@ Cypress.Commands.add('enterChangesToTheTrust', () => {
 Cypress.Commands.add('changesToTheTrustSubmit', () => {
     cy.get('input[type="submit"]').click()
 })
+
+Cypress.Commands.add('localGovernanceArrangementsElementsVisible', () => {
+    cy.get('a[class="govuk-back-link"]').contains('Back to trust summary')
+    cy.get('.govuk-caption-l').eq(0).contains('PLYMOUTH CAST (step 3 of 3)')
+    cy.get('.govuk-heading-l').contains('Local governance arrangements')
+    cy.get('legend').eq(0).contains('Will there be any changes at a local level due to this school joining?')
+    cy.get('.govuk-caption-l').eq(1).contains('For example, setting up a local sub-regional hub or changes to any schemes of delegation.')
+    cy.get('#changesToLaGovernanceYes').should('not.be.checked')
+    cy.get('label[for="changesToLaGovernanceYes"]').contains('Yes')
+    cy.get('#changesToLaGovernanceNo').should('be.checked')
+    cy.get('label[for="changesToLaGovernanceNo"]').contains('No')
+    cy.get('input[type="submit"]').should('be.visible').contains('Save and continue')
+
+})
