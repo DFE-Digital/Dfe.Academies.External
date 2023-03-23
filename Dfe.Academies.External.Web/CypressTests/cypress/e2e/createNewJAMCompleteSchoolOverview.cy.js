80b8481df36d685bc0d174b4ac62a1d812150c2f
Dan Good <dangood84@me.com>
2023-03-23 16:13:18 +0000

    Adding next step of CreateNewJAMApp with createNewJAMCompleteSchoolOverview spec file

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/e2e/createNewJAMCompleteSchoolOverview.cy.js
@@ -0,0 +1,101 @@
+import { url, login_username, login_password } from '../../config'
+import Header from '../page-objects/components/Header'
+import CookieHeaderModal from '../page-objects/components/CookieHeaderModal'
+import A2BHome from '../page-objects/pages/A2BHome'
+import A2BLogin from '../page-objects/pages/A2BLogin'
+import A2BYourApplication from '../page-objects/pages/A2BYourApplication'
+import A2BYourApplications from '../page-objects/pages/A2BYourApplications'
+import A2BAboutTheConversion from '../page-objects/pages/A2BAboutTheConversion'
+import Footer from '../page-objects/components/Footer'
+
+describe('Complete School Overview Section', () => {
+
+    beforeEach(function() {
+        cy.visit(url)
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BHome.h1ApplyToBecomeAnAcademyVisible()
+        A2BHome.p3Visible()
+        A2BHome.p4Visible()
+        A2BHome.checkSpecialSchoolsLinkVisible()
+        A2BHome.checkPupilReferralUnitsLinkVisible()
+        A2BHome.p6Visible()
+        A2BHome.h2Visible()
+        A2BHome.p7Visible()
+        A2BHome.completeAnEqualityImpactAssessmentVisible()
+        A2BHome.consultationWithStakeholdersLinkVisible()
+        A2BHome.p8Visible()
+        A2BHome.contactYourRegionalDirectorLinkVisible()
+        A2BHome.allInformationAndEvidenceYouWillNeedLinkVisible()
+        
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+        
+        CookieHeaderModal.clickAcceptAnalyticsCookies()
+        A2BHome.StartNowVisible()
+        A2BHome.clickStartNow()
+      })
+
+    it('should be able to complete JAM School Overview Section', () => {
+        A2BLogin.login(login_username, login_password)
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BYourApplications.yourApplicationsElementsVisible()
+
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+
+        A2BYourApplications.selectTempSecondHalfCreateNewJAMApplication()
+
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BYourApplication.yourApplicationNotStartedButTrustSectionCompleteElementsVisible()
+   
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+
+        //  OK - Let's Start By Clicking On About the Conversion Section
+        A2BYourApplication.selectAboutTheConversion()
+
+        // OK we're Now on About the Conversion Page - Let's check all elements display correctly
+        Header.govUkHeaderVisible()
+        Header.applyToBecomeAnAcademyHeaderLinkVisible()
+
+        A2BAboutTheConversion.aboutTheConversionNotStartedElementsVisible()
+
+        Footer.accessibilityStatementLinkVisible()
+        Footer.cookiesLinkVisible()
+        Footer.termsAndConditionsLinkVisible()
+        Footer.privacyLinkVisible()
+        Footer.oglLogoVisible()
+        Footer.allContentTextVisible()
+        Footer.openGovernmentLicence3LinkVisible()
+        Footer.crownCopyrightLinkVisible()
+
+        
+    })
+
+})
