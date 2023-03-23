a173bfde68c0bf012a73b848f4ffdfb62908126b
Dan Good <dangood84@me.com>
2023-03-23 16:18:12 +0000

    Added content and aboutTheConversionNotStartedElementsVisible function to A2BAboutTheConversion page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BAboutTheConversion.js
@@ -0,0 +1,14 @@
+import BasePage from "../BasePage"
+export default class A2BAboutTheConversion extends BasePage {
+    
+    static aboutTheConversionNotStartedElementsVisible()
+    {
+        cy.aboutTheConversionNotStartedElementsVisible()
+    }
+    
+
+
+
+
+
+}
