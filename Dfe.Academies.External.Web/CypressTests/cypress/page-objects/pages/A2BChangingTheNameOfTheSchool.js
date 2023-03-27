cce733c277ce6567b2fd332a6f43ff159e60e7c0
Dan Good <dangood84@me.com>
2023-03-27 11:32:13 +0100

    Adding content and two functions to A2BChangingTheNameOfTheSchool page object - changingTheNameOfTheSchoolElementsVisible() AND submitChangingTheNameOfTheSchool()

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BChangingTheNameOfTheSchool.js
@@ -0,0 +1,14 @@
+import BasePage from "../BasePage"
+export default class A2BChangingTheNameOfTheSchool extends BasePage {
+
+    static changingTheNameOfTheSchoolElementsVisible()
+    {
+        cy.changingTheNameOfTheSchoolElementsVisible()
+    }
+
+    static submitChangingTheNameOfTheSchool()
+    {
+        cy.submitChangingTheNameOfTheSchool()
+    }
+
+}
