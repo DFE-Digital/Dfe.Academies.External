b32892c5fabc32dfbd1cc2298fc4c14f3327a9ef
Dan Good <dangood84@me.com>
2023-03-28 18:26:03 +0100

    Adding content and functions to A2BAdditionalDetailsDetails page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BAdditionalDetailsDetails.js
@@ -0,0 +1,74 @@
+
+import BasePage from "../BasePage"
+export default class A2BAdditionalDetailsDetails extends BasePage {
+    static additionalDetailsDetailsNotStartedElementsVisible()
+    {
+        cy.additionalDetailsDetailsNotStartedElementsVisible()
+    }
+
+    static fillSchoolContribution()
+    {
+        cy.fillSchoolContribution()
+    }
+
+    static selectYesIsSchoolLinkedToDiocese()
+    {
+        cy.selectYesIsSchoolLinkedToDiocese()
+    }
+
+    static dioceseSectionElementsVisible()
+    {
+        cy.dioceseSectionElementsVisible()
+    }
+
+    static inputDioceseName()
+    {
+        cy.inputDioceseName()
+    }
+
+    static dioceseFileUpload()
+    {
+        cy.dioceseFileUpload()
+    }
+
+    static selectYesSchoolSupportedByTrustOrFoundation()
+    {
+        cy.selectYesSchoolSupportedByTrustOrFoundation()
+    }
+
+    static schoolSupportedByElementsVisible()
+    {
+        cy.schoolSupportedByElementsVisible()
+    }
+
+    static inputBodyName()
+    {
+        cy.inputBodyName()
+    }
+
+    static uploadSchoolSupportedByTrustOrBody()
+    {
+        cy.uploadSchoolSupportedByTrustOrBody()
+    }
+
+    static inputListOfFeederSchools()
+    {
+        cy.inputListOfFeederSchools()
+    }
+
+    static uploadSchoolLetterOfConsent()
+    {
+        cy.uploadSchoolLetterOfConsent()
+    }
+
+    static submitAdditionalDetailsDetails()
+    {
+        cy.submitAdditionalDetailsDetails()
+    }
+
+    
+
+
+
+
+}
