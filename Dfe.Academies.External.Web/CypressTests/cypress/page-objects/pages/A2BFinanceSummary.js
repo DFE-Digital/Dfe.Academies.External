f1cb1eab638ad0289f41d6246bc2ce763860ce3c
Dan Good <dangood84@me.com>
2023-03-28 18:29:05 +0100

    Adding content and functions to A2BFinanceSummary page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BFinanceSummary.js
@@ -0,0 +1,10 @@
+import BasePage from "../BasePage"
+export default class A2BYourApplication extends BasePage {
+
+
+static financeSummaryNotStartedElementsVisible()
+{
+    cy.financeSummaryNotStartedElementsVisible()
+}
+
+}
