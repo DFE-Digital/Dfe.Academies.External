391ae1050e88b7c6b3ce953009c4dcb415ba5134
Dan Good <dangood84@me.com>
2023-04-03 15:54:46 +0100

    Added content and functions to A2BFinancialInvestigations page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BFinancialInvestigations.js
@@ -0,0 +1,15 @@
+import BasePage from "../BasePage"
+export default class A2BFinancialInvestigations extends BasePage {
+
+
+static financialInvestigationsElementsVisible()
+{
+    cy.financialInvestigationsElementsVisible()
+}
+
+static submitFinancialInvestigations()
+{
+    cy.submitFinancialInvestigations()
+}
+
+}
