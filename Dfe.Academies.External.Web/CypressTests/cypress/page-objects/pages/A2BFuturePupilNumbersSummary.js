6a5433904f798cf6560df7a2969415a946806036
Dan Good <dangood84@me.com>
2023-04-11 17:49:55 +0100

    Adding content and functions to A2BFuturePupilNumbersSummary page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BFuturePupilNumbersSummary.js
@@ -0,0 +1,27 @@
+import BasePage from "../BasePage"
+export default class A2BFuturePupilNumbersSummary extends BasePage {
+
+static futurePupilNumbersSummaryElementsVisible()
+{
+    cy.futurePupilNumbersSummaryElementsVisible()
+}
+
+
+static selectFuturePupilNumbersStartSection()
+{
+    cy.selectFuturePupilNumbersStartSection()
+}
+
+static futurePupilNumbersSummaryCompleteElementsVisible()
+{
+    cy.futurePupilNumbersSummaryCompleteElementsVisible()
+}
+
+static submitFuturePupilNumbersSummary()
+{
+    cy.submitFuturePupilNumbersSummary()
+}
+
+
+
+}
