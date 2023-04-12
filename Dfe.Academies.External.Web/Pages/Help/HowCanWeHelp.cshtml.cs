90579c5c964deaa40c427bfea57161a339b857b3
danielryannimble <danielryan@nimbleapproach.com>
2023-04-12 13:41:58 +0100

    how can we help page update

--- Dfe.Academies.External.Web/Pages/Help/HowCanWeHelp.cshtml.cs
+++ Dfe.Academies.External.Web/Pages/Help/HowCanWeHelp.cshtml.cs
@@ -1,18 +1,8 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using static GovUk.Frontend.AspNetCore.ComponentDefaults;
-
-using System.Dynamic;
-using System.Security.Claims;
 using Dfe.Academies.External.Web.Attributes;
-using Dfe.Academies.External.Web.Dtos;
 using Dfe.Academies.External.Web.Enums;
-using Dfe.Academies.External.Web.Models;
 using Dfe.Academies.External.Web.Pages.Base;
-using Dfe.Academies.External.Web.Services;
-using Dfe.Academies.External.Web.ViewModels;
-using Microsoft.AspNetCore.Mvc;
+
 
 namespace Dfe.Academies.External.Web.Pages.Help
 {
@@ -23,16 +13,14 @@ namespace Dfe.Academies.External.Web.Pages.Help
        [RequiredEnum(ErrorMessage = "You must choose an option")]
        public HelpTypes HelpType {get;set;}
 
-       private const string HelpPage = "Help";
-
        public IActionResult OnPost()
        {
             if (!RunUiValidation())
 			{
 				return Page();
 			}
-           var helpType = Request.Form["HelpType"];
-           return RedirectToPage(HelpPage, new {HelpType = helpType});
+        
+           return RedirectToPage(HelpType.ToString());
        }
 
        public bool RunUiValidation()
