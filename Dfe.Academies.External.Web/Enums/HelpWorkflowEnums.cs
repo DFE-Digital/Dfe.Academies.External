c13305a449fc07c3ca1bf6699ee80886aafb1e4d
danielryannimble <danielryan@nimbleapproach.com>
2023-04-12 13:41:29 +0100

    changes to enums and application help page added

--- Dfe.Academies.External.Web/Enums/HelpWorkflowEnums.cs
+++ Dfe.Academies.External.Web/Enums/HelpWorkflowEnums.cs
@@ -5,11 +5,11 @@ namespace Dfe.Academies.External.Web.Enums;
 public enum HelpTypes
 {
 	[Description("I want to report a problem with this form")]
-	ProblemWithForm = 0,
+	Problem,
 	
 	[Description("I need help with an application")]
-	HelpWithApplication = 1,
+	ApplicationHelp,
 
 	[Description("I have a comment or want to to suggest something")]
-	CommentOrSuggestion = 2
+	Feedback 
 }
