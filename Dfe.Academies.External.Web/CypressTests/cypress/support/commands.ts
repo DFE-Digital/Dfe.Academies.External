cd3a3c971566695d53dcddd0b95d9a8540403671
Dan Good <dangood84@me.com>
2023-05-11 15:31:21 +0100

    Changed aboutTheConversionNotStartedElementsVisible, mainContactsNotStartedElementsVisible, and aboutTheConversionMainContactsCompleteElementsVisible functions in commands.ts to remove the Headteacher telephone and Chair telephone number fields

--- Dfe.Academies.External.Web/CypressTests/cypress/support/commands.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/commands.ts
@@ -623,7 +623,7 @@ Cypress.Commands.add('selectAboutTheConversion', () => {
     cy.contains('About the conversion').click()
 })
 
-Cypress.Commands.add('aboutTheConversionNotStartedElementsVisible', () => {
+Cypress.Commands.add('aboutTheConversionNotStartedElementsVisible', ():void => {
     cy.get('.govuk-back-link').contains('Back')
 
     cy.get('.govuk-caption-l').contains('Plymstock School')
@@ -632,71 +632,59 @@ Cypress.Commands.add('aboutTheConversionNotStartedElementsVisible', () => {
 
     cy.get('.govuk-body').eq(0).contains('Name of headteacher')
     cy.get('.govuk-body').eq(1).contains('You have not added any information')
-    cy.get('hr').eq(0).should('be.visible')
 
     cy.get('.govuk-body').eq(2).contains('Headteacher\'s email address')
     cy.get('.govuk-body').eq(3).contains('You have not added any information')
-    cy.get('hr').eq(1).should('be.visible')
 
-    cy.get('.govuk-body').eq(4).contains('Headteacher\'s telephone number')
+    //cy.get('.govuk-body').eq(4).contains('Headteacher\'s telephone number')
+    //cy.get('.govuk-body').eq(5).contains('You have not added any information')
+
+    cy.get('.govuk-body').eq(4).contains('Name of the chair of the governing body')
     cy.get('.govuk-body').eq(5).contains('You have not added any information')
-    cy.get('hr').eq(2).should('be.visible')
 
-    cy.get('.govuk-body').eq(6).contains('Name of the chair of the governing body')
+    cy.get('.govuk-body').eq(6).contains('Chair\'s email address')
     cy.get('.govuk-body').eq(7).contains('You have not added any information')
-    cy.get('hr').eq(3).should('be.visible')
 
-    cy.get('.govuk-body').eq(8).contains('Chair\'s email address')
+    //cy.get('.govuk-body').eq(10).contains('Chair\'s telephone number')
+    //cy.get('.govuk-body').eq(11).contains('You have not added any information')
+
+    cy.get('.govuk-body').eq(8).contains('Who is the main contact for the conversion')
     cy.get('.govuk-body').eq(9).contains('You have not added any information')
-    cy.get('hr').eq(4).should('be.visible')
 
-    cy.get('.govuk-body').eq(10).contains('Chair\'s telephone number')
+    cy.get('.govuk-body').eq(10).contains('Approver\'s full name')
     cy.get('.govuk-body').eq(11).contains('You have not added any information')
-    cy.get('hr').eq(5).should('be.visible')
 
-    cy.get('.govuk-body').eq(12).contains('Who is the main contact for the conversion')
+    cy.get('.govuk-body').eq(12).contains('Approver\'s email address')
     cy.get('.govuk-body').eq(13).contains('You have not added any information')
-    cy.get('hr').eq(6).should('be.visible')
 
-    cy.get('.govuk-body').eq(14).contains('Approver\'s full name')
-    cy.get('.govuk-body').eq(15).contains('You have not added any information')
-    cy.get('hr').eq(7).should('be.visible')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(0).should('be.visible').contains('Start section')
 
-    cy.get('.govuk-body').eq(16).contains('Approver\'s email address')
-    cy.get('.govuk-body').eq(17).contains('You have not added any information')
-    cy.get('hr').eq(8).should('be.visible')
-
-    cy.get('a[aria-describedby="component-not started"]').eq(0).should('be.visible').contains('Start section')
-
-  // HR PART OF START SECTION COMPONENT SECTION
+   // HR PART OF START SECTION COMPONENT SECTION
 
     cy.get('.govuk-heading-m').eq(1).contains('Date for conversion')
-    cy.get('.govuk-body').eq(18).contains('Do you want the conversion to happen on a particular date?')
-    cy.get('.govuk-body').eq(19).contains('You have not added any information')
+    cy.get('.govuk-body').eq(14).contains('Do you want the conversion to happen on a particular date?')
+    cy.get('.govuk-body').eq(15).contains('You have not added any information')
 
-    cy.get('hr').eq(9).should('be.visible')
-
-    cy.get('a[aria-describedby="component-not started"]').eq(1).should('be.visible').contains('Start section')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(1).should('be.visible').contains('Start section')
 
     // HR PART OF START SECTION COMPONENT SECTION
 
     cy.get('.govuk-heading-m').eq(2).contains('Reasons for joining')
-    cy.get('.govuk-body').eq(20).contains('Why does the school want to join this trust in particular?')
-    cy.get('.govuk-body').eq(21).contains('You have not added any information')
+    cy.get('.govuk-body').eq(16).contains('Why does the school want to join this trust in particular?')
+    cy.get('.govuk-body').eq(17).contains('You have not added any information')
 
-    cy.get('hr').eq(10).should('be.visible')
+  //  cy.get('hr').eq(10).should('be.visible')
 
-    cy.get('a[aria-describedby="component-not started"]').eq(2).should('be.visible').contains('Start section')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(2).should('be.visible').contains('Start section')
 
     // HR PART OF START SECTION COMPONENT SECTION
 
     cy.get('.govuk-heading-m').eq(3).contains('Changing the name of the school')
-    cy.get('.govuk-body').eq(22).contains('Is the school planning to change its name when it becomes an academy?')
-    cy.get('.govuk-body').eq(23).contains('You have not added any information')
+    cy.get('.govuk-body').eq(18).contains('Is the school planning to change its name when it becomes an academy?')
+    cy.get('.govuk-body').eq(19).contains('You have not added any information')
 
-    cy.get('hr').eq(11).should('be.visible')
 
-    cy.get('a[aria-describedby="component-not started"]').eq(3).should('be.visible').contains('Start section')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(3).should('be.visible').contains('Start section')
 
     // HR PART OF START SECTION COMPONENT SECTION
 
@@ -710,30 +698,30 @@ Cypress.Commands.add('selectContactDetailsStartSection', () => {
     cy.get('a[aria-describedby="component-not started"]').eq(0).click()
 })
 
-Cypress.Commands.add('mainContactsNotStartedElementsVisible', () => {
+Cypress.Commands.add('mainContactsNotStartedElementsVisible', ():void => {
     cy.get('.govuk-back-link').contains('Back')
     cy.get('.govuk-caption-l').contains('Conversion key details')
     cy.get('.govuk-heading-l').contains('Main contacts')
     
     cy.get('.govuk-label').eq(0).contains('Name of headteacher')
-    cy.get('#ContactHeadName').should('be.enabled')
+    cy.get('#ViewModel\\.ContactHeadName').should('be.enabled')
     
     cy.get('.govuk-label').eq(1).contains('Headteacher\'s email address')
     cy.get('.govuk-hint').eq(0).contains('We will only use this to contact you regarding your application')
-    cy.get('#ContactHeadEmail').should('be.enabled')
+    cy.get('#ViewModel\\.ContactHeadEmail').should('be.enabled')
 
-    cy.get('.govuk-label').eq(2).contains('Headteacher\'s telephone number')
-    cy.get('#ContactHeadTel').should('be.enabled')
+   // cy.get('.govuk-label').eq(2).contains('Headteacher\'s telephone number')
+   // cy.get('#ViewModel\\.ContactHeadTel').should('be.enabled')
 
-    cy.get('.govuk-label').eq(3).contains('Name of the chair of the governing body')
-    cy.get('#ContactChairName').should('be.enabled')
+    cy.get('.govuk-label').eq(2).contains('Name of the chair of the governing body')
+    cy.get('#ViewModel\\.ContactChairName').should('be.enabled')
 
-    cy.get('.govuk-label').eq(4).contains('Chair\'s email address')
+    cy.get('.govuk-label').eq(3).contains('Chair\'s email address')
     cy.get('.govuk-hint').eq(1).contains('We will only use this to contact you regarding your application')
-    cy.get('#ContactChairEmail').should('be.enabled')
+    cy.get('#ViewModel\\.ContactChairEmail').should('be.enabled')
 
-    cy.get('.govuk-label').eq(5).contains('Chair\'s telephone number')
-    cy.get('#ContactChairTel')
+   // cy.get('.govuk-label').eq(5).contains('Chair\'s telephone number')
+   // cy.get('#ViewModel\\.ContactChairTel')
 
     cy.get('span[class="govuk-fieldset__legend govuk-fieldset__legend--s"]').contains('Who is the main contact for the conversion?')
     
@@ -758,7 +746,7 @@ Cypress.Commands.add('mainContactsNotStartedElementsVisible', () => {
     cy.get('.govuk-hint').eq(4).contains('We will only use this to contact you regarding your application')
     cy.get('#ApproverContactEmail').should('be.enabled')
 
-    cy.get('input[type="submit"]').should('be.visible').contains('Save and return to overview')
+    cy.get('input[type="submit"]').should('be.visible').contains('Save and continue')
 })
 
 Cypress.Commands.add('fillHeadTeacherDetails', () => {
@@ -788,80 +776,67 @@ Cypress.Commands.add('submitMainContactsForm', () => {
     cy.get('input[type="submit"]').click()
 })
 
-Cypress.Commands.add('aboutTheConversionMainContactsCompleteElementsVisible', () => {
+Cypress.Commands.add('aboutTheConversionMainContactsCompleteElementsVisible', ():void => {
     cy.get('.govuk-back-link').contains('Back')
 
     cy.get('.govuk-caption-l').contains('Plymstock School')
     cy.get('.govuk-heading-l').contains('About the conversion')
     cy.get('.govuk-heading-m').eq(0).contains('Contact details')
 
+    cy.get('a[class="govuk-link"]').eq(1).contains('Change your answers')
+
     cy.get('.govuk-body').eq(0).contains('Name of headteacher')
     cy.get('.govuk-body').eq(1).contains('Paul Lockwood')
-    cy.get('hr').eq(0).should('be.visible')
 
     cy.get('.govuk-body').eq(2).contains('Headteacher\'s email address')
     cy.get('.govuk-body').eq(3).contains('paul.lockwood@education.gov.uk')
-    cy.get('hr').eq(1).should('be.visible')
 
-    cy.get('.govuk-body').eq(4).contains('Headteacher\'s telephone number')
-    cy.get('.govuk-body').eq(5).contains('01752404930')
-    cy.get('hr').eq(2).should('be.visible')
+  //  cy.get('.govuk-body').eq(4).contains('Headteacher\'s telephone number')
+  //  cy.get('.govuk-body').eq(5).contains('01752404930')
 
-    cy.get('.govuk-body').eq(6).contains('Name of the chair of the governing body')
-    cy.get('.govuk-body').eq(7).contains('Dan Good')
-    cy.get('hr').eq(3).should('be.visible')
+    cy.get('.govuk-body').eq(4).contains('Name of the chair of the governing body')
+    cy.get('.govuk-body').eq(5).contains('Dan Good')
 
-    cy.get('.govuk-body').eq(8).contains('Chair\'s email address')
-    cy.get('.govuk-body').eq(9).contains('dan.good@education.gov.uk')
-    cy.get('hr').eq(4).should('be.visible')
+    cy.get('.govuk-body').eq(6).contains('Chair\'s email address')
+    cy.get('.govuk-body').eq(7).contains('dan.good@education.gov.uk')
 
-    cy.get('.govuk-body').eq(10).contains('Chair\'s telephone number')
-    cy.get('.govuk-body').eq(11).contains('01752404000')
-    cy.get('hr').eq(5).should('be.visible')
+  //  cy.get('.govuk-body').eq(8).contains('Chair\'s telephone number')
+  //  cy.get('.govuk-body').eq(9).contains('01752404000')
 
-    cy.get('.govuk-body').eq(12).contains('Who is the main contact for the conversion')
-    cy.get('.govuk-body').eq(13).contains('The chair of the governing body')
-    cy.get('hr').eq(6).should('be.visible')
+    cy.get('.govuk-body').eq(8).contains('Who is the main contact for the conversion')
+    cy.get('.govuk-body').eq(9).contains('The chair of the governing body')
 
-    cy.get('.govuk-body').eq(14).contains('Approver\'s full name')
-    cy.get('.govuk-body').eq(15).contains('Nicky Price')
-    cy.get('hr').eq(7).should('be.visible')
+    cy.get('.govuk-body').eq(10).contains('Approver\'s full name')
+    cy.get('.govuk-body').eq(11).contains('Nicky Price')
 
-    cy.get('.govuk-body').eq(16).contains('Approver\'s email address')
-    cy.get('.govuk-body').eq(17).contains('nicky.price@aol.com')
-    cy.get('hr').eq(8).should('be.visible')
+    cy.get('.govuk-body').eq(12).contains('Approver\'s email address')
+    cy.get('.govuk-body').eq(13).contains('nicky.price@aol.com')
 
-    cy.get('a[aria-describedby="component-change"]').eq(0).contains('Change your answers')
+    
 
    // HR PART OF START SECTION COMPONENT SECTION
 
     cy.get('.govuk-heading-m').eq(1).contains('Date for conversion')
-    cy.get('.govuk-body').eq(18).contains('Do you want the conversion to happen on a particular date?')
-    cy.get('.govuk-body').eq(19).contains('You have not added any information')
+    cy.get('.govuk-body').eq(14).contains('Do you want the conversion to happen on a particular date?')
+    cy.get('.govuk-body').eq(15).contains('You have not added any information')
 
-    cy.get('hr').eq(9).should('be.visible')
-
-    cy.get('a[aria-describedby="component-not started"]').eq(0).should('be.visible').contains('Start section')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(0).should('be.visible').contains('Start section')
 
     // HR PART OF START SECTION COMPONENT SECTION
 
     cy.get('.govuk-heading-m').eq(2).contains('Reasons for joining')
-    cy.get('.govuk-body').eq(20).contains('Why does the school want to join this trust in particular?')
-    cy.get('.govuk-body').eq(21).contains('You have not added any information')
+    cy.get('.govuk-body').eq(16).contains('Why does the school want to join this trust in particular?')
+    cy.get('.govuk-body').eq(17).contains('You have not added any information')
 
-    cy.get('hr').eq(10).should('be.visible')
-
-    cy.get('a[aria-describedby="component-not started"]').eq(1).should('be.visible').contains('Start section')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(1).should('be.visible').contains('Start section')
 
     // HR PART OF START SECTION COMPONENT SECTION
 
     cy.get('.govuk-heading-m').eq(3).contains('Changing the name of the school')
-    cy.get('.govuk-body').eq(22).contains('Is the school planning to change its name when it becomes an academy?')
-    cy.get('.govuk-body').eq(23).contains('You have not added any information')
+    cy.get('.govuk-body').eq(18).contains('Is the school planning to change its name when it becomes an academy?')
+    cy.get('.govuk-body').eq(19).contains('You have not added any information')
 
-    cy.get('hr').eq(11).should('be.visible')
-
-    cy.get('a[aria-describedby="component-not started"]').eq(2).should('be.visible').contains('Start section')
+    cy.get('a[class="govuk-button govuk-button--secondary"]').eq(2).should('be.visible').contains('Start section')
 
     // HR PART OF START SECTION COMPONENT SECTION
 
