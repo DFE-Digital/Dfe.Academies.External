522b81f58056ce4cb8698cd4afbfbefc6dc7d328
Dan Good <dangood84@me.com>
2023-05-16 15:36:06 +0100

    Updating commands.ts with local copy removing unnecessary page object functions from commands.ts

--- Dfe.Academies.External.Web/CypressTests/cypress/support/commands.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/commands.ts
@@ -2,7 +2,7 @@ import 'cypress-file-upload'
 
 import DataGenerator from "../fixtures/data-generator"
 
-let globalApplicationId = 10041;
+let globalApplicationId = 10080;
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -13,21 +13,7 @@ let globalApplicationId = 10041;
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
 
 Cypress.Commands.add('loginWithWrongUsername', (username, password) => {
     //cy.get('#form-signin').should('be.visible')
