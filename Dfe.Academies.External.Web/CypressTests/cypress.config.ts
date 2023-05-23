dc41ef77f6d0f268a89d5ca690a81704e1ff40ba
Dan Good <dangood84@me.com>
2023-05-23 18:55:18 +0100

    Changed login_username and login_password values in cypress.config.ts as Cypress.env type vars wouldn't work here

--- Dfe.Academies.External.Web/CypressTests/cypress.config.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress.config.ts
@@ -1,7 +1,7 @@
 import { url, login_username, login_password, dfeSignInTestEnvURLForA2BDevAndA2BTest, dfeSignInTestEnvForgotPasswordCodeInputURLForA2BDevAndA2BTest, dfeSignInTestEnvCreateAccountForA2BDevAndA2BTest } from './config'
 
 import { Cypress } from 'cypress';
- 
+
 const { defineConfig } = require('cypress')
 /*const {
   username,
@@ -13,8 +13,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     url: url,
-    login_username: Cypress.env('username'),
-    login_password: Cypress.env('password'),
+    login_username: login_username,
+    login_password: login_password,
     experimentalOriginDependencies: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
