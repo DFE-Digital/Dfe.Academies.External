170f5dcce33fed119c3d779daf652d2e73193ae2
Dan Good <dangood84@me.com>
2023-05-24 16:47:13 +0100

    Consolidated homepage check functions in A2BHome page object into one function in homePageElementsVisible()

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BHome.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BHome.ts
@@ -11,68 +11,25 @@ export default class A2BHome extends BasePage {
 
 */
 
-    static h1ApplyToBecomeAnAcademyVisible() {
-        cy.get('h1').contains('Apply to become an Academy')
-    }
-
-    static p3Visible() {
-        cy.get('p').eq(2).contains('This form is for local authority maintained schools that want to become academies.')
-    }
-
-    static p4Visible() {
-        cy.get('p').eq(3).contains('There is a separate process for:')
-    }
-
-    static checkSpecialSchoolsLinkVisible() {
-        cy.get('li').eq(0).contains('special schools')
-    }
-
-    static checkPupilReferralUnitsLinkVisible() {
-        cy.get('li').eq(1).contains('pupil referral units (PRUs)')
-    }
-
-    static p6Visible() {
-        cy.get('p').eq(4).contains('Although you can invite other people to help with this application, only the chair of governors will be able to submit it.')
-    }
-
-    static h2Visible() {
-        cy.get('.govuk-heading-l').contains('Before you start')
-    }
-
-    static p7Visible() {
-        cy.get('p').eq(5).contains('You must:')
-    }
-
-    static consultationWithStakeholdersLinkVisible() {
-        cy.get('a[target="_blank"]').eq(0).contains('carry out a consultation with your stakeholders (opens in new tab)')
-    }
-
-    static completeAnEqualityImpactAssessmentVisible() {
-        cy.get('li').eq(3).contains('complete an Equality Impact Assessment (EIA)')
-    }
-
-    static provideBankDetailsLinkVisible() {
-        cy.get('a[target="_blank"]').eq(1).contains('provide bank details (opens in a new tab)')
-    }
-
-    static p8Visible()
-    {
-        cy.get('p').eq(6).contains('You should also:')
-    }
-
-    static contactYourRegionalDirectorLinkVisible()
-    {
-        cy.get('a[target="_blank"]').eq(2).contains('your regional director (opens in a new tab)')
-    }
-
-    static allInformationAndEvidenceYouWillNeedLinkVisible()
-    {
-        cy.get('a[target="_blank"]').eq(3).contains('all of the information and evidence you will need (opens in a new tab)')
-    }
-
-    static StartNowVisible() {
-        cy.get('.govuk-grid-column-two-thirds > .govuk-button').should('be.visible')
-    }
+static homePageElementsVisible()
+{
+    cy.get('h1').contains('Apply to become an Academy')
+    cy.get('p').eq(2).contains('This form is for local authority maintained schools that want to become academies.')
+    cy.get('p').eq(3).contains('There is a separate process for:')
+    cy.get('li').eq(0).contains('special schools')
+    cy.get('li').eq(1).contains('pupil referral units (PRUs)')
+    cy.get('p').eq(4).contains('Although you can invite other people to help with this application, only the chair of governors will be able to submit it.')
+    cy.get('.govuk-heading-l').contains('Before you start')
+    cy.get('p').eq(5).contains('You must:')
+    cy.get('a[target="_blank"]').eq(0).contains('carry out a consultation with your stakeholders (opens in new tab)')
+    cy.get('li').eq(3).contains('complete an Equality Impact Assessment (EIA)')
+    cy.get('a[target="_blank"]').eq(1).contains('provide bank details (opens in a new tab)')
+    cy.get('p').eq(6).contains('You should also:')
+    cy.get('a[target="_blank"]').eq(2).contains('your regional director (opens in a new tab)')
+    cy.get('a[target="_blank"]').eq(3).contains('all of the information and evidence you will need (opens in a new tab)')
+    cy.get('.govuk-grid-column-two-thirds > .govuk-button').should('be.visible')
+    
+}
 
     static clickStartNow() {
         cy.get('.govuk-grid-column-two-thirds > .govuk-button').click()
