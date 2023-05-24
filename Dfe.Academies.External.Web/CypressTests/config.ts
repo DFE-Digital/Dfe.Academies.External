7e2b3e68ca06faccccc9287898ae1abb2ef856ea
Dan Good <dangood84@me.com>
2023-05-24 13:14:42 +0100

    Attempted to change value of url initialised in congfig.ts from process.env.url to process.env.URL

--- Dfe.Academies.External.Web/CypressTests/config.ts
+++ Dfe.Academies.External.Web/CypressTests/config.ts
@@ -1,6 +1,5 @@
-import { Cypress } from 'cypress';
 
-export const url = process.env.url
+export const url = process.env.URL
 export const dfeSignInTestEnvURLForA2BDevAndA2BTest: string = "https://test-interactions.signin.education.gov.uk//7fbd2f4e-8296-4211-a7e4-a38df63d3ff5/usernamepassword"
 export const dfeSignInTestEnvForgotPasswordCodeInputURLForA2BDevAndA2BTest: string = "https://test-interactions.signin.education.gov.uk/bb11be36-f9b9-420d-8765-aeab083b495d/resetpassword/1786759B-D42E-4A52-B189-4788B76DD793/confirm"
 export const dfeSignInTestEnvCreateAccountForA2BDevAndA2BTest: string  = "https://test-profile.signin.education.gov.uk/register"
