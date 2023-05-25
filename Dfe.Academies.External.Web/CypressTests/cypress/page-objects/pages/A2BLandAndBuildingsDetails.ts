32377a11938616138dda42edd239e229347ff7ee
Dan Good <dangood84@me.com>
2023-05-25 15:34:28 +0100

    Adding in radio button selection functions into A2BLandAndBuildingsDetails page object

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BLandAndBuildingsDetails.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BLandAndBuildingsDetails.ts
@@ -10,6 +10,42 @@ export default class A2BLandAndBuildingsDetails extends BasePage {
     cy.fillLandAndBuildingsDetails()
     }
 
+    static selectCurrentPlannedBuildingWorksOptionNo()
+    {   
+        cy.get('#buildingWorksOptionNo').click()
+        cy.get('#buildingWorksOptionNo').should('be.checked')
+    }
+
+    static selectSharedFacilitiesOptionNo()
+    {
+        cy.get('#sharedFacilitiesOptionNo').click()
+        cy.get('#sharedFacilitiesOptionNo').should('be.checked')
+    }
+
+    static selectLandGrantsOptionNo()
+    {
+        cy.get('#landGrantsOptionNo').click()
+        cy.get('#landGrantsOptionNo').should('be.checked')
+    }
+
+    static selectSchoolPFIOptionNo()
+    {
+        cy.get('#pfiSchemeOptionNo').click()
+        cy.get('#pfiSchemeOptionNo').should('be.checked')
+    }
+
+    static selectPrioritySchoolBuildProgOptionNo()
+    {
+        cy.get('#SchoolBuildLandPriorityBuildingProgrammeNo').click()
+        cy.get('#SchoolBuildLandPriorityBuildingProgrammeNo').should('be.checked')
+    }
+
+    static selectBuildSchoolsForFutureOptionNo()
+    {
+        cy.get('#SchoolBuildLandFutureProgrammeNo').click()
+        cy.get('#SchoolBuildLandFutureProgrammeNo').should('be.checked')
+    }
+
 
     static submitLandAndBuildingsDetails()
     {
