80ac63cbebf41fe768c9bbad805986b8eccdc882
Dan Good <dangood84@me.com>
2023-05-25 16:56:53 +0100

    Consolidated fill functions down into one function in A2BPreviousFinancialYear page object

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BPreviousFinancialYear.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BPreviousFinancialYear.ts
@@ -7,47 +7,26 @@ static previousFinancialYrElementsVisible()
     cy.previousFinancialYrElementsVisible()
 }
 
-static inputPreviousFinancialYrDate()
+static inputPreviousFinancialYrDataAndSubmit()
 {
     cy.get('#sip_pfyenddate-day').type('31')
     cy.get('#sip_pfyenddate-month').type('03')
     cy.get('#sip_pfyenddate-year').type('2022')
-}
 
-static inputPreviousFinancialYrRevenueCarryForward()
-{
     cy.get('#Revenue').clear()
     cy.get('#Revenue').type('4999.99')
-}
 
-static selectRevenueCarryForwardSurplus()
-{
     cy.get('#revenueRevenueTypeSurplus').click()
-}
 
-static verifyRevenueCarryForwardSurplusSelected()
-{
     cy.get('#revenueRevenueTypeSurplus').should('be.checked')
-}
 
-static inputPreviousFinancialYrCapitalCarryForward()
-{
     cy.get('#CapitalCarryForward').clear()
     cy.get('#CapitalCarryForward').type('4998.98')
-}
 
-static selectCapitalCarryForwardSurplus()
-{
     cy.get('#capitalRevenueTypeSurplus').click()
-}
 
-static verifyCapitalCarryForwardSurplusSelected()
-{
     cy.get('#capitalRevenueTypeSurplus').should('be.checked')
-}
 
-static submitPreviousFinancialYr()
-{
     cy.get('input[type="submit"]').click()
 }
 
