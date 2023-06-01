52ae72c776f2f506c3724904f6e5cbee5560a9cf
Dan Good <dangood84@me.com>
2023-06-01 15:37:47 +0100

    Made necessary changes to Footer component/page object

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/components/Footer.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/components/Footer.ts
@@ -6,35 +6,35 @@ export default class Footer extends BasePage {
         cy.checkFooterLinksVisible()
     }
 
-    static accessibilityStatementLinkVisible() {
-        cy.get('a[href="/accessibility"]').should('be.visible').contains('Accessibility statement')
+    static accessibilityStatementLinkVisible():void {
+        cy.get('a[href="/accessibility-statement"]').should('be.visible').contains('Accessibility statement')
     }
 
-    static cookiesLinkVisible() {
+    static cookiesLinkVisible():void {
         cy.contains('Cookie policy').should('be.visible').contains('Cookie policy')
     }
 
-    static termsAndConditionsLinkVisible() {
+    static termsAndConditionsLinkVisible():void {
         cy.get('a[href="/terms"]').should('be.visible').contains('Terms and Conditions')
     }
 
-    static privacyLinkVisible() {
+    static privacyLinkVisible():void {
         cy.get('a[href="/privacy"]').should('be.visible').contains('Privacy')
     }
 
-    static oglLogoVisible() {
+    static oglLogoVisible():void {
         cy.get('.govuk-footer__licence-logo').should('be.visible')
     }
 
-    static allContentTextVisible() {
+    static allContentTextVisible():void {
         cy.get('.govuk-footer__licence-description').should('be.visible').contains('All content is available under the Open Government Licence v3.0, except where otherwise stated')
     }
 
-    static openGovernmentLicence3LinkVisible() {
+    static openGovernmentLicence3LinkVisible():void {
         cy.get('a[href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"]').should('be.visible').contains('Open Government Licence v3.0')
     }
 
-    static crownCopyrightLinkVisible() {
+    static crownCopyrightLinkVisible():void {
         cy.get('.govuk-footer__link.govuk-footer__copyright-logo').should('be.visible').contains('© Crown copyright')
     }
 
