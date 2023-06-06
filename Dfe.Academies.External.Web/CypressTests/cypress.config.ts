f26bd0329a6c456fb462cc1958e2a523437e019c
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-06-06 14:55:54 +0100

    Add docker-compose to run Cypress with ZAP

--- Dfe.Academies.External.Web/CypressTests/cypress.config.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress.config.ts
@@ -1,6 +1,6 @@
 const { defineConfig } = require ('cypress')
 require("dotenv").config(); // Load the .env file
-
+import { generateZapReport } from './cypress/plugins/generateZapReport'
 
 module.exports = defineConfig({
   env: {
@@ -22,6 +22,12 @@ module.exports = defineConfig({
         // Map .env variables to Cypress env
         config.env = process.env;
      });
+
+     on('after:run', async () => {
+      if(process.env.zapReport) {
+        await generateZapReport()
+      }
+     })
    },
    },
  });
