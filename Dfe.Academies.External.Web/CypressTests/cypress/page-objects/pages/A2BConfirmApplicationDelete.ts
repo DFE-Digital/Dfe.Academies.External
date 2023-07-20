d23231515880f306e2ab1d52c59840a8400bc425
Dan Good <dangood84@me.com>
2023-07-20 14:00:27 +0100

    Adding A2BConfirmApplicationDelete page object

---
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BConfirmApplicationDelete.ts
@@ -0,0 +1,13 @@
+import BasePage from "../BasePage"
+export default class A2BConfirmApplicationDelete extends BasePage {
+    static confirmApplicationDeleteVerifyElementsVisible()
+    {
+
+    }
+
+    static selectConfirmDelete()
+    {
+
+    }
+
+}
