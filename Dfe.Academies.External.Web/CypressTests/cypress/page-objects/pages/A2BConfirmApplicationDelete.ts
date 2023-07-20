da24c129562742e9ead3b736be7cb4ab4e56de92
Dan Good <dangood84@me.com>
2023-07-20 14:03:01 +0100

    Added cy.confirmApplicationDeleteVerifyElementsVisible() call to static confirmApplicationDeleteVerifyElementsVisible() function in A2BConfirmApplicationDelete.ts

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BConfirmApplicationDelete.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BConfirmApplicationDelete.ts
@@ -2,7 +2,7 @@ import BasePage from "../BasePage"
 export default class A2BConfirmApplicationDelete extends BasePage {
     static confirmApplicationDeleteVerifyElementsVisible()
     {
-
+        cy.confirmApplicationDeleteVerifyElementsVisible()
     }
 
     static selectConfirmDelete()
