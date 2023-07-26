2ca9fa4a5ee014b1a215dd4d6b95f7160d4400bb
Dan Good <dangood84@me.com>
2023-07-26 12:35:41 +0100

    Amended A2BConfirmApplicationDelete page object function to checkAppIDIsCorrectAndselectConfirmDelete()

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BConfirmApplicationDelete.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/A2BConfirmApplicationDelete.ts
@@ -1,13 +1,9 @@
 import BasePage from "../BasePage"
 export default class A2BConfirmApplicationDelete extends BasePage {
-    static confirmApplicationDeleteVerifyElementsVisible()
-    {
-        cy.confirmApplicationDeleteVerifyElementsVisible()
-    }
 
-    static selectConfirmDelete()
+    static checkAppIDIsCorrectAndselectConfirmDelete()
     {
-
+        cy.checkAppIDIsCorrectAndselectConfirmDelete()
     }
 
 }
