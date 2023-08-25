d273d92aa6d372dac5a8cc040aafc0309af725b6
Dan Good <dangood84@me.com>
2023-08-25 15:45:12 +0100

    Remove all unnecessary data-generation functions APART from generateFAMTrustOpeningDate()

--- Dfe.Academies.External.Web/CypressTests/cypress/fixtures/data-generator.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/fixtures/data-generator.ts
@@ -15,43 +15,6 @@ class DataGenerator {
         return yearDate
     }
 
-    generateName() {
-        let result = ''
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-        const charactersLength = characters.length
-        
-        for (let i = 0; i < charactersLength; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength))
-        }
-        return result
-    }
-
-    generateEmail() {
-        const values = 'abcdefghijklmnopqrstuvwxyz0123456789'
-        let email = ''
-        let temp = ''
-
-        for (let i = 0; i < 10; i++) {
-            temp = values.charAt(Math.round(values.length * Math.random()))
-            email += temp
-        }
-
-        temp = ''
-        email += '@'
-
-        for (let i = 0; i < 8; i++) {
-            temp = values.charAt(Math.round(values.length * Math.random()))
-            email += temp
-        }
-        email += '.com'
-        return email
-    }
-
-    generateNumbers() {
-        const numbers = Math.floor(Math.random() * 9000000000) + 1000000000
-        return numbers.toString()
-    }
-
 }
 
 export default DataGenerator
