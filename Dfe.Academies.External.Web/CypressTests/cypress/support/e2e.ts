a7a447d3cc50b418340767d455aef0692ef2ed88
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2024-06-04 11:07:37 +0100

    Add in stricter styling rules for linter

--- Dfe.Academies.External.Web/CypressTests/cypress/support/e2e.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/e2e.ts
@@ -1,10 +1,15 @@
 import './commands'
 import 'cypress-axe'
 
-
 Cypress.on('uncaught:exception', (err) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    console.log('Cypress detected uncaught exception: ', err);
-    return false;
-  });
+  // returning false here prevents Cypress from
+  // failing the test
+  console.log('Cypress detected uncaught exception: ', err)
+  return false
+})
+
+/**
+* Cypress Grep module for filtering tests
+*/
+import registerCypressGrep from '@cypress/grep/src/support'
+registerCypressGrep()
