d6d14d893f1dcb586903359f7f985446c74c4881
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2024-06-06 15:00:51 +0100

    Cypress: clean up dependencies and README

--- Dfe.Academies.External.Web/CypressTests/cypress/support/e2e.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/e2e.ts
@@ -1,13 +1,6 @@
 import './commands'
 import 'cypress-axe'
 
-Cypress.on('uncaught:exception', (err) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  console.log('Cypress detected uncaught exception: ', err)
-  return false
-})
-
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
