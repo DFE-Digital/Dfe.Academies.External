0c58b23b910f4b939150ee2ed3085f5734519eb5
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-19 10:45:56 +0100

    Update Data Protection module to v1.1.0 (#651)
    * Sets an expiry date on Key Vault Keys
    
    * Enables Diagnostic Audit Logs for Key Vault

--- terraform/data-protection.tf
+++ terraform/data-protection.tf
@@ -1,12 +1,13 @@
 module "data_protection" {
-  source = "github.com/DFE-Digital/terraform-azurerm-aspnet-data-protection?ref=v1.0.2"
+  source = "github.com/DFE-Digital/terraform-azurerm-aspnet-data-protection?ref=v1.1.0"
 
-  data_protection_key_vault_assign_role           = true
-  data_protection_key_vault_subnet_prefix         = "172.16.100.0/28"
-  data_protection_key_vault_access_ipv4           = local.key_vault_access_ipv4
-  data_protection_resource_prefix                 = "${local.environment}${local.project_name}"
-  data_protection_azure_location                  = local.azure_location
-  data_protection_tags                            = local.tags
-  data_protection_resource_group_name             = module.azure_container_apps_hosting.azurerm_resource_group_default.name
-  data_protection_container_identity_principal_id = module.azure_container_apps_hosting.container_app_managed_identity.principal_id
+  data_protection_key_vault_assign_role                 = true
+  data_protection_key_vault_subnet_prefix               = "172.16.100.0/28"
+  data_protection_key_vault_access_ipv4                 = local.key_vault_access_ipv4
+  data_protection_resource_prefix                       = "${local.environment}${local.project_name}"
+  data_protection_azure_location                        = local.azure_location
+  data_protection_tags                                  = local.tags
+  data_protection_resource_group_name                   = module.azure_container_apps_hosting.azurerm_resource_group_default.name
+  data_protection_container_identity_principal_id       = module.azure_container_apps_hosting.container_app_managed_identity.principal_id
+  data_protection_diagnostic_log_analytics_workspace_id = module.azure_container_apps_hosting.azurerm_log_analytics_workspace_container_app.id
 }
