8d7ec90fbd0e321d75816c142f7e145b8e523e24
Dan Good <dangood84@me.com>
2024-12-16 17:58:55 +0000

    Remove redundant components from page objects as they're now defined inside spec files

--- Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/preOpeningSupportGrantDetails.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/page-objects/pages/preOpeningSupportGrantDetails.ts
@@ -17,17 +17,6 @@ class PreopeningSupportGrantDetails {
     return this
   }
 
-  public warningIcon(): this {
-    cy.get('span[class="govuk-warning-text__icon"]')
-
-    return this
-  }
-
-  public warningText(): this {
-    cy.get('strong[class="govuk-warning-text__text"]')
-
-    return this
-  }
 }
 
 const preopeningSupportGrantDetails = new PreopeningSupportGrantDetails()
