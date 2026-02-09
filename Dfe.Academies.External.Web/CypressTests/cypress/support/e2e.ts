74153e636da28d42f25ce2c932e157ab3728cc7f
paddy-clark <patrick.clark@education.gov.uk>
2026-02-09 12:54:13 +0000

    remove uncaught exception ignore

--- Dfe.Academies.External.Web/CypressTests/cypress/support/e2e.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/e2e.ts
@@ -10,13 +10,6 @@ declare global {
   }
 }
 
-Cypress.on('uncaught:exception', (err) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  console.log('Cypress detected uncaught exception: ', err)
-  return false
-})
-
 /**
 * Cypress Grep module for filtering tests
 */
