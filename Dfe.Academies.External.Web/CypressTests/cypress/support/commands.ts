55367f6e769e0e8cfd7a5c9bd8cf504aa8627146
paddy-clark <patrick.clark@education.gov.uk>
2026-02-09 14:18:44 +0000

    workaround cypress-axe issue

--- Dfe.Academies.External.Web/CypressTests/cypress/support/commands.ts
+++ Dfe.Academies.External.Web/CypressTests/cypress/support/commands.ts
@@ -11,6 +11,12 @@ Cypress.Commands.add('executeAccessibilityTests', () => {
   const impactLevel = ['critical', 'minor', 'moderate', 'serious']
   const continueOnFail = false
   Logger.log("Injecting Axe and checking accessibility");
+
+  // Temporarily increase timeout so axe.run() has time to complete on complex pages
+  // (see https://github.com/component-driven/cypress-axe/issues/160)
+  const prevTimeout = Cypress.config('defaultCommandTimeout')
+  Cypress.config('defaultCommandTimeout', 15000)
+
   cy.injectAxe()
   cy.checkA11y(
     null,
@@ -25,4 +31,7 @@ Cypress.Commands.add('executeAccessibilityTests', () => {
     null,
     continueOnFail,
   )
+
+  // Restore the original timeout
+  cy.then(() => Cypress.config('defaultCommandTimeout', prevTimeout))
 })
